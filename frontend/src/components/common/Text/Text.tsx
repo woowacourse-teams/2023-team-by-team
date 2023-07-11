8644f92b5a78f128b7d5f4c5699eaab09a585b33
hafnium1923 <hafnium1923@gmail.com>
2023-07-11 15:09:36 +0900

    feat: 여러 태그 대응 가능한 Text 컴포넌트

--- frontend/src/components/common/Text/Text.tsx
+++ frontend/src/components/common/Text/Text.tsx
@@ -5,16 +5,27 @@ import * as S from './Text.styled';
 
 export type Weight = 'light' | 'normal' | 'bold';
 export interface TextProps extends PropsWithChildren {
+  as?:
+    | 'h1'
+    | 'h2'
+    | 'h3'
+    | 'h4'
+    | 'h5'
+    | 'h6'
+    | 'p'
+    | 'span'
+    | 'strong'
+    | 'small';
   size?: Size;
   weight?: Weight;
   css?: CSSProp;
 }
 
 const Text = (props: TextProps) => {
-  const { size, weight, css, children } = props;
+  const { as = 'p', size, weight, css, children } = props;
 
   return (
-    <S.Text size={size} css={css} weight={weight}>
+    <S.Text as={as} size={size} css={css} weight={weight}>
       {children}
     </S.Text>
   );
