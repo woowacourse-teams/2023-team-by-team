5f4fb80682379f0760a013467c05e5fb82a22093
sh111-coder <ohk9134@naver.com>
2023-07-12 17:24:41 +0900

    test: 스케쥴 테스트용 더미데이터 추가
    Co-authored-by: pilyang <pilyang.dev@gmail.com>
    Co-authored-by: the9kim <the9rla@gmail.com>
    Co-authored-by: SproutMJ <mjk54565@gmail.com>

---
+++ backend/src/main/java/team/teamby/teambyteam/schedule/domain/ScheduleRepository.java
@@ -0,0 +1,6 @@
+package team.teamby.teambyteam.schedule.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+}
