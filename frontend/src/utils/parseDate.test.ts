c9083a638292d86c447c217fa26c5f445aea3963
Suyoung Jung <devsuyoung@gmail.com>
2023-07-13 16:29:15 +0900

    feat: date 객체를 파싱하는 parseDate 함수 및 테스트 작성

---
+++ frontend/src/utils/parseDate.test.ts
@@ -0,0 +1,10 @@
+import { parseDate } from './parseDate';
+
+describe('parseDate', () => {
+  it('인자로 전달받은 Date객체를 년, 월, 일, 요일을 갖는 객체로 반환한다.', () => {
+    const rawDate = new Date(2023, 6, 13);
+    const result = parseDate(rawDate);
+
+    expect(result).toEqual({ year: 2023, month: 6, date: 13, day: 4 });
+  });
+});
