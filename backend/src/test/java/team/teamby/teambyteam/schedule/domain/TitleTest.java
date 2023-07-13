b58320a9531b25c87165422d3ae86db8fc1d80f9
pilyang <30036534+pilyang@users.noreply.github.com>
2023-07-13 16:07:10 +0900

    test: 테스트 명 변경

--- backend/src/test/java/team/teamby/teambyteam/schedule/domain/TitleTest.java
+++ backend/src/test/java/team/teamby/teambyteam/schedule/domain/TitleTest.java
@@ -8,7 +8,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 class TitleTest {
 
     @Test
-    @DisplayName("일정의 이름이 null이 되면 예외가 발생한다.")
+    @DisplayName("일정의 제목이 null이 되면 예외가 발생한다.")
     void failNameNull() {
         // given
         final String nullName = null;
