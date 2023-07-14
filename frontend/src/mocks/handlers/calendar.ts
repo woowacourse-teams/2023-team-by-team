d160fd08f89dc312961316e3a05b2902d12c2519
Suyoung <jsy19970512@naver.com>
2023-07-14 14:04:52 +0900

    [FE] 캘린더 일정 등록 및 조회 모킹 구현 (#54)
    * test: 캘린더 일정 조회 handler 작성
    
    Co-authored-by: Rulu <hafnium1923@users.noreply.github.com>
    Co-authored-by: 요술토끼 <curious.wzrabbit@gmail.com>
    
    * feat: 일정 조회 fetcher 함수 구현
    
    Co-authored-by: Rulu <hafnium1923@users.noreply.github.com>
    Co-authored-by: 요술토끼 <curious.wzrabbit@gmail.com>
    
    ---------
    
    Co-authored-by: Rulu <hafnium1923@users.noreply.github.com>
    Co-authored-by: 요술토끼 <curious.wzrabbit@gmail.com>

---
+++ frontend/src/mocks/handlers/calendar.ts
@@ -0,0 +1,39 @@
+import { rest } from 'msw';
+import type { Schedule } from '~/types/schedule';
+
+const schedules: Schedule[] = [
+  {
+    id: 1,
+    name: 'test',
+    startDateTime: '2023-07-13 00:00',
+    endDateTime: '2023-07-14 00:00',
+  },
+];
+
+export const calendarHandlers = [
+  rest.get(
+    `/api/team-place/:teamPlaceId/calendar/schedules`,
+    (req, res, ctx) => {
+      return res(ctx.status(200), ctx.json(schedules));
+    },
+  ),
+
+  rest.post(
+    `/api/team-place/:teamPlaceId/calendar/schedules`,
+    async (req, res, ctx) => {
+      const { name, startDateTime, endDateTime } = await req.json();
+      const newSchedule = { id: Date.now(), name, startDateTime, endDateTime };
+      const teamPlaceId = Number(req.params.teamPlaceId);
+
+      schedules.push(newSchedule);
+
+      return res(
+        ctx.status(201),
+        ctx.set(
+          'Location',
+          `/api/team-place/${teamPlaceId}/calendar/schedules/${newSchedule.id}`,
+        ),
+      );
+    },
+  ),
+];
