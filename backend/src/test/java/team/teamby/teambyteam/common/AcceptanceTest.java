18cf76d8cdda787c300f744555dd7ca40e4e6b52
sh111-coder <ohk9134@naver.com>
2023-07-15 15:51:10 +0900

    test: AcceptanceTest 슈퍼 클래스 구현

---
+++ backend/src/test/java/team/teamby/teambyteam/common/AcceptanceTest.java
@@ -0,0 +1,27 @@
+package team.teamby.teambyteam.common;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.restassured.RestAssured;
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.jdbc.Sql;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@Sql({"/h2-reset-pk.sql", "/h2-data.sql"})
+public class AcceptanceTest {
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @LocalServerPort
+    private int port;
+
+    @BeforeEach
+    void setUp() {
+        RestAssured.port = port;
+    }
+}
