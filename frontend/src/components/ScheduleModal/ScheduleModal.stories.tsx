0e93ce13531cf8d46549096944abdd3ceb187c94
hafnium1923 <hafnium1923@gmail.com>
2023-07-16 16:27:51 +0900

    test: 트리거별로 렌더링 위치 동적 변경

--- frontend/src/components/ScheduleModal/ScheduleModal.stories.tsx
+++ frontend/src/components/ScheduleModal/ScheduleModal.stories.tsx
@@ -1,7 +1,7 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import Button from '~/components/common/Button/Button';
 import { useModal } from '~/hooks/useModal';
 import ScheduleModal from '~/components/ScheduleModal/ScheduleModal';
+import { useRef, useState } from 'react';
 
 const meta = {
   title: 'ScheduleModal',
@@ -15,16 +15,39 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   render: () => {
     const { openModal } = useModal();
+    const array = [1, 2, 3, 4, 5];
+    const refs = array.map(() => useRef(null));
+
+    const [targetRef, setTargetRef] = useState<React.RefObject<HTMLDivElement>>(
+      refs[0],
+    );
+
+    const handleOpen = (index: number) => {
+      openModal();
+      setTargetRef(refs[index]);
+    };
 
     return (
       <>
-        <Button onClick={openModal}>모달 열기</Button>
-        <ScheduleModal id={1} />
+        {array.map((_, index) => {
+          return (
+            <div
+              key={index}
+              ref={refs[index]}
+              onClick={() => handleOpen(index)}
+            >
+              모달 열기
+            </div>
+          );
+        })}
+        <ScheduleModal targetRef={targetRef} id={1} />
       </>
     );
   },
-
   args: {
     id: 1,
+    // eslint-disable-next-line
+    //@ts-ignore
+    targetRef: null,
   },
 };
