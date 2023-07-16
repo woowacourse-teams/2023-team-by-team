c57b28592271603f675bf0e0baaa717b7b9d65be
hafnium1923 <hafnium1923@gmail.com>
2023-07-16 14:53:01 +0900

    feat: 일정 조회 모달 UI 구현

--- frontend/src/components/ScheduleModal/Schedule.styled.ts
+++ frontend/src/components/ScheduleModal/Schedule.styled.ts
@@ -1,9 +1,15 @@
 import { styled, css } from 'styled-components';
 
 export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: relative;
+  z-index: ${({ theme }) => theme.zIndex.MODAL};
+  gap: 28px;
+
   width: 551px;
   height: 272px;
-  padding: 30px 40px;
+  padding: 30px 40px 30px 40px;
 
   border-radius: 10px;
 
@@ -47,12 +53,48 @@ export const TeamName = css`
   max-width: 200px;
 
   &:hover {
-    white-space: normal;
     overflow: visible;
   }
 `;
 
 export const MenuWrapper = styled.div`
   display: flex;
+  align-items: center;
   gap: 12px;
+`;
+
+export const EditWrapper = styled.div`
+  cursor: pointer;
+`;
+
+export const DeleteWrapper = styled.div`
+  cursor: pointer;
+`;
+
+export const CloseWrapper = styled.div`
+  display: flex;
+  align-items: center;
+
+  padding-left: 6px;
+
+  cursor: pointer;
+`;
+
+export const PeriodWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 2px;
+`;
+
+export const Button = css`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  align-self: flex-end;
+
+  width: 96px;
+  height: 42px;
+  padding: 10px 30px;
+
+  cursor: pointer;
 `;
--- frontend/src/components/ScheduleModal/ScheduleModal.tsx
+++ frontend/src/components/ScheduleModal/ScheduleModal.tsx
@@ -3,6 +3,7 @@ import Text from '~/components/common/Text/Text';
 import { useModal } from '~/hooks/useModal';
 import * as S from './Schedule.styled';
 import { CloseIcon, DeleteIcon, EditIcon } from '~/assets/svg';
+import Button from '~/components/common/Button/Button';
 
 interface ScheduleModalProps {
   id: number;
@@ -13,6 +14,7 @@ const ScheduleModal = (props: ScheduleModalProps) => {
   const { closeModal } = useModal();
   return (
     <Modal>
+      <S.Backdrop onClick={closeModal} />
       <S.Container>
         <S.Header>
           <S.TeamWrapper>
@@ -23,13 +25,32 @@ const ScheduleModal = (props: ScheduleModalProps) => {
             </Text>
           </S.TeamWrapper>
           <S.MenuWrapper>
-            <EditIcon />
-            <DeleteIcon />
-            <CloseIcon onClick={closeModal} />
+            <S.EditWrapper>
+              <EditIcon />
+            </S.EditWrapper>
+            <S.DeleteWrapper>
+              <DeleteIcon />
+            </S.DeleteWrapper>
+            <S.CloseWrapper>
+              <CloseIcon onClick={closeModal} />
+            </S.CloseWrapper>
           </S.MenuWrapper>
         </S.Header>
+        <Text as="h4">1차 데모데이</Text>
+        <S.PeriodWrapper>
+          <Text size="lg">07월 13일 15:00</Text>
+          <Text size="lg">~</Text>
+          <Text size="lg">07월 13일 19:00</Text>
+        </S.PeriodWrapper>
+        <Button
+          type="button"
+          variant="primary"
+          css={S.Button}
+          onClick={closeModal}
+        >
+          확인
+        </Button>
       </S.Container>
-      <S.Backdrop onClick={closeModal} />
     </Modal>
   );
 };
