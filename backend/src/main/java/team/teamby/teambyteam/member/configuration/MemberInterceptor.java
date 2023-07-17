e8898ab22eb9b165a4d8708fe31701f611561c24
SproutMj <mjk54565@gmail.com>
2023-07-17 15:17:06 +0900

    refactor: member 조회를 exist를 이용하도록 변경

--- backend/src/main/java/team/teamby/teambyteam/member/configuration/MemberInterceptor.java
+++ backend/src/main/java/team/teamby/teambyteam/member/configuration/MemberInterceptor.java
@@ -8,13 +8,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
 import org.springframework.util.StringUtils;
 import org.springframework.web.servlet.HandlerInterceptor;
-import team.teamby.teambyteam.member.domain.Member;
 import team.teamby.teambyteam.member.domain.MemberRepository;
 import team.teamby.teambyteam.member.domain.vo.Email;
 import team.teamby.teambyteam.member.exception.MemberException;
 
 import java.util.Base64;
-import java.util.Optional;
 
 @RequiredArgsConstructor
 @Component
@@ -29,11 +27,14 @@ public final class MemberInterceptor implements HandlerInterceptor {
     public boolean preHandle(final HttpServletRequest request, final HttpServletResponse response, final Object handler) throws Exception {
         final String token = extractToken(request);
         String email = extractEmailFromToken(token);
-        final Optional<Member> member = memberRepository.findByEmail(new Email(email));
-        if (member.isEmpty()) {
+        if (notExistsByEmail(email)) {
             throw new MemberException.MemberNotFoundException("멤버 조회 실패");
         }
         return true;
+    }
+
+    private boolean notExistsByEmail(final String email) {
+        return !memberRepository.existsByEmail(new Email(email));
     }
 
     private String extractToken(HttpServletRequest request) {
--- backend/src/main/java/team/teamby/teambyteam/member/domain/MemberRepository.java
+++ backend/src/main/java/team/teamby/teambyteam/member/domain/MemberRepository.java
@@ -4,8 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.query.Param;
 import team.teamby.teambyteam.member.domain.vo.Email;
 
-import java.util.Optional;
-
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    Optional<Member> findByEmail(@Param("email")Email email);
+
+    boolean existsByEmail(@Param("email") Email email);
 }
