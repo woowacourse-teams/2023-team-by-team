c49342cbc86a387acb41826596866b2d514200ad
sh111-coder <ohk9134@naver.com>
2023-07-17 17:12:54 +0900

    feat: Title 빈 값 검증 추가

--- backend/src/main/java/team/teamby/teambyteam/schedule/domain/Title.java
+++ backend/src/main/java/team/teamby/teambyteam/schedule/domain/Title.java
@@ -5,6 +5,7 @@ import jakarta.persistence.Embeddable;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import team.teamby.teambyteam.schedule.exception.ScheduleException;
 
 import java.util.Objects;
 
@@ -24,6 +25,10 @@ public class Title {
     private void validate(final String value) {
         if (Objects.isNull(value)) {
             throw new NullPointerException("일정의 제목이 null일 수 없습니다.");
+        }
+
+        if (value.isBlank()) {
+            throw new ScheduleException.TitleBlankException("일정의 제목은 빈 칸일 수 없습니다.");
         }
     }
 
