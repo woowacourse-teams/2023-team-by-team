c864e92db107d5d563ae778f0729e34486a914b1
요술토끼 <curious.wzrabbit@gmail.com>
2023-07-17 16:18:02 +0900

    test: ScheduleAddModal 컴포넌트를 테스트하기 위한 스토리북 작성

---
+++ frontend/src/components/ScheduleAddModal/ScheduleAddModal.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import Modal from '../common/Modal/Modal';
+import Button from '../common/Button/Button';
+import { useModal } from '~/hooks/useModal';
+import ScheduleAddModal from './ScheduleAddModal';
+
+/**
+ * `ScheduleAddModal` 컴포넌트는 일정 등록을 위한 폼을 포함하고 있는 모달 컴포넌트입니다.
+ */
+const meta = {
+  title: 'ScheduleAddModal',
+  component: ScheduleAddModal,
+  tags: ['autodocs'],
+} satisfies Meta<typeof ScheduleAddModal>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  render: () => {
+    const { openModal } = useModal();
+
+    return (
+      <>
+        <Button onClick={openModal}>모달 열기</Button>
+        <ScheduleAddModal teamPlaceLabel="Woowacourse TeamByTeam Corporation" />
+      </>
+    );
+  },
+  args: {
+    teamPlaceLabel: '',
+  },
+};
