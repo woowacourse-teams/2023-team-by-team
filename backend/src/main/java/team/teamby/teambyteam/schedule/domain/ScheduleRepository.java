aea0308982f447237ae9f7ff8cd2bd1fe7d2a1b7
pilyang <pilyang.dev@gmail.com>
2023-07-17 21:40:47 +0900

    feat: 특정 기간 내 팀플래이스 일정 조회 레포지토리 구현
    - 테스트용 더미 데이터 추가

--- backend/src/main/java/team/teamby/teambyteam/schedule/domain/ScheduleRepository.java
+++ backend/src/main/java/team/teamby/teambyteam/schedule/domain/ScheduleRepository.java
@@ -1,6 +1,22 @@
 package team.teamby.teambyteam.schedule.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.time.LocalDateTime;
+import java.util.List;
 
 public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
+
+    @Query("SELECT s FROM Schedule s " +
+            "WHERE s.teamPlaceId = :teamPlaceId " +
+            "AND s.span.startDateTime < :lastDateTime " +
+            "AND s.span.endDateTime > :firstDateTime"
+    )
+    List<Schedule> findAllByTeamPlaceInPeriod(
+            Long teamPlaceId,
+            LocalDateTime firstDateTime,
+            LocalDateTime lastDateTime
+    );
+
 }
---
+++ backend/src/test/java/team/teamby/teambyteam/schedule/domain/ScheduleRepositoryTest.java
@@ -0,0 +1,45 @@
+package team.teamby.teambyteam.schedule.domain;
+
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Sql(value = {"/h2-reset-pk.sql", "/h2-data.sql"})
+class ScheduleRepositoryTest {
+
+    @Autowired
+    private ScheduleRepository scheduleRepository;
+
+    @Test
+    @DisplayName("특정 기간 내 팀플레이스 일정을 조회한다.")
+    void findTeamPlaceScheduleInRange() {
+        // given
+        final Long teamPlaceId = 3L;
+        final LocalDateTime firstDateTime = LocalDateTime.of(2023, 7, 1, 0, 0);
+        final LocalDateTime endDateTime = LocalDateTime.of(2023, 7, 31, 23, 59);
+
+        // when
+        final List<Schedule> schedules = scheduleRepository.findAllByTeamPlaceInPeriod(teamPlaceId, firstDateTime, endDateTime);
+
+        //then
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(schedules).hasSize(3);
+            softly.assertThat(schedules.get(0).getTitle().getValue()).isEqualTo("3번 팀플 B");
+            softly.assertThat(schedules.get(1).getTitle().getValue()).isEqualTo("3번 팀플 C");
+            softly.assertThat(schedules.get(2).getTitle().getValue()).isEqualTo("3번 팀플 D");
+        });
+    }
+}
