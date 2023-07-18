e6b1153fd2919702490e1817ac9982b0554c124b
hafnium1923 <hafnium1923@gmail.com>
2023-07-18 13:35:39 +0900

    feat: 일정 관련 msw 핸들러 작성

--- frontend/src/mocks/handlers/calendar.ts
+++ frontend/src/mocks/handlers/calendar.ts
@@ -1,28 +1,33 @@
 import { rest } from 'msw';
-import type { Schedule } from '~/types/schedule';
+import { schedules as scheduleData } from '~/mocks/data/calendarData';
 
-const schedules: Schedule[] = [
-  {
-    id: 1,
-    title: 'test',
-    startDateTime: '2023-07-13 00:00',
-    endDateTime: '2023-07-14 00:00',
-  },
-];
+let schedules = [...scheduleData];
 
 export const calendarHandlers = [
+  //팀플레이스 일정 기간 조회
+  rest.get(`/api/team-place/:teamPlaceId/calendar/schedules`, (_, res, ctx) => {
+    return res(ctx.status(200), ctx.json(schedules));
+  }),
+  //팀플레이스 특정 일정 조회
   rest.get(
-    `/api/team-place/:teamPlaceId/calendar/schedules`,
+    `/api/team-place/:teamPlaceId/calendar/schedules/:scheduleId`,
     (req, res, ctx) => {
-      return res(ctx.status(200), ctx.json(schedules));
+      const scheduleId = Number(req.params.scheduleId);
+      const data = schedules.find((schedule) => schedule.id === scheduleId);
+      return res(ctx.status(200), ctx.json(data));
     },
   ),
-
+  //팀플레이스 일정 등록
   rest.post(
     `/api/team-place/:teamPlaceId/calendar/schedules`,
     async (req, res, ctx) => {
       const { title, startDateTime, endDateTime } = await req.json();
-      const newSchedule = { id: Date.now(), title, startDateTime, endDateTime };
+      const newSchedule = {
+        id: schedules.length,
+        title,
+        startDateTime,
+        endDateTime,
+      };
       const teamPlaceId = Number(req.params.teamPlaceId);
 
       schedules.push(newSchedule);
@@ -34,6 +39,44 @@ export const calendarHandlers = [
           `/api/team-place/${teamPlaceId}/calendar/schedules/${newSchedule.id}`,
         ),
       );
+    },
+  ),
+  //팀플레이스 일정 수정
+  rest.patch(
+    `/api/team-place/:teamPlaceId/calendar/schedules/:scheduleId`,
+    async (req, res, ctx) => {
+      const scheduleId = Number(req.params.scheduleId);
+      const { title, startDateTime, endDateTime } = await req.json();
+
+      const index = schedules.findIndex(
+        (schedule) => schedule.id === scheduleId,
+      );
+      schedules[index] = {
+        id: scheduleId,
+        title,
+        startDateTime,
+        endDateTime,
+      };
+
+      return res(ctx.status(200));
+    },
+  ),
+  //팀플레이스 일정 삭제
+  rest.delete(
+    `/api/team-place/:teamPlaceId/calendar/schedules/:scheduleId`,
+    async (req, res, ctx) => {
+      const scheduleId = Number(req.params.scheduleId);
+      schedules = schedules.filter((schedule) => schedule.id !== scheduleId);
+      schedules = schedules.map((schedule, index) => {
+        const { title, startDateTime, endDateTime } = schedule;
+        return {
+          id: index,
+          title,
+          startDateTime,
+          endDateTime,
+        };
+      });
+      return res(ctx.status(204));
     },
   ),
 ];
