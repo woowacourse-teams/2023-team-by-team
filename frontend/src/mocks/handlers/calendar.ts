5b08114af27c9d6d35af415b84574a9562c002fc
hafnium1923 <hafnium1923@gmail.com>
2023-07-18 13:52:57 +0900

    refactor: id를 length에서 Date.now로 변경

--- frontend/src/mocks/handlers/calendar.ts
+++ frontend/src/mocks/handlers/calendar.ts
@@ -1,5 +1,5 @@
 import { rest } from 'msw';
-import { schedules as scheduleData } from '~/mocks/data/calendarData';
+import { schedules as scheduleData } from '~/mocks/fixtures/schedules';
 
 let schedules = [...scheduleData];
 
@@ -8,22 +8,26 @@ export const calendarHandlers = [
   rest.get(`/api/team-place/:teamPlaceId/calendar/schedules`, (_, res, ctx) => {
     return res(ctx.status(200), ctx.json(schedules));
   }),
+
   //팀플레이스 특정 일정 조회
   rest.get(
     `/api/team-place/:teamPlaceId/calendar/schedules/:scheduleId`,
     (req, res, ctx) => {
       const scheduleId = Number(req.params.scheduleId);
       const data = schedules.find((schedule) => schedule.id === scheduleId);
+
+      if (data === undefined) return res(ctx.status(404));
       return res(ctx.status(200), ctx.json(data));
     },
   ),
+
   //팀플레이스 일정 등록
   rest.post(
     `/api/team-place/:teamPlaceId/calendar/schedules`,
     async (req, res, ctx) => {
       const { title, startDateTime, endDateTime } = await req.json();
       const newSchedule = {
-        id: schedules.length,
+        id: Date.now(),
         title,
         startDateTime,
         endDateTime,
@@ -41,16 +45,19 @@ export const calendarHandlers = [
       );
     },
   ),
+
   //팀플레이스 일정 수정
   rest.patch(
     `/api/team-place/:teamPlaceId/calendar/schedules/:scheduleId`,
     async (req, res, ctx) => {
       const scheduleId = Number(req.params.scheduleId);
       const { title, startDateTime, endDateTime } = await req.json();
-
       const index = schedules.findIndex(
         (schedule) => schedule.id === scheduleId,
       );
+
+      if (index === -1) return res(ctx.status(404));
+
       schedules[index] = {
         id: scheduleId,
         title,
@@ -61,21 +68,20 @@ export const calendarHandlers = [
       return res(ctx.status(200));
     },
   ),
+
   //팀플레이스 일정 삭제
   rest.delete(
     `/api/team-place/:teamPlaceId/calendar/schedules/:scheduleId`,
     async (req, res, ctx) => {
       const scheduleId = Number(req.params.scheduleId);
+      const index = schedules.findIndex(
+        (schedule) => schedule.id === scheduleId,
+      );
+
+      if (index === -1) return res(ctx.status(404));
+
       schedules = schedules.filter((schedule) => schedule.id !== scheduleId);
-      schedules = schedules.map((schedule, index) => {
-        const { title, startDateTime, endDateTime } = schedule;
-        return {
-          id: index,
-          title,
-          startDateTime,
-          endDateTime,
-        };
-      });
+
       return res(ctx.status(204));
     },
   ),
