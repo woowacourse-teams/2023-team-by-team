a8e12f695a33d51afc04bd8c1134fe77b306249e
Suyoung Jung <devsuyoung@gmail.com>
2023-07-19 21:11:24 +0900

    feat: 로고 svg icon 추가

--- frontend/src/assets/svg/index.ts
+++ frontend/src/assets/svg/index.ts
@@ -3,3 +3,4 @@ export { ReactComponent as ArrowRightIcon } from './arrow-right.svg';
 export { ReactComponent as CloseIcon } from './close.svg';
 export { ReactComponent as DeleteIcon } from './delete.svg';
 export { ReactComponent as EditIcon } from './edit.svg';
+export { ReactComponent as LogoIcon } from './logo.svg';
