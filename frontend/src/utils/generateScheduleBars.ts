90a9e05b56be7bc3c7b597d271621ab0e927c812
요술토끼 <curious.wzrabbit@gmail.com>
2023-07-19 18:28:39 +0900

    feat: 일정이 해당 달의 범위를 벗어나더라도 보이는 부분을 적절히 랜더링하도록 개선

--- frontend/src/utils/generateScheduleBars.ts
+++ frontend/src/utils/generateScheduleBars.ts
@@ -11,15 +11,15 @@ interface Position {
 type CalendarObject = Record<string, Position>;
 
 /**
- * 《convertSchedulesToBars》
+ * 《generateScheduleBars》
  * 이 유틸 함수는 가공 전의 스케줄 데이터를 받고, 이를 즉시 랜더링이 가능한 바 형태로 내보내는 역할을 수행합니다.
  * - 스케줄 바를 한 줄에 랜더링할 수 있도록 여러 줄에 걸친 스케줄을 여러 개의 작은 스케줄로 쪼갭니다.
  * - 스케줄이 중첩될 경우 겹치지 않는 적절한 위치에 랜더링할 수 있도록 level 값을 포함하여 제공합니다.
  *
- *
  * @param year - 스케줄 바를 표시할 연도
- * @param month - 스케줄 바를 표시할 월 (주의: 일상생활에서의 월 그대로를 사용해 주세요 - 7월일 경우: 7)
+ * @param month - 스케줄 바를 표시할 월
  * @param schedules - 가공되기 전의 스케줄 데이터가 저장된 배열
+ * @returns leveledScheduleBars - ScheduleBarProps 타입의 형태로 결과물을 반환합니다
  */
 export const generateScheduleBars = (
   year: number,
@@ -27,26 +27,41 @@ export const generateScheduleBars = (
   schedules: Schedule[],
 ) => {
   const calendarObject = generateCalendarObject(year, month);
-  const rawScheduleBars = generateRawScheduleBars(schedules, calendarObject);
+  const rawScheduleBars = generateRawScheduleBars(
+    year,
+    month,
+    schedules,
+    calendarObject,
+  );
   const slicedScheduleBars = sliceScheduleBars(rawScheduleBars);
   const leveledScheduleBars = giveLevelToScheduleBars(slicedScheduleBars);
 
   return leveledScheduleBars;
 };
 
-const generateCalendarObject = (year: number, month: number) => {
-  const firstDayOfMonth = new Date(year, month);
-  const firstDayOfCalendar = new Date(
-    firstDayOfMonth.getTime() - ONE_DAY * firstDayOfMonth.getDay(),
+const getFirstLastDateOfCalendar = (year: number, month: number) => {
+  const firstDateOfMonth = new Date(year, month);
+  const firstDateOfCalendar = new Date(
+    firstDateOfMonth.getTime() - ONE_DAY * firstDateOfMonth.getDay(),
+  );
+  const lastDateOfCalendar = new Date(
+    firstDateOfCalendar.getTime() +
+      CALENDAR.ROW_SIZE * CALENDAR.COLUMN_SIZE * ONE_DAY -
+      1,
   );
 
+  return { firstDateOfCalendar, lastDateOfCalendar };
+};
+
+const generateCalendarObject = (year: number, month: number) => {
+  const { firstDateOfCalendar } = getFirstLastDateOfCalendar(year, month);
   const calendarObject: CalendarObject = {};
 
   Array.from(
     { length: CALENDAR.ROW_SIZE * CALENDAR.COLUMN_SIZE },
     (_, index) => {
       const currentDate = new Date(
-        firstDayOfCalendar.getTime() + index * ONE_DAY,
+        firstDateOfCalendar.getTime() + index * ONE_DAY,
       );
       const formattedDate = formatDate(currentDate);
       const position = {
@@ -62,34 +77,51 @@ const generateCalendarObject = (year: number, month: number) => {
 };
 
 const formatDate = (rawDate: Date) => {
-  const { month, date } = parseDate(rawDate);
-  const formattedDate = `${month + 1}/${date}`;
+  const { year, month, date } = parseDate(rawDate);
+  const formattedDate = `${year}/${month + 1}/${date}`;
 
   return formattedDate;
 };
 
 const generateRawScheduleBars = (
+  year: number,
+  month: number,
   schedules: Schedule[],
   calendarObject: CalendarObject,
 ) => {
-  const rawScheduleBars: ScheduleBarProps[] = schedules.map((schedule) => {
-    const { startDateTime, endDateTime, id: scheduleId, title } = schedule;
-    const startDate = new Date(startDateTime);
-    const duration = calcDuration(startDateTime, endDateTime);
-    const { row, column } = calendarObject[formatDate(startDate)];
-    const id = crypto.randomUUID();
-    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+  const rawScheduleBars: ScheduleBarProps[] = [];
 
-    return {
-      id,
-      scheduleId,
-      title,
-      row,
-      column,
-      duration,
-      level: 0,
-      color: `#${randomColor}`,
-    };
+  schedules.forEach((schedule) => {
+    const { startDateTime, endDateTime, id: scheduleId, title } = schedule;
+    const { firstDateOfCalendar, lastDateOfCalendar } =
+      getFirstLastDateOfCalendar(year, month);
+    const startDate = new Date(
+      Math.max(
+        new Date(startDateTime).getTime(),
+        firstDateOfCalendar.getTime(),
+      ),
+    );
+    const endDate = new Date(
+      Math.min(new Date(endDateTime).getTime(), lastDateOfCalendar.getTime()),
+    );
+
+    if (startDate <= endDate) {
+      const duration = calcDuration(startDateTime, endDateTime);
+      const id = crypto.randomUUID();
+      const randomColor = Math.floor(Math.random() * 16777215).toString(16);
+      const { row, column } = calendarObject[formatDate(startDate)];
+
+      rawScheduleBars.push({
+        id,
+        scheduleId,
+        title,
+        row,
+        column,
+        duration,
+        level: 0,
+        color: `#${randomColor}`,
+      });
+    }
   });
 
   return rawScheduleBars;
@@ -129,7 +161,7 @@ const sliceScheduleBars = (rawScheduleBars: ScheduleBarProps[]) => {
     let currentRow = row;
     let currentColumn = column;
 
-    while (remainingDuration > 0 && currentRow <= CALENDAR.ROW_SIZE) {
+    while (remainingDuration > 0 && currentRow < CALENDAR.ROW_SIZE) {
       const currentDuration = Math.min(
         remainingDuration,
         CALENDAR.COLUMN_SIZE - currentColumn,
