8ff2ac47578c4533cb6fb5a3eb278f3423fa4d41
pilyang <pilyang.dev@gmail.com>
2023-07-20 15:15:54 +0900

    feat: 여러 팀플래이스 아이디의 기간 내 일정들 조회 기능 구현

--- backend/src/main/java/team/teamby/teambyteam/schedule/domain/ScheduleRepository.java
+++ backend/src/main/java/team/teamby/teambyteam/schedule/domain/ScheduleRepository.java
@@ -28,4 +28,23 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
             LocalDateTime lastDateTime
     );
 
+    /**
+     * Find All schedules in range
+     *
+     * @param teamPlaceIds teamPlaceId for the schedule
+     * @param firstDateTime first-date-time of the period (Inclusive)
+     * @param lastDateTime last=date-time of the period (Exclusive)
+     * @return List of the Schedules. If there is no Schedule, it will return the List with size 0.
+     */
+    @Query("SELECT s FROM Schedule s " +
+            "WHERE s.teamPlaceId IN :teamPlaceIds " +
+            "AND s.span.startDateTime < :lastDateTime " +
+            "AND s.span.endDateTime >= :firstDateTime " +
+            "ORDER BY s.span.startDateTime ASC"
+    )
+    List<Schedule> findAllByTeamPlaceIdAndPeriod(
+            List<Long> teamPlaceIds,
+            LocalDateTime firstDateTime,
+            LocalDateTime lastDateTime
+    );
 }
