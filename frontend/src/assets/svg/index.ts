91918f204ee343422712c35b29f665415673c69b
Suyoung Jung <devsuyoung@gmail.com>
2023-07-20 11:38:18 +0900

    feat: plus icon 추가

--- frontend/src/assets/svg/index.ts
+++ frontend/src/assets/svg/index.ts
@@ -4,3 +4,4 @@ export { ReactComponent as CloseIcon } from './close.svg';
 export { ReactComponent as DeleteIcon } from './delete.svg';
 export { ReactComponent as EditIcon } from './edit.svg';
 export { ReactComponent as LogoIcon } from './logo.svg';
+export { ReactComponent as PlusIcon } from './plus.svg';
