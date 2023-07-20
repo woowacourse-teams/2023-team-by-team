cc8ebda4c6db6a2e26149c2bd2f73285bd636002
Suyoung Jung <devsuyoung@gmail.com>
2023-07-20 11:39:13 +0900

    feat: 일정 등록 모달 추가 및 등록 API 연결

--- frontend/src/components/Calendar/Calendar.tsx
+++ frontend/src/components/Calendar/Calendar.tsx
@@ -1,13 +1,15 @@
 import Text from '~/components/common/Text/Text';
 import Button from '~/components/common/Button/Button';
 import DateCell from '~/components/Calendar/DateCell/DateCell';
-import { ArrowLeftIcon, ArrowRightIcon } from '~/assets/svg';
+import { ArrowLeftIcon, ArrowRightIcon, PlusIcon } from '~/assets/svg';
 import useCalendar from '~/hooks/useCalendar';
 import * as S from './Calendar.styled';
 import ScheduleBar from '~/components/ScheduleBar/ScheduleBar';
 import { generateScheduleBars } from '~/utils/generateScheduleBars';
 import { useSchedules } from '~/hooks/queries/useSchedules';
 import { DAYS_OF_WEEK } from '~/constants/calendar';
+import { useModal } from '~/hooks/useModal';
+import ScheduleAddModal from '~/components/ScheduleAddModal/ScheduleAddModal';
 
 const Calendar = () => {
   const {
@@ -18,6 +20,7 @@ const Calendar = () => {
     handlers: { handlePrevButtonClick, handleNextButtonClick },
   } = useCalendar();
   const { schedules } = useSchedules(1, year, month);
+  const { isModalOpen, openModal } = useModal();
 
   if (schedules === undefined) {
     return null;
@@ -26,67 +29,73 @@ const Calendar = () => {
   const scheduleBars = generateScheduleBars(year, month, schedules);
 
   return (
-    <S.Container>
-      <S.CalendarHeader>
-        <S.ButtonContainer>
-          <Button
-            variant="plain"
-            onClick={handlePrevButtonClick}
-            aria-label="이전 달로 이동하기"
-          >
-            <ArrowLeftIcon />
-          </Button>
-          <Text css={S.calendarTitle}>
-            {year}년 {month + 1}월
-          </Text>
-          <Button
-            variant="plain"
-            onClick={handleNextButtonClick}
-            aria-label="다음 달로 이동하기"
-          >
-            <ArrowRightIcon />
-          </Button>
-        </S.ButtonContainer>
-      </S.CalendarHeader>
-      <div>
-        <S.DaysOfWeek>
-          {DAYS_OF_WEEK.map((day) => {
-            return <S.DayOfWeek key={day}>{day}</S.DayOfWeek>;
-          })}
-        </S.DaysOfWeek>
+    <>
+      <S.Container>
+        <S.CalendarHeader>
+          <S.ButtonContainer>
+            <Button
+              variant="plain"
+              onClick={handlePrevButtonClick}
+              aria-label="이전 달로 이동하기"
+            >
+              <ArrowLeftIcon />
+            </Button>
+            <Text css={S.calendarTitle}>
+              {year}년 {month + 1}월
+            </Text>
+            <Button
+              variant="plain"
+              onClick={handleNextButtonClick}
+              aria-label="다음 달로 이동하기"
+            >
+              <ArrowRightIcon />
+            </Button>
+            <Button css={S.scheduleAddButton} onClick={openModal}>
+              <PlusIcon />
+            </Button>
+          </S.ButtonContainer>
+        </S.CalendarHeader>
         <div>
-          {calendar.map((week, rowIndex) => {
-            return (
-              <>
-                <S.ScheduleBarContainer>
-                  {scheduleBars.map((scheduleBar) => {
-                    const { id, row, ...rest } = scheduleBar;
+          <S.DaysOfWeek>
+            {DAYS_OF_WEEK.map((day) => {
+              return <S.DayOfWeek key={day}>{day}</S.DayOfWeek>;
+            })}
+          </S.DaysOfWeek>
+          <div>
+            {calendar.map((week, rowIndex) => {
+              return (
+                <>
+                  <S.ScheduleBarContainer>
+                    {scheduleBars.map((scheduleBar) => {
+                      const { id, row, ...rest } = scheduleBar;
 
-                    if (row === rowIndex)
+                      if (row === rowIndex)
+                        return (
+                          <ScheduleBar key={id} id={id} row={row} {...rest} />
+                        );
+
+                      return null;
+                    })}
+                  </S.ScheduleBarContainer>
+                  <S.DateView>
+                    {week.map((day) => {
                       return (
-                        <ScheduleBar key={id} id={id} row={row} {...rest} />
+                        <DateCell
+                          key={day.toISOString()}
+                          rawDate={day}
+                          currentMonth={month}
+                        />
                       );
-
-                    return null;
-                  })}
-                </S.ScheduleBarContainer>
-                <S.DateView>
-                  {week.map((day) => {
-                    return (
-                      <DateCell
-                        key={day.toISOString()}
-                        rawDate={day}
-                        currentMonth={month}
-                      />
-                    );
-                  })}
-                </S.DateView>
-              </>
-            );
-          })}
+                    })}
+                  </S.DateView>
+                </>
+              );
+            })}
+          </div>
         </div>
-      </div>
-    </S.Container>
+      </S.Container>
+      {isModalOpen && <ScheduleAddModal teamPlaceName="팀바팀" />}
+    </>
   );
 };
 
--- frontend/src/components/ScheduleAddModal/ScheduleAddModal.tsx
+++ frontend/src/components/ScheduleAddModal/ScheduleAddModal.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import * as S from './ScheduleAddModal.styled';
 import { useModal } from '~/hooks/useModal';
 import { CloseIcon } from '~/assets/svg';
@@ -5,6 +6,9 @@ import Modal from '~/components/common/Modal/Modal';
 import Text from '../common/Text/Text';
 import Button from '../common/Button/Button';
 import Input from '../common/Input/Input';
+import { useSendSchedule } from '~/hooks/queries/useSendSchedule';
+import type { ChangeEventHandler, FormEventHandler } from 'react';
+import type { ScheduleWithoutId } from '~/types/schedule';
 
 interface ScheduleAddModalProps {
   teamPlaceName: string;
@@ -13,6 +17,46 @@ interface ScheduleAddModalProps {
 const ScheduleAddModal = (props: ScheduleAddModalProps) => {
   const { teamPlaceName } = props;
   const { closeModal } = useModal();
+  const [schedule, setSchedule] = useState({
+    title: '',
+    startDateTime: '2023-07-20T00:00',
+    endDateTime: '2023-07-20T00:00',
+  });
+  const { mutateSendSchedule } = useSendSchedule(1);
+
+  const handleScheduleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    const { name, value } = e.target;
+
+    setSchedule((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleScheduleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+
+    const { startDateTime, endDateTime } = schedule;
+
+    mutateSendSchedule(
+      {
+        ...schedule,
+        startDateTime: startDateTime.replace(
+          'T',
+          ' ',
+        ) as ScheduleWithoutId['startDateTime'],
+        endDateTime: endDateTime.replace(
+          'T',
+          ' ',
+        ) as ScheduleWithoutId['startDateTime'],
+      },
+      {
+        onSuccess: () => closeModal(),
+      },
+    );
+  };
 
   return (
     <Modal>
@@ -35,44 +79,56 @@ const ScheduleAddModal = (props: ScheduleAddModalProps) => {
             height="100%"
             placeholder="일정 제목"
             css={S.title}
+            name="title"
+            value={schedule['title']}
+            required
+            onChange={handleScheduleChange}
           />
         </S.TitleWrapper>
 
-        <S.TimeSelectContainer>
-          <Text size="xxl" weight="bold">
-            일정 시작
-          </Text>
-          <Input
-            width="220px"
-            height="40px"
-            type="datetime-local"
-            css={S.dateTimeLocalInput}
-          />
-          <Text size="xxl" weight="bold">
-            종일
-          </Text>
-          <S.CheckBox type="checkbox" />
-        </S.TimeSelectContainer>
-        <S.TimeSelectContainer>
-          <Text size="xxl" weight="bold">
-            일정 마감
-          </Text>
-          <Input
-            width="220px"
-            height="40px"
-            type="datetime-local"
-            css={S.dateTimeLocalInput}
-          />
-        </S.TimeSelectContainer>
-        <S.TeamNameContainer title={teamPlaceName}>
-          <S.Circle />
-          <Text css={S.teamPlaceName}>{teamPlaceName}</Text>
-        </S.TeamNameContainer>
-        <S.ControlButtonWrapper>
-          <Button variant="primary" onClick={closeModal}>
-            등록
-          </Button>
-        </S.ControlButtonWrapper>
+        <form onSubmit={handleScheduleSubmit}>
+          <S.TimeSelectContainer>
+            <Text size="xxl" weight="bold">
+              일정 시작
+            </Text>
+            <Input
+              width="220px"
+              height="40px"
+              type="datetime-local"
+              css={S.dateTimeLocalInput}
+              name="startDateTime"
+              value={schedule['startDateTime']}
+              onChange={handleScheduleChange}
+              required
+            />
+            <Text size="xxl" weight="bold">
+              종일
+            </Text>
+            <S.CheckBox type="checkbox" />
+          </S.TimeSelectContainer>
+          <S.TimeSelectContainer>
+            <Text size="xxl" weight="bold">
+              일정 마감
+            </Text>
+            <Input
+              width="220px"
+              height="40px"
+              type="datetime-local"
+              css={S.dateTimeLocalInput}
+              name="endDateTime"
+              value={schedule['endDateTime']}
+              onChange={handleScheduleChange}
+              required
+            />
+          </S.TimeSelectContainer>
+          <S.TeamNameContainer title={teamPlaceName}>
+            <S.Circle />
+            <Text css={S.teamPlaceName}>{teamPlaceName}</Text>
+          </S.TeamNameContainer>
+          <S.ControlButtonWrapper>
+            <Button variant="primary">등록</Button>
+          </S.ControlButtonWrapper>
+        </form>
       </S.Container>
     </Modal>
   );
