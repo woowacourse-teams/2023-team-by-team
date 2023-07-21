1179f6c571e346f0c0226ce71f21342440d8a18c
SproutMj <mjk54565@gmail.com>
2023-07-21 17:44:41 +0900

    refactor: @Param 제거

--- backend/src/main/java/team/teamby/teambyteam/member/domain/MemberRepository.java
+++ backend/src/main/java/team/teamby/teambyteam/member/domain/MemberRepository.java
@@ -1,10 +1,9 @@
 package team.teamby.teambyteam.member.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import team.teamby.teambyteam.member.domain.vo.Email;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    boolean existsByEmail(@Param("email") Email email);
+    boolean existsByEmail(Email email);
 }
