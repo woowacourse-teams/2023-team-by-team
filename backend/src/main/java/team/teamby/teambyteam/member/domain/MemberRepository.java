0fcd2051f7439b2311ad8e7685d5bcfecfde786d
pilyang <pilyang.dev@gmail.com>
2023-07-22 17:54:02 +0900

    refactor: Param annotation 제거

--- backend/src/main/java/team/teamby/teambyteam/member/domain/MemberRepository.java
+++ backend/src/main/java/team/teamby/teambyteam/member/domain/MemberRepository.java
@@ -1,14 +1,13 @@
 package team.teamby.teambyteam.member.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import team.teamby.teambyteam.member.domain.vo.Email;
 
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    boolean existsByEmail(@Param("email") Email email);
+    boolean existsByEmail(Email email);
 
-    Optional<Member> findByEmail(@Param("email") Email email);
+    Optional<Member> findByEmail(Email email);
 }
