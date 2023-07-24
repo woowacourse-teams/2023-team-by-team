3d0a6c5e872bb4e2c7338374ae2f9de97cd1502b
Suyoung Jung <devsuyoung@gmail.com>
2023-07-24 16:45:38 +0900

    refactor: 타입 가드 매개변수명을 범용적인 네이밍으로 수정

--- frontend/src/types/typeGuard.ts
+++ frontend/src/types/typeGuard.ts
@@ -1,7 +1,5 @@
 import type { YYYYMMDDHHMM } from '~/types/schedule';
 
-export const isYYYYMMDDHHMM = (
-  dateString: string,
-): dateString is YYYYMMDDHHMM => {
-  return /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}$/.test(dateString);
+export const isYYYYMMDDHHMM = (value: string): value is YYYYMMDDHHMM => {
+  return /^\d{4}-\d{2}-\d{2} \d{2}:\d{2}$/.test(value);
 };
