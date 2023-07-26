c27a16bf50b62366b13f29255cd4790f1eb373ff
the9kim <the9rla@gmail.com>
2023-07-26 16:51:15 +0900

    feat: 팀 피드 도메인 구현
    Co-authored-by: pilyang <pilyang.dev@gmail.com>
    Co-authored-by: sh111-coder <ohk9134@naver.com>
    Co-authored-by: the9kim <the9rla@gmail.com>
    Co-authored-by: SproutMJ <mjk54565@gmail.com>

---
+++ backend/src/main/java/team/teamby/teambyteam/feed/domain/vo/Content.java
@@ -0,0 +1,35 @@
+package team.teamby.teambyteam.feed.domain.vo;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import team.teamby.teambyteam.feed.exception.FeedException;
+
+import java.util.Objects;
+
+@Embeddable
+@Getter
+@EqualsAndHashCode
+@NoArgsConstructor
+public class Content {
+
+    public static final int MAX_LENGTH = 10000;
+    @Column(name = "content", nullable = false)
+    private String value;
+
+    public Content(String value) {
+        validate(value);
+        this.value = value;
+    }
+
+    private void validate(final String value) {
+        if (Objects.isNull(value)) {
+            throw new NullPointerException("피드 내용은 null일 수 없습니다.");
+        }
+        if (value.length() > MAX_LENGTH) {
+            throw new FeedException.ContentLengthException();
+        }
+    }
+}
