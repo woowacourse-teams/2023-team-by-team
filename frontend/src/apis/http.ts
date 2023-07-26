310320ba352f00600aca6dd0a59feab4a5fa965a
Suyoung Jung <devsuyoung@gmail.com>
2023-07-26 17:23:23 +0900

    refactor: access token의 불필요한 중괄호 제거

--- frontend/src/apis/http.ts
+++ frontend/src/apis/http.ts
@@ -1,7 +1,7 @@
 const options = {
   headers: {
     'Content-Type': 'application/json',
-    Authorization: `Bearer ${process.env.REACT_APP_ACCESS_TOKEN}}`,
+    Authorization: `Bearer ${process.env.REACT_APP_ACCESS_TOKEN}`,
   },
 };
 
