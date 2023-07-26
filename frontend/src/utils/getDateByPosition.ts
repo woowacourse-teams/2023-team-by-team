9fa6776a41d8b84d54252709512018b4ad255ad7
요술토끼 <curious.wzrabbit@gmail.com>
2023-07-26 11:26:41 +0900

    feat: 좌표를 날짜객체로 변환하는 유틸함수 구현

---
+++ frontend/src/utils/getDateByPosition.ts
@@ -0,0 +1,31 @@
+const ROW_REGEX = /^[0-5]$/;
+const COLUMN_REGEX = /^[0-6]$/;
+const ONE_DAY = 86_400_000;
+const ERROR_MESSAGE =
+  '잘못된 행 또는 열이 대입되었습니다. 입력 데이터가 아래의 조건을 지키는 지 확인해 주세요:\n- 행은 0 이상 5 이하의 정수여야 합니다.\n- 열은 0 이상 6 이하의 정수여야 합니다.';
+
+export const getDateByPosition = (
+  year: number,
+  month: number,
+  row: number,
+  column: number,
+) => {
+  throwIfInvalidRowColumn(row, column);
+
+  const firstDateOfMonth = new Date(year, month, 1);
+  const desiredDate = new Date(
+    firstDateOfMonth.getTime() +
+      ONE_DAY * (row * 7 + column - firstDateOfMonth.getDay()),
+  );
+
+  return desiredDate;
+};
+
+const throwIfInvalidRowColumn = (row: number, column: number) => {
+  if (
+    !ROW_REGEX.test(row.toString()) ||
+    !COLUMN_REGEX.test(column.toString())
+  ) {
+    throw Error(ERROR_MESSAGE);
+  }
+};
