55be6629228b98778a65c3d48d577cb586380de8
sh111-coder <ohk9134@naver.com>
2023-07-27 09:19:18 +0900

    refactor: ServiceTest truncate sql 삭제

--- backend/src/test/java/team/teamby/teambyteam/common/ServiceTest.java
+++ backend/src/test/java/team/teamby/teambyteam/common/ServiceTest.java
@@ -2,13 +2,11 @@ package team.teamby.teambyteam.common;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.jdbc.Sql;
 import org.springframework.transaction.annotation.Transactional;
 import team.teamby.teambyteam.common.builder.TestFixtureBuilder;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @Transactional
-@Sql(value = {"/h2-truncate.sql"})
 public abstract class ServiceTest {
 
     @Autowired
