04e092597f55bb0bc01892c3ca377643c08438ca
hafnium1923 <hafnium1923@gmail.com>
2023-07-27 13:43:22 +0900

    refactor: return 전에 한줄 띄우기

--- frontend/src/components/Notification/Notification.tsx
+++ frontend/src/components/Notification/Notification.tsx
@@ -11,6 +11,7 @@ export interface NotificationProps {
 const Notification = (props: NotificationProps) => {
   const { color = '#516FFF', content, size = 'md' } = props;
   const textSize: Extract<TextSize, 'md' | 'xl'> = size === 'md' ? 'xl' : 'md';
+
   return (
     <S.Container color={color} size={size}>
       <Text size={textSize} weight="bold">
