1d1a8bb1649c980bcb5c640ec1ddf6501c586e58
요술토끼 <curious.wzrabbit@gmail.com>
2023-07-27 22:41:56 +0900

    feat: 임시 스레드 태그 구현

---
+++ frontend/src/components/NoticeThread/Tag/Tag.styled.ts
@@ -0,0 +1,23 @@
+import { styled, css } from 'styled-components';
+import type { TagProps } from './Tag';
+
+export const TagContainer = styled.div<TagProps>`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  column-gap: 8px;
+
+  width: 150px;
+  height: 42px;
+
+  border-radius: 18px;
+  background-color: ${({ theme }) => theme.color.PRIMARY};
+
+  box-shadow: 0 0 10px ${({ theme }) => theme.color.GRAY400};
+
+  ${({ css }) => css}
+`;
+
+export const tagLabel = css`
+  color: ${({ theme }) => theme.color.WHITE};
+`;
