a67e65d23638c999c8b7225d4d48e7a36fffb26e
pilyang <pilyang.dev@gmail.com>
2023-07-27 17:07:01 +0900

    style: import wildcard 제거

--- backend/src/main/java/team/teamby/teambyteam/feed/domain/Feed.java
+++ backend/src/main/java/team/teamby/teambyteam/feed/domain/Feed.java
@@ -1,6 +1,14 @@
 package team.teamby.teambyteam.feed.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.DiscriminatorColumn;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
