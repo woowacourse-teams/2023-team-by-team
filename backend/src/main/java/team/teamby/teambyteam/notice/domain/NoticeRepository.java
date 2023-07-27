63dfdc58a1eddd33b4412be05127a88e8e2ef1ed
the9kim <the9rla@gmail.com>
2023-07-27 19:19:02 +0900

    feat: 팀 피드 공지 레포지토리 생성

---
+++ backend/src/main/java/team/teamby/teambyteam/notice/domain/NoticeRepository.java
@@ -0,0 +1,7 @@
+package team.teamby.teambyteam.notice.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface NoticeRepository extends JpaRepository<NoticeRepository, Long> {
+
+}
