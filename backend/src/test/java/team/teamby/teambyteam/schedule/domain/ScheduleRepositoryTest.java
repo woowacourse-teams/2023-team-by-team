786fc720095188dff90fa7102c3e42e77d20998b
sh111-coder <ohk9134@naver.com>
2023-07-27 09:35:14 +0900

    refactor: ScheduleRepositoryTest data.sql 의존성 제거

--- backend/src/test/java/team/teamby/teambyteam/schedule/domain/ScheduleRepositoryTest.java
+++ backend/src/test/java/team/teamby/teambyteam/schedule/domain/ScheduleRepositoryTest.java
@@ -1,19 +1,21 @@
 package team.teamby.teambyteam.schedule.domain;
 
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import team.teamby.teambyteam.common.RepositoryTest;
-import team.teamby.teambyteam.fixtures.ScheduleFixtures;
-import team.teamby.teambyteam.schedule.domain.vo.Title;
+import team.teamby.teambyteam.teamplace.domain.TeamPlace;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.SoftAssertions.assertSoftly;
+import static team.teamby.teambyteam.common.fixtures.ScheduleFixtures.*;
+import static team.teamby.teambyteam.common.fixtures.TeamPlaceFixtures.ENGLISH_TEAM_PLACE;
+import static team.teamby.teambyteam.common.fixtures.TeamPlaceFixtures.JAPANESE_TEAM_PLACE;
 
 public class ScheduleRepositoryTest extends RepositoryTest {
 
@@ -24,68 +26,78 @@ public class ScheduleRepositoryTest extends RepositoryTest {
     @DisplayName("특정 기간 내 팀플레이스 일정을 조회한다.")
     void findTeamPlaceScheduleInRange() {
         // given
-        final Long teamPlaceId = 3L;
+        final TeamPlace ENGLISH_TEAM_PLACE = testFixtureBuilder.buildTeamPlace(ENGLISH_TEAM_PLACE());
+        final Long ENGLISH_TEAM_PLACE_ID = ENGLISH_TEAM_PLACE.getId();
+        final List<Schedule> schedulesToSave = List.of(MONTH_7_AND_DAY_12_ALL_DAY_SCHEDULE(ENGLISH_TEAM_PLACE_ID), MONTH_7_AND_DAY_12_N_HOUR_SCHEDULE(ENGLISH_TEAM_PLACE_ID));
+        final List<Schedule> expectedSchedule = testFixtureBuilder.buildSchedules(schedulesToSave);
+
         final LocalDateTime firstDateTime = LocalDateTime.of(2023, 7, 1, 0, 0);
         final LocalDateTime endDateTime = LocalDateTime.of(2023, 8, 1, 0, 0);
 
         // when
-        final List<Schedule> schedules = scheduleRepository.findAllByTeamPlaceIdAndPeriod(teamPlaceId, firstDateTime, endDateTime);
+        final List<Schedule> actualSchedules = scheduleRepository.findAllByTeamPlaceIdAndPeriod(ENGLISH_TEAM_PLACE_ID, firstDateTime, endDateTime);
 
         //then
-        assertSoftly(softly -> {
-            softly.assertThat(schedules).hasSize(4);
-            softly.assertThat(schedules.get(0).getTitle().getValue()).isEqualTo("3번 팀플 B");
-            softly.assertThat(schedules.get(1).getTitle().getValue()).isEqualTo("3번 팀플 C");
-            softly.assertThat(schedules.get(2).getTitle().getValue()).isEqualTo("3번 팀플 E");
-            softly.assertThat(schedules.get(3).getTitle().getValue()).isEqualTo("3번 팀플 D");
-        });
+        Assertions.assertThat(actualSchedules).usingRecursiveFieldByFieldElementComparator()
+                .isEqualTo(expectedSchedule);
     }
 
     @Test
     @DisplayName("특정 기간 내 팀플레이스 일정을 조회한다.")
     void findMultipleTeamPlaceScheduleInRange() {
         // given
-        final List<Long> teamPlaceId = List.of(2L, 3L);
-        final LocalDateTime firstDateTime = LocalDateTime.of(2023, 6, 1, 0, 0);
-        final LocalDateTime endDateTime = LocalDateTime.of(2023, 7, 1, 0, 0);
+        final TeamPlace ENGLISH_TEAM_PLACE = testFixtureBuilder.buildTeamPlace(ENGLISH_TEAM_PLACE());
+        final TeamPlace JAPANESE_TEAM_PLACE = testFixtureBuilder.buildTeamPlace(JAPANESE_TEAM_PLACE());
+        final Long ENGLISH_TEAM_PLACE_ID = ENGLISH_TEAM_PLACE.getId();
+        final Long JAPANESE_TEAM_PLACE_ID = JAPANESE_TEAM_PLACE.getId();
+
+        List<Schedule> schedulesToSave = List.of(
+                MONTH_7_AND_DAY_12_ALL_DAY_SCHEDULE(ENGLISH_TEAM_PLACE_ID), MONTH_7_AND_DAY_12_N_HOUR_SCHEDULE(ENGLISH_TEAM_PLACE_ID),
+                MONTH_7_DAY_28_AND_MONTH_8_SCHEDULE(JAPANESE_TEAM_PLACE_ID), MONTH_7_DAY_29_AND_MONTH_8_SCHEDULE(JAPANESE_TEAM_PLACE_ID)
+        );
+        List<Schedule> expectedSchedules = testFixtureBuilder.buildSchedules(schedulesToSave);
+
+        final List<Long> teamPlaceId = List.of(ENGLISH_TEAM_PLACE_ID, JAPANESE_TEAM_PLACE_ID);
+        final LocalDateTime firstDateTime = LocalDateTime.of(2023, 7, 1, 0, 0);
+        final LocalDateTime endDateTime = LocalDateTime.of(2023, 8, 1, 0, 0);
 
         // when
-        final List<Schedule> schedules = scheduleRepository.findAllByTeamPlaceIdAndPeriod(teamPlaceId, firstDateTime, endDateTime);
+        final List<Schedule> actualSchedules = scheduleRepository.findAllByTeamPlaceIdAndPeriod(teamPlaceId, firstDateTime, endDateTime);
 
         //then
-        assertSoftly(softly -> {
-            softly.assertThat(schedules).hasSize(5);
-            softly.assertThat(schedules.get(0).getTitle().getValue()).isEqualTo("3번 팀플 6월 첫날");
-            softly.assertThat(schedules.get(1).getTitle().getValue()).isEqualTo("2번 팀플 6월 첫날");
-            softly.assertThat(schedules.get(2).getTitle().getValue()).isEqualTo("3번 팀플 A");
-            softly.assertThat(schedules.get(3).getTitle().getValue()).isEqualTo("2번 팀플 6월 어느날");
-            softly.assertThat(schedules.get(4).getTitle().getValue()).isEqualTo("3번 팀플 B");
-        });
+        assertThat(actualSchedules).usingRecursiveFieldByFieldElementComparator()
+                .isEqualTo(expectedSchedules);
     }
 
     @Test
     @DisplayName("팀 캘린더 하루 일정을 조회한다.")
     void findDailyTeamCalendarSchedule() {
         // given
-        final Long teamPlaceId = ScheduleFixtures.Schedule1_N_Hour.TEAM_PLACE_ID;
+        final TeamPlace ENGLISH_TEAM_PLACE = testFixtureBuilder.buildTeamPlace(ENGLISH_TEAM_PLACE());
+        final Long ENGLISH_TEAM_PLACE_ID = ENGLISH_TEAM_PLACE.getId();
+        final List<Schedule> schedulesToSave = List.of(MONTH_7_AND_DAY_12_ALL_DAY_SCHEDULE(ENGLISH_TEAM_PLACE_ID), MONTH_7_AND_DAY_12_N_HOUR_SCHEDULE(ENGLISH_TEAM_PLACE_ID));
+        final List<Schedule> expectedSchedule = testFixtureBuilder.buildSchedules(schedulesToSave);
+
         final LocalDateTime startDateTime = LocalDateTime.of(2023, 7, 12, 0, 0, 0);
         final LocalDateTime endDateTime = LocalDateTime.of(2023, 7, 12, 23, 59, 59);
 
         // when
-        final List<Schedule> schedules = scheduleRepository.findAllByTeamPlaceIdAndDailyPeriod(teamPlaceId, startDateTime, endDateTime);
+        final List<Schedule> actualSchedules = scheduleRepository.findAllByTeamPlaceIdAndDailyPeriod(ENGLISH_TEAM_PLACE_ID, startDateTime, endDateTime);
 
         // then
-        assertSoftly(softly -> {
-            softly.assertThat(schedules).hasSize(2);
-            softly.assertThat(schedules.stream().map(Schedule::getTitle).map(Title::getValue))
-                    .containsExactly("1번 팀플 종일 일정", "1번 팀플 N시간 일정");
-        });
+        Assertions.assertThat(actualSchedules).usingRecursiveFieldByFieldElementComparator()
+                .isEqualTo(expectedSchedule);
     }
 
     @ParameterizedTest
     @CsvSource(value = {"-1:false", "1:true"}, delimiter = ':')
     @DisplayName("일정이 존재하면 true, 존재하지 않으면 false를 반환한다.")
     void isExistById(Long scheduleId, boolean expected) {
+        // given
+        final TeamPlace ENGLISH_TEAM_PLACE = testFixtureBuilder.buildTeamPlace(ENGLISH_TEAM_PLACE());
+        final Long ENGLISH_TEAM_PLACE_ID = ENGLISH_TEAM_PLACE.getId();
+        testFixtureBuilder.buildSchedule(MONTH_7_AND_DAY_12_N_HOUR_SCHEDULE(ENGLISH_TEAM_PLACE_ID));
+
         // when
         final boolean actual = scheduleRepository.existsById(scheduleId);
 
@@ -97,12 +109,14 @@ public class ScheduleRepositoryTest extends RepositoryTest {
     @DisplayName("일정이 있는 경우에 삭제가 되는지 확인한다.")
     void deleteById() {
         //given
-        final long id = 1L;
+        final TeamPlace ENGLISH_TEAM_PLACE = testFixtureBuilder.buildTeamPlace(ENGLISH_TEAM_PLACE());
+        final Long ENGLISH_TEAM_PLACE_ID = ENGLISH_TEAM_PLACE.getId();
+        testFixtureBuilder.buildSchedule(MONTH_7_AND_DAY_12_N_HOUR_SCHEDULE(ENGLISH_TEAM_PLACE_ID));
 
         // when
-        final boolean before = scheduleRepository.existsById(id);
-        scheduleRepository.deleteById(id);
-        final boolean after = scheduleRepository.existsById(id);
+        final boolean before = scheduleRepository.existsById(ENGLISH_TEAM_PLACE_ID);
+        scheduleRepository.deleteById(ENGLISH_TEAM_PLACE_ID);
+        final boolean after = scheduleRepository.existsById(ENGLISH_TEAM_PLACE_ID);
 
         // then
         assertThat(before).isTrue();
