95d427edd168406526e93b1fdaaa31524912aea8
sh111-coder <ohk9134@naver.com>
2023-07-27 16:56:09 +0900

    refactor: TeamPlaceRepositoryTest data.sql 의존성 제거

--- backend/src/test/java/team/teamby/teambyteam/teamplace/domain/TeamPlaceRepositoryTest.java
+++ backend/src/test/java/team/teamby/teambyteam/teamplace/domain/TeamPlaceRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import team.teamby.teambyteam.common.RepositoryTest;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static team.teamby.teambyteam.common.fixtures.TeamPlaceFixtures.ENGLISH_TEAM_PLACE;
 
 class TeamPlaceRepositoryTest extends RepositoryTest {
 
@@ -17,6 +18,9 @@ class TeamPlaceRepositoryTest extends RepositoryTest {
     @CsvSource(value = {"-1:false", "1:true"}, delimiter = ':')
     @DisplayName("id에 해당하는 팀 플레이스가 존재하면 true, 존재하지 않으면 false를 반환한다.")
     void isExistById(Long teamPlaceId, boolean expected) {
+        // given
+        testFixtureBuilder.buildTeamPlace(ENGLISH_TEAM_PLACE());
+
         // when
         final boolean actual = teamPlaceRepository.existsById(teamPlaceId);
 
