ec80ae5e2b2ea0ae8adca48cbdf02b426cb4a140
Suyoung Jung <devsuyoung@gmail.com>
2023-07-28 14:13:15 +0900

    refactor: MenuItem에서 cssprop을 props로 받도록 수정

--- frontend/src/components/common/Menu/MenuItem/MenuItem.styled.ts
+++ frontend/src/components/common/Menu/MenuItem/MenuItem.styled.ts
@@ -1,5 +1,6 @@
 import { styled } from 'styled-components';
+import type { MenuItemProps } from '~/components/common/Menu/MenuItem/MenuItem';
 
-export const Wrapper = styled.li`
+export const Wrapper = styled.li<MenuItemProps>`
   padding: 8px 12px;
 `;
--- frontend/src/components/common/Menu/MenuItem/MenuItem.tsx
+++ frontend/src/components/common/Menu/MenuItem/MenuItem.tsx
@@ -1,10 +1,19 @@
 import * as S from './MenuItem.styled';
 import type { PropsWithChildren } from 'react';
+import type { CSSProp } from 'styled-components';
 
-const MenuItem = (props: PropsWithChildren) => {
-  const { children } = props;
+export interface MenuItemProps {
+  css: CSSProp;
+}
 
-  return <S.Wrapper role="menuitem">{children}</S.Wrapper>;
+const MenuItem = (props: PropsWithChildren<MenuItemProps>) => {
+  const { children, css } = props;
+
+  return (
+    <S.Wrapper role="menuitem" css={css}>
+      {children}
+    </S.Wrapper>
+  );
 };
 
 export default MenuItem;
