667a9a27882bdbf6cac62f8ce8df4c9981d87f74
the9kim <the9rla@gmail.com>
2023-07-29 12:16:44 +0900

    refactor: MemberEmailDto 내 validation 삭제 및 기타 리팩터링

--- backend/src/main/java/team/teamby/teambyteam/member/configuration/dto/MemberEmailDto.java
+++ backend/src/main/java/team/teamby/teambyteam/member/configuration/dto/MemberEmailDto.java
@@ -1,9 +1,5 @@
 package team.teamby.teambyteam.member.configuration.dto;
 
-import jakarta.validation.constraints.NotBlank;
-
-public record MemberEmailDto(
-        @NotBlank(message = "이메일은 공백이 될 수 없습니다.")
-        String email
-) {
+public record MemberEmailDto(String email) {
 }
+
--- backend/src/main/java/team/teamby/teambyteam/notice/domain/NoticeRepository.java
+++ backend/src/main/java/team/teamby/teambyteam/notice/domain/NoticeRepository.java
@@ -7,5 +7,4 @@ import java.util.List;
 public interface NoticeRepository extends JpaRepository<Notice, Long> {
 
     List<Notice> findByTeamPlaceId(Long teamPlaceId);
-
 }
