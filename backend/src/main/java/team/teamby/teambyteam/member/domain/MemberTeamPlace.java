0e31762d89b0c41d20e549deedc3bce644c77262
pilyang <pilyang.dev@gmail.com>
2023-07-29 14:47:51 +0900

    feat: MemberTeamPlace 필드 추가
    - DisplayMemberName 필드 추가
    - DisplayTeamPlaceName 필드 추가

--- backend/src/main/java/team/teamby/teambyteam/member/domain/MemberTeamPlace.java
+++ backend/src/main/java/team/teamby/teambyteam/member/domain/MemberTeamPlace.java
@@ -1,9 +1,18 @@
 package team.teamby.teambyteam.member.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import team.teamby.teambyteam.global.domain.BaseEntity;
+import team.teamby.teambyteam.member.domain.vo.DisplayMemberName;
+import team.teamby.teambyteam.member.domain.vo.DisplayTeamPlaceName;
 import team.teamby.teambyteam.teamplace.domain.TeamPlace;
 
 @Getter
@@ -23,9 +32,17 @@ public class MemberTeamPlace extends BaseEntity {
     @JoinColumn(nullable = false)
     private TeamPlace teamPlace;
 
+    @Embedded
+    private DisplayMemberName displayMemberName;
+
+    @Embedded
+    private DisplayTeamPlaceName displayTeamPlaceName;
+
     public void setMemberAndTeamPlace(final Member member, final TeamPlace teamPlace) {
         this.member = member;
         this.teamPlace = teamPlace;
+        this.displayMemberName = new DisplayMemberName(member.getName().getValue());
+        this.displayTeamPlaceName = new DisplayTeamPlaceName(teamPlace.getName().getValue());
         member.getMemberTeamPlaces().add(this);
         teamPlace.getMemberTeamPlaces().add(this);
     }
