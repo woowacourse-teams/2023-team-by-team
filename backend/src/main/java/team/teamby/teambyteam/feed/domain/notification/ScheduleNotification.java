50f922e2ce5b58436d81e63ac1779cda48c89e54
sh111-coder <ohk9134@naver.com>
2023-07-30 03:25:46 +0900

    feat: 일정 알림 기능 구현

--- backend/src/main/java/team/teamby/teambyteam/feed/domain/notification/ScheduleNotification.java
+++ backend/src/main/java/team/teamby/teambyteam/feed/domain/notification/ScheduleNotification.java
@@ -1,14 +1,12 @@
 package team.teamby.teambyteam.feed.domain.notification;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import team.teamby.teambyteam.feed.domain.FeedType;
 import team.teamby.teambyteam.feed.domain.vo.Content;
+import team.teamby.teambyteam.schedule.application.event.ScheduleEvent;
 
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
@@ -27,8 +25,12 @@ public class ScheduleNotification extends Notification {
         return FeedType.SCHEDULE_NOTIFICATION;
     }
 
-    public ScheduleNotification(final Long teamPlaceId, final Content content, final Long scheduleId) {
+    protected ScheduleNotification(final Long teamPlaceId, final Content content, final Long scheduleId) {
         super(teamPlaceId, content);
         this.scheduleId = scheduleId;
+    }
+
+    public static ScheduleNotification from(final ScheduleEvent scheduleEvent) {
+        return ScheduleNotificationFactory.from(scheduleEvent);
     }
 }
