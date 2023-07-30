1c02795e048140583183cce9b4dab7ea88022728
요술토끼 <curious.wzrabbit@gmail.com>
2023-07-31 01:27:55 +0900

    test: NoticeTag 컴포넌트에 대해 스토리 개선

--- frontend/src/components/NoticeTag/NoticeTag.stories.tsx
+++ frontend/src/components/NoticeTag/NoticeTag.stories.tsx
@@ -1,6 +1,9 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import NoticeTag from './NoticeTag';
 
+/**
+ * `NoticeTag` 는 공지 여부임을 표시하는 카테고리 컴포넌트입니다.
+ */
 const meta = {
   title: 'NoticeTag',
   component: NoticeTag,
@@ -12,5 +15,16 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {},
+  args: {
+    size: 'md',
+  },
+};
+
+/**
+ * 이 사이즈는 모아보기 페이지에서 사용될 사이즈입니다.
+ */
+export const Small: Story = {
+  args: {
+    size: 'sm',
+  },
 };
