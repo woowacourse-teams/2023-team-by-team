67fb66998f7c96651b0db2f4f7122262760f5069
요술토끼 <curious.wzrabbit@gmail.com>
2023-07-31 16:23:11 +0900

    chore: 컨플릭트 해소

--- frontend/src/assets/svg/index.ts
+++ frontend/src/assets/svg/index.ts
@@ -5,7 +5,7 @@ export { ReactComponent as DeleteIcon } from './delete.svg';
 export { ReactComponent as EditIcon } from './edit.svg';
 export { ReactComponent as LogoIcon } from './logo.svg';
 export { ReactComponent as PlusIcon } from './plus.svg';
-export { ReactComponent as ClockIcon } from './clock.svg';
+export { ReactComponent as CheckIcon } from './check.svg';
 export { ReactComponent as ArrowExpandMoreIcon } from './arrow-expand-more.svg';
 export { ReactComponent as ArrowExpandLessIcon } from './arrow-expand-less.svg';
 export { ReactComponent as MegaphoneIcon } from './megaphone.svg';
