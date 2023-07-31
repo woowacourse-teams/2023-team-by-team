967ed35c0ed8a345d82d14725c56f2f70c45824d
Rulu <79538610+hafnium1923@users.noreply.github.com>
2023-07-31 17:22:08 +0900

    [FE] 팀피드 스레드 조회 api 연결 (#241)
    * feat: 스레드 조회 API 작성
    
    * feat: 스레드 조회 msw 작성
    
    * feat: 스레드 조회 API 작성
    
    * feat: 스레드 조회 msw 작성
    
    * refactor: handlers에 feed handler 추가
    
    * refactor: searchParams last-thread-id로 변경
    
    * fix: 스레드 조회 msw 19개씩 가져오는 문제 fix
    
    * feat: 스레드 조회 getNextPageParam 구현
    
    * feat: 스레드 마지막 관찰하는 intersection 훅 구현
    
    * feat: 스레드 리스트 구현
    
    * feat: 스레드 리스트 스토리 구현
    
    * refactor: ThreadData 네이밍 변경
    
    * refactor: 의존성 배열 추가
    
    * refactor: MutableRefObject사용해여 IntersectionObserver 구현

---
+++ frontend/src/hooks/queries/useFetchThreads.ts
@@ -0,0 +1,22 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
+import { fetchThreads } from '~/apis/feed';
+import { THREAD_SIZE } from '~/constants/feed';
+
+export const useFetchThreads = (teamPlaceId: number) => {
+  const {
+    data: threadPages,
+    hasNextPage,
+    fetchNextPage,
+  } = useInfiniteQuery(
+    ['threadData', teamPlaceId],
+    ({ pageParam = undefined }) => fetchThreads(teamPlaceId, pageParam),
+    {
+      getNextPageParam: (lastPage) => {
+        if (lastPage.threads.length !== THREAD_SIZE) return undefined;
+        return lastPage.threads[THREAD_SIZE - 1].id;
+      },
+    },
+  );
+
+  return { threadPages, hasNextPage, fetchNextPage };
+};
