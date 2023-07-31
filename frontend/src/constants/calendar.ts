46242162d4e18b09d17a3ae16a9142291858c80e
Suyoung Jung <devsuyoung@gmail.com>
2023-07-31 22:09:41 +0900

    feat: 시간대 선택을 위한 TIME_TABLE 상수 추가

--- frontend/src/constants/calendar.ts
+++ frontend/src/constants/calendar.ts
@@ -1,3 +1,5 @@
+import { arrayOf } from '~/utils/arrayOf';
+
 export const DAYS_OF_WEEK = ['일', '월', '화', '수', '목', '금', '토'] as const;
 
 export const ONE_DAY = 86_400_000;
@@ -6,6 +8,14 @@ export const CALENDAR = {
   ROW_SIZE: 6,
   COLUMN_SIZE: 7,
 } as const;
+
+export const TIME_TABLE = arrayOf(48).map((_, i) => {
+  const hour = Math.floor(i / 2);
+  const minute = i % 2 === 0 ? '00' : '30';
+  const isAM = hour < 12;
+
+  return `${isAM ? '오전' : '오후'} ${hour % 12 || 12}:${minute}`;
+});
 
 export const MODAL_OPEN_TYPE = {
   ADD: 'add',
