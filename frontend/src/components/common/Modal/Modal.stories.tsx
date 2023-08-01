6c4a7cc8f354164a4ce65d011857a502b5b1839a
Suyoung Jung <devsuyoung@gmail.com>
2023-08-01 14:59:03 +0900

    fix: Modal 스토리 lint error 수정

--- frontend/src/components/common/Modal/Modal.stories.tsx
+++ frontend/src/components/common/Modal/Modal.stories.tsx
@@ -14,7 +14,8 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  render: ({ children }) => {
+  render: () => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
     const { openModal } = useModal();
 
     return (
