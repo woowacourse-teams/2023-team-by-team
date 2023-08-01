708c038fa717675ed8bfa9f39bb34a0e3c223a8f
Suyoung Jung <devsuyoung@gmail.com>
2023-08-01 23:42:40 +0900

    feat: 일정 수정 종일/시간대 선택 기능 구현

--- frontend/src/hooks/schedule/useScheduleEditModal.ts
+++ frontend/src/hooks/schedule/useScheduleEditModal.ts
@@ -3,20 +3,32 @@ import { useState } from 'react';
 import { useModifySchedule } from '~/hooks/queries/useModifySchedule';
 import { useModal } from '~/hooks/useModal';
 import { isYYYYMMDDHHMM } from '~/types/typeGuard';
-import { formatISOString } from '~/utils/formatISOString';
 import type { Schedule } from '~/types/schedule';
 
 const useScheduleEditModal = (
   scheduleId: Schedule['id'],
   initialSchedule?: Schedule,
 ) => {
+  const [startDate, startTime] = initialSchedule?.startDateTime.split(' ') ?? [
+    '',
+  ];
+  const [endDate, endTime] = initialSchedule?.endDateTime.split(' ') ?? [''];
   const [schedule, setSchedule] = useState({
     title: initialSchedule?.title,
-    startDateTime: initialSchedule?.startDateTime,
-    endDateTime: initialSchedule?.endDateTime,
+    startDate,
+    endDate,
   });
+  const [times, setTimes] = useState({
+    startTime,
+    endTime: endTime === '23:59' ? '23:30' : endTime,
+  });
+  const [isAllDay, setIsAllDay] = useState(endTime === '23:59');
   const { closeModal } = useModal();
   const { mutateModifySchedule } = useModifySchedule(1, scheduleId);
+
+  const handleIsAllDayChange = () => {
+    setIsAllDay((prev) => !prev);
+  };
 
   const handleScheduleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
     const { name, value } = e.target;
@@ -24,20 +36,71 @@ const useScheduleEditModal = (
     setSchedule((prev) => ({ ...prev, [name]: value }));
   };
 
+  const handleStartTimeChange = (value: string) => {
+    if (!isValidEndTime(value, times['endTime'])) {
+      setTimes((prev) => {
+        return {
+          ...prev,
+          ['startTime']: value,
+          ['endTime']: value,
+        };
+      });
+
+      return;
+    }
+
+    setTimes((prev) => {
+      return {
+        ...prev,
+        ['startTime']: value,
+      };
+    });
+  };
+
+  const handleEndTimeChange = (value: string) => {
+    if (!isValidEndTime(times['startTime'], value)) {
+      setTimes((prev) => {
+        return {
+          ...prev,
+          ['endTime']: prev['startTime'],
+        };
+      });
+
+      return;
+    }
+
+    setTimes((prev) => {
+      return {
+        ...prev,
+        ['endTime']: value,
+      };
+    });
+  };
+
+  const isValidEndTime = (startTime: string, endTime: string) => {
+    const { startDate, endDate } = schedule;
+    const start = new Date(`${startDate} ${startTime}`);
+    const end = new Date(`${endDate} ${endTime}`);
+
+    return start < end;
+  };
+
   const handleScheduleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
 
-    const { title, startDateTime, endDateTime } = schedule;
-
+    const { title, startDate, endDate } = schedule;
+    const { startTime, endTime } = times;
     if (
       typeof title !== 'string' ||
-      typeof startDateTime !== 'string' ||
-      typeof endDateTime !== 'string'
+      typeof startDate !== 'string' ||
+      typeof endDate !== 'string'
     )
       return;
 
-    const formattedStartDateTime = formatISOString(startDateTime);
-    const formattedEndDateTime = formatISOString(endDateTime);
+    const formattedStartDateTime = `${startDate} ${
+      isAllDay ? '00:00' : startTime
+    }`;
+    const formattedEndDateTime = `${endDate} ${isAllDay ? '23:59' : endTime}`;
 
     if (
       !isYYYYMMDDHHMM(formattedStartDateTime) ||
@@ -60,10 +123,15 @@ const useScheduleEditModal = (
 
   return {
     schedule,
+    times,
+    isAllDay,
 
     handlers: {
       handleScheduleChange,
       handleScheduleSubmit,
+      handleStartTimeChange,
+      handleEndTimeChange,
+      handleIsAllDayChange,
     },
   };
 };
