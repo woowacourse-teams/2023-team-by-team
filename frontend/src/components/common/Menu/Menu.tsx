938c01b739e535abcbaa6469bce3efca76e10ee5
Suyoung Jung <devsuyoung@gmail.com>
2023-08-01 22:38:53 +0900

    refactor: Menu의 프로퍼티를 통해 각각의 부속 컴포넌트에 접근 가능하도록 수정

--- frontend/src/components/common/Menu/Menu.tsx
+++ frontend/src/components/common/Menu/Menu.tsx
@@ -1,6 +1,9 @@
+import type { PropsWithChildren } from 'react';
 import { MenuProvider } from '~/components/common/Menu/MenuContext';
 import * as S from './Menu.styled';
-import type { PropsWithChildren } from 'react';
+import MenuButton from '~/components/common/Menu/MenuButton/MenuButton';
+import MenuList from '~/components/common/Menu/MenuList/MenuList';
+import MenuItem from '~/components/common/Menu/MenuItem/MenuItem';
 
 const Menu = (props: PropsWithChildren) => {
   const { children } = props;
@@ -11,5 +14,9 @@ const Menu = (props: PropsWithChildren) => {
     </MenuProvider>
   );
 };
+
+Menu.Button = MenuButton;
+Menu.List = MenuList;
+Menu.Item = MenuItem;
 
 export default Menu;
