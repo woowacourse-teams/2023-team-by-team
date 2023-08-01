379919aad383354d5aa23c918dba890947489ebf
요술토끼 <curious.wzrabbit@gmail.com>
2023-08-01 14:25:33 +0900

    [FE] 공지 스레드 컴포넌트 구현 (#244)
    * feat: 스레드에 사용될 확장/축소 버튼 구현
    
    * test: 확장/축소 버튼에 대한 스토리북 작성
    
    * feat: 스레드의 확장/축소 및 확장 상태 토글링을 위한 커스텀 훅 구현
    
    * feat: 공지 스레드의 배경 이미지 업로드
    
    * feat: 공지 스레드 구현을 위한 svg 이미지들 업로드
    
    * feat: 공지 스레드의 작성자 정보, 작성 시간, 스레드 내용, 펼치기/접기 기능 구현
    
    * test: 공지 스레드에 대한 스토리북 작성
    
    * feat: 임시 스레드 태그 구현
    
    * test: 임시 스레드 태그에 대한 스토리북 작성
    
    * feat: 구현한 임시 태그를 공지 스레드 컴포넌트에 부착
    
    * feat: 임시 태그에 사용할 확성기 아이콘 업로드
    
    * chore: png 글로벌 타입 정의
    
    * chore: 확장 버튼 / 태그의 디렉토리 변경
    
    * feat: 작성 일자를 props로 넣을 수 있도록 변경
    
    * refactor: 스레드 태그의 이름명 변경
    
    * feat: 공지 태그에서 모아보기 페이지에 대응하는 작은 사이즈 추가 구현
    
    * test: NoticeTag 컴포넌트에 대해 스토리 개선
    
    * feat: 시계 아이콘 제거, 디바이더 추가
    
    * feat: 공지 스레드 컴포넌트에 모아보기 전용 사이즈를 선택할 수 있도록 개선
    
    * docs: 스토리북 공지 스레드 API문서에 컴포넌트 설명 추가
    
    * refactor: 불필요 속성 제거, 길이 및 여백 짝수로 변경
    
    * refactor: 중복되는 컨테이너 제거 및 리팩토링
    
    * refactor: 공지 스레드의 기본 높이에 해당하는 상수를 상수파일로 분리
    
    * refactor: NoticeTag의 스토리 타이틀 변경
    
    * refactor: children -> content prop으로 내용을 받기로 결정 후 반영
    
    * refactor: 불필요한 import 제거
    
    * refactor: 사실은 불필요하지 않았던 useRef 다시 추가
    
    * refactor: 공지 스레드 컴포넌트의 props에서 size에 해당하는 default value를 정의
    
    * chore: 공지 태그의 경로 수정
    
    * chore: 불필요 svg 제거
    
    * chore: 상수가 저장되는 파일을 thread -> feed 로 변경
    
    * chore: 컨플릭트 해소
    
    * refactor: 공지 스레드 본문의 폰트 크기 변경

--- frontend/src/assets/svg/index.ts
+++ frontend/src/assets/svg/index.ts
@@ -6,3 +6,6 @@ export { ReactComponent as EditIcon } from './edit.svg';
 export { ReactComponent as LogoIcon } from './logo.svg';
 export { ReactComponent as PlusIcon } from './plus.svg';
 export { ReactComponent as CheckIcon } from './check.svg';
+export { ReactComponent as ArrowExpandMoreIcon } from './arrow-expand-more.svg';
+export { ReactComponent as ArrowExpandLessIcon } from './arrow-expand-less.svg';
+export { ReactComponent as MegaphoneIcon } from './megaphone.svg';
