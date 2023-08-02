d264e4de452791b50febe80c23645d5f933b159a
SproutMj <mjk54565@gmail.com>
2023-08-02 11:03:47 +0900

    feat: 로그인 후 토큰을 헤더에 저장하고 "/"로 리다이렉트한다.
    Co-authored-by: pilyang <pilyang.dev@gmail.com>
    Co-authored-by: sh111-coder <ohk9134@naver.com>
    Co-authored-by: the9kim <the9rla@gmail.com>

--- backend/src/main/java/team/teamby/teambyteam/auth/oauth/presentation/AuthController.java
+++ backend/src/main/java/team/teamby/teambyteam/auth/oauth/presentation/AuthController.java
@@ -2,12 +2,12 @@ package team.teamby.teambyteam.auth.oauth.presentation;
 
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import team.teamby.teambyteam.auth.oauth.application.GoogleOAuthService;
-import team.teamby.teambyteam.auth.oauth.application.dto.TokenResponse;
 import team.teamby.teambyteam.auth.oauth.util.OAuthUriGenerator;
 
 import java.io.IOException;
@@ -17,6 +17,7 @@ import java.io.IOException;
 @RequestMapping("/api/auth")
 public class AuthController {
 
+    private static final String BEARER_TYPE = "Bearer ";
     private final OAuthUriGenerator oAuthUriGenerator;
     private final GoogleOAuthService googleOAuthService;
 
@@ -27,7 +28,8 @@ public class AuthController {
     }
 
     @GetMapping("/code/google")
-    public TokenResponse googleRedirect(@RequestParam final String code) {
-        return googleOAuthService.createToken(code);
+    public void googleRedirect(final HttpServletResponse response, @RequestParam final String code) throws IOException {
+        response.addHeader(HttpHeaders.AUTHORIZATION, BEARER_TYPE + googleOAuthService.createToken(code).accessToken());
+        response.sendRedirect("/");
     }
 }
