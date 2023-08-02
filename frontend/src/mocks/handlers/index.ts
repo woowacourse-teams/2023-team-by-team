89ba305333c5b4528cab18d2ce418980279ca64c
요술토끼 <curious.wzrabbit@gmail.com>
2023-08-02 15:38:51 +0900

    refactor: 불필요한 상대경로를 절대경로로 수정

--- frontend/src/mocks/handlers/index.ts
+++ frontend/src/mocks/handlers/index.ts
@@ -1,5 +1,5 @@
 import { calendarHandlers } from '~/mocks/handlers/calendar';
 import { feedHandlers } from '~/mocks/handlers/feed';
-import { teamHandlers } from './team';
+import { teamHandlers } from '~/mocks/handlers/team';
 
 export const handlers = [...calendarHandlers, ...feedHandlers, ...teamHandlers];
