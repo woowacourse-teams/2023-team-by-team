912d22df2b19df72f0a295e841417d6165001fb7
요술토끼 <curious.wzrabbit@gmail.com>
2023-08-02 13:54:07 +0900

    fix: 잘못된 키값 수정

--- frontend/src/mocks/handlers/team.ts
+++ frontend/src/mocks/handlers/team.ts
@@ -1,11 +1,9 @@
 import { rest } from 'msw';
-import { teamPlaces as teamPlaceData } from '~/mocks/fixtures/team';
-
-const teamPlaces = { teamPlaceData };
+import { teamPlaces } from '~/mocks/fixtures/team';
 
 export const teamHandlers = [
   // 팀플레이스 목록 조회
   rest.get('/api/me/team-places', async (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(teamPlaces));
+    return res(ctx.status(200), ctx.json({ teamPlaces }));
   }),
 ];
