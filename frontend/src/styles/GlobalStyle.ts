21c09a84904cd30c7599da6b27de8b2a5b50bc1f
요술토끼 <curious.wzrabbit@gmail.com>
2023-08-03 17:45:54 +0900

    feat: 접근성을 위한 hidden 클래스 추가

--- frontend/src/styles/GlobalStyle.ts
+++ frontend/src/styles/GlobalStyle.ts
@@ -53,6 +53,20 @@ const GlobalStyle = createGlobalStyle`
   button:disabled {
     cursor: not-allowed;
   }
+
+  *.hidden {
+    overflow: hidden;
+    white-space: nowrap;
+    clip: rect(1px, 1px, 1px, 1px);
+    -webkit-clip-path: inset(50%);
+    clip-path: inset(50%);
+    position: absolute;
+    width: 1px;
+    height: 1px;
+    margin: 0;
+    padding: 0;
+    border: 0;
+  }
 `;
 
 export default GlobalStyle;
