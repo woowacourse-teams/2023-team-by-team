1cf289d54881109838bd94af4b45d99eaf47dfc3
Suyoung Jung <devsuyoung@gmail.com>
2023-08-03 11:14:37 +0900

    refactor: 공지 스레드 prop을 API 명세와 통일

--- frontend/src/components/NoticeThread/NoticeThread.tsx
+++ frontend/src/components/NoticeThread/NoticeThread.tsx
@@ -9,15 +9,21 @@ import { formatWriteTime } from '~/utils/formatWriteTime';
 import type { NoticeThreadSize } from '~/types/size';
 
 interface NoticeThreadProps {
-  author: string;
-  profileImageSrc: string;
+  authorName: string;
+  profileImageUrl: string;
   createdAt: YYYYMMDDHHMM;
   content: string;
   size?: NoticeThreadSize;
 }
 
 const NoticeThread = (props: NoticeThreadProps) => {
-  const { author, profileImageSrc, createdAt, content, size = 'md' } = props;
+  const {
+    authorName,
+    profileImageUrl,
+    createdAt,
+    content,
+    size = 'md',
+  } = props;
   const threadRef = useRef<HTMLDivElement>(null);
   const contentRef = useRef<HTMLDivElement>(null);
   const { shouldShowExpandButton, isExpanded, toggleExpanded, resultHeight } =
@@ -26,16 +32,16 @@ const NoticeThread = (props: NoticeThreadProps) => {
   return (
     <S.ThreadContainer>
       <S.Thread
-        aria-label={`${author}의 공지`}
+        aria-label={`${authorName}의 공지`}
         ref={threadRef}
         height={resultHeight}
         isExpanded={isExpanded}
       >
         {size === 'md' && (
           <S.ThreadHeader>
-            <S.ProfileImage src={profileImageSrc} rel="프로필 사진" />
+            <S.ProfileImage src={profileImageUrl} rel="프로필 사진" />
             <Text size="lg" weight="bold">
-              {author}
+              {authorName}
             </Text>
             <S.Divider />
             <Text size="lg">{formatWriteTime(createdAt)}</Text>
