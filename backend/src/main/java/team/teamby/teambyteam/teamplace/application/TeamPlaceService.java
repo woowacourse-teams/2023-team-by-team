ccbef4db53ee1266bf58e70aab57732f2c7db046
pilyang <pilyang.dev@gmail.com>
2023-08-07 23:04:40 +0900

    feat: 팀플래이스 생성 서비스 구현

---
+++ backend/src/main/java/team/teamby/teambyteam/teamplace/application/TeamPlaceService.java
@@ -0,0 +1,40 @@
+package team.teamby.teambyteam.teamplace.application;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import team.teamby.teambyteam.member.configuration.dto.MemberEmailDto;
+import team.teamby.teambyteam.member.domain.Member;
+import team.teamby.teambyteam.member.domain.MemberRepository;
+import team.teamby.teambyteam.member.domain.MemberTeamPlace;
+import team.teamby.teambyteam.member.domain.MemberTeamPlaceRepository;
+import team.teamby.teambyteam.member.domain.vo.Email;
+import team.teamby.teambyteam.member.exception.MemberException;
+import team.teamby.teambyteam.teamplace.application.dto.TeamPlaceCreateResponse;
+import team.teamby.teambyteam.teamplace.domain.TeamPlace;
+import team.teamby.teambyteam.teamplace.domain.TeamPlaceRepository;
+import team.teamby.teambyteam.teamplace.domain.vo.Name;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class TeamPlaceService {
+
+    private final MemberRepository memberRepository;
+    private final TeamPlaceRepository teamPlaceRepository;
+    private final MemberTeamPlaceRepository memberTeamPlaceRepository;
+
+    public TeamPlaceCreateResponse create(final MemberEmailDto memberEmailDto, final String teamPlaceName) {
+
+        final Member member = memberRepository.findByEmail(new Email(memberEmailDto.email()))
+                .orElseThrow(MemberException.MemberNotFoundException::new);
+
+        final TeamPlace createdTeamPlace = teamPlaceRepository.save(new TeamPlace(new Name(teamPlaceName)));
+        final MemberTeamPlace participatedMemberTeamPlace = member.participate(createdTeamPlace);
+
+        memberTeamPlaceRepository.save(participatedMemberTeamPlace);
+
+        return new TeamPlaceCreateResponse(createdTeamPlace.getId());
+    }
+
+}
