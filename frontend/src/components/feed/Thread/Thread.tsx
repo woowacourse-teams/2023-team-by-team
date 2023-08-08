d05829cd1fac818e23153f62ce300c5d4cc54001
hafnium1923 <hafnium1923@gmail.com>
2023-08-08 11:12:10 +0900

    refactor: 전체 파일 구조 변경

---
+++ frontend/src/components/feed/Thread/Thread.tsx
@@ -0,0 +1,46 @@
+import type { YYYYMMDDHHMM } from '~/types/schedule';
+import * as S from './Thread.styled';
+import Text from '~/components/common/Text/Text';
+import { formatWriteTime } from '~/utils/formatWriteTime';
+import type { ThreadSize } from '~/types/size';
+
+interface ThreadProps {
+  size?: ThreadSize;
+  authorName: string;
+  profileImageUrl: string;
+  createdAt: YYYYMMDDHHMM;
+  content: string;
+}
+
+const Thread = (props: ThreadProps) => {
+  const {
+    size = 'md',
+    authorName,
+    profileImageUrl,
+    createdAt,
+    content,
+  } = props;
+  const createdTime = formatWriteTime(createdAt);
+
+  return (
+    <S.Container>
+      {size === 'md' && (
+        <S.ThreadHeader>
+          <S.Author>
+            <S.ProfileImg src={profileImageUrl} alt={authorName} />
+            <Text size="lg" weight="bold">
+              {authorName}
+            </Text>
+          </S.Author>
+          <S.Divider />
+          <Text size="lg">{createdTime}</Text>
+        </S.ThreadHeader>
+      )}
+      <Text size="xl" css={S.contentField}>
+        {content}
+      </Text>
+    </S.Container>
+  );
+};
+
+export default Thread;
---
+++ frontend/src/components/feed/ThreadAddBottomSheet/ThreadAddBottomSheet.tsx
@@ -0,0 +1,61 @@
+import Text from '~/components/common/Text/Text';
+import * as S from './ThreadAddBottomSheet.styled';
+import Button from '~/components/common/Button/Button';
+import Checkbox from '~/components/common/Checkbox/Checkbox';
+import { useThreadAddBottomSheet } from '~/hooks/thread/useThreadAddBottomSheet';
+
+const ThreadAddBottomSheet = () => {
+  const {
+    content,
+    isNotice,
+    isClosing,
+
+    handlers: {
+      handleClose,
+      handleContentChange,
+      handleIsNoticeChange,
+      handleCancelButtonClick,
+      handleSubmit,
+    },
+  } = useThreadAddBottomSheet();
+
+  return (
+    <>
+      <S.Backdrop onClick={handleClose} />
+      <form onSubmit={handleSubmit}>
+        <S.Container isClosing={isClosing}>
+          <S.TitleWrapper>
+            <Text as="h3" css={S.title}>
+              팀 피드 작성하기
+            </Text>
+            <S.NoticeWrapper>
+              <Text as="span" css={S.notice}>
+                공지로 등록
+              </Text>
+              <Checkbox isChecked={isNotice} onChange={handleIsNoticeChange} />
+            </S.NoticeWrapper>
+          </S.TitleWrapper>
+          <S.Textarea
+            placeholder="내용을 입력해주세요."
+            maxLength={1500}
+            value={content}
+            onChange={handleContentChange}
+            required
+          />
+          <S.ButtonWrapper>
+            <Button
+              type="button"
+              css={S.cancelButton}
+              onClick={handleCancelButtonClick}
+            >
+              취소
+            </Button>
+            <Button css={S.submitButton}>등록</Button>
+          </S.ButtonWrapper>
+        </S.Container>
+      </form>
+    </>
+  );
+};
+
+export default ThreadAddBottomSheet;
