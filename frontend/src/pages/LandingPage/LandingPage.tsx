54d8cd6f6952c705e2703b7b01d8d2b8a695da6e
Suyoung Jung <devsuyoung@gmail.com>
2023-08-08 14:07:23 +0900

    feat: 구글 로그인 테스트 버튼 구현

--- frontend/src/pages/LandingPage/LandingPage.tsx
+++ frontend/src/pages/LandingPage/LandingPage.tsx
@@ -1,18 +1,27 @@
 import { useNavigate } from 'react-router-dom';
 import Button from '~/components/common/Button/Button';
 import { PATH_NAME } from '~/constants/routes';
+import * as S from './LandingPage.styled';
+import { sendGoogleLogin } from '~/apis/auth';
 
 const LandingPage = () => {
   const navigate = useNavigate();
+
+  const handleGoogleLogin = async () => {
+    const response = await sendGoogleLogin();
+
+    console.log(response);
+  };
+
   const handleNavigateClick = () => {
     navigate(PATH_NAME.TEAM_SELECT);
   };
 
   return (
-    <>
-      <div>랜딩페이지입니다.</div>
+    <S.Container>
+      <Button onClick={handleGoogleLogin}>구글 로그인</Button>
       <Button onClick={handleNavigateClick}>팀바팀 이동하기</Button>
-    </>
+    </S.Container>
   );
 };
 
