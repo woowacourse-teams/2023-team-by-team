1db8ddb0123e9a778c25cf38c9e775ed7bb88e21
pilyang <pilyang.dev@gmail.com>
2023-08-08 11:10:40 +0900

    refactor: 사용하지 않는 dto 제거

--- backend/src/main/java/team/teamby/teambyteam/member/domain/MemberIdAndDisplayNameOnly.java
+++
@@ -1,9 +0,0 @@
-package team.teamby.teambyteam.member.domain;
-
-import team.teamby.teambyteam.member.domain.vo.DisplayMemberName;
-
-public record MemberIdAndDisplayNameOnly(
-        Long id,
-        DisplayMemberName displayMemberName
-) {
-}
