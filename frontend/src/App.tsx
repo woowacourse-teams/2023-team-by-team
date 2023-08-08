453f0d6c12f0919017066f19c22687c6c12afd32
Rulu <79538610+hafnium1923@users.noreply.github.com>
2023-08-08 15:37:54 +0900

    feat: 소셜 로그인 accesstToken 로컬에 저장하는 기능 (#383)
    Co-authored-by: 요술토끼 <curious.wzrabbit@gmail.com>
    Co-authored-by: Suyoung <devsuyoung@gmail.com>

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 import { PATH_NAME } from '~/constants/routes';
 import LandingPage from '~/pages/LandingPage/LandingPage';
+import LoginPage from '~/pages/LoginPage/LoginPage';
 import PageTemplate from '~/pages/PageTemplate/PageTemplate';
 import TeamCalendarPage from '~/pages/TeamCalendarPage/TeamCalendarPage';
 import TeamFeedPage from '~/pages/TeamFeedPage/TeamFeedPage';
@@ -10,6 +11,7 @@ const App = () => {
   return (
     <Routes>
       <Route path={PATH_NAME.LANDING} element={<LandingPage />} />
+      <Route path={PATH_NAME.LOGIN} element={<LoginPage />} />
       <Route element={<PageTemplate />}>
         <Route path={PATH_NAME.TEAM_SELECT} element={<TeamSelectPage />} />
         <Route path={PATH_NAME.TEAM_CALENDAR} element={<TeamCalendarPage />} />
