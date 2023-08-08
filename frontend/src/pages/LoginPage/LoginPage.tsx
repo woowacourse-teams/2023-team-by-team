f5755cb2aa116d558cbfaeeade0ca629e5810038
hafnium1923 <hafnium1923@gmail.com>
2023-08-08 15:44:34 +0900

    feat: 로그인 성공시 페이지 이동
    Co-Authored-By: 요술토끼 <curious.wzrabbit@gmail.com>
    Co-Authored-By: Suyoung <devsuyoung@gmail.com>

--- frontend/src/pages/LoginPage/LoginPage.tsx
+++ frontend/src/pages/LoginPage/LoginPage.tsx
@@ -1,19 +1,23 @@
 import { useSearchParams, useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+import { PATH_NAME } from '~/constants/routes';
 
 const LoginPage = () => {
   const navigate = useNavigate();
   const [params] = useSearchParams();
   const accessToken = params.get('accessToken');
 
-  if (!accessToken) {
-    alert('로그인이 필요합니다.');
-    navigate('/');
+  useEffect(() => {
+    if (!accessToken) {
+      alert('로그인이 필요합니다.');
+      navigate(PATH_NAME.LANDING);
 
-    return <></>;
-  }
+      return;
+    }
 
-  localStorage.setItem('accessToken', accessToken);
-  navigate('/team');
+    localStorage.setItem('accessToken', accessToken);
+    navigate(PATH_NAME.LOGIN);
+  }, []);
 
   return <></>;
 };
