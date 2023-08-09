a57cca2cc575d63959a1e59702d6f8ad447e62bf
hafnium1923 <hafnium1923@gmail.com>
2023-08-09 11:55:13 +0900

    feat: 로그아웃 기능 구현

--- frontend/src/components/common/Header/Header.tsx
+++ frontend/src/components/common/Header/Header.tsx
@@ -1,11 +1,12 @@
 import { useCallback, useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { LogoIcon } from '~/assets/svg';
+import { Link, useNavigate } from 'react-router-dom';
+import { LogoIcon, LogoutIcon } from '~/assets/svg';
 import * as S from './Header.styled';
 import TeamBadge from '~/components/team/TeamBadge/TeamBadge';
 import { useTeamPlace } from '~/hooks/useTeamPlace';
 import TeamPlaceMenu from '~/components/team/TeamPlaceMenu/TeamPlaceMenu';
 import { PATH_NAME } from '~/constants/routes';
+import Button from '~/components/common/Button/Button';
 
 const Header = () => {
   const { teamPlaces, changeTeamPlace, teamPlaceColor, displayName } =
@@ -36,18 +37,36 @@ const Header = () => {
     handleTeamNameChange(displayName);
   }, [handleTeamNameChange, displayName]);
 
+  const handleLogoutClick = () => {
+    const isLogout = confirm('로그아웃 하시겠습니까?');
+
+    if (!isLogout) return;
+
+    alert('로그아웃 되었습니다.');
+    localStorage.removeItem('accessToken');
+    navigate(PATH_NAME.LANDING);
+  };
+
   return (
     <S.Container tabIndex={0}>
-      <LogoIcon />
-      <div>
-        <TeamBadge teamPlaceColor={teamPlaceColor} />
-        <S.TeamNameWrapper>
-          <TeamPlaceMenu
-            displayValue={teamName}
-            onClickMenu={handleTeamNameChange}
-          />
-        </S.TeamNameWrapper>
-      </div>
+      <S.InnerContainer>
+        <Link to={PATH_NAME.TEAM_CALENDAR} aria-label="홈 페이지로 가기 버튼">
+          <LogoIcon />
+        </Link>
+        <div>
+          <TeamBadge teamPlaceColor={teamPlaceColor} />
+          <S.TeamNameWrapper>
+            <TeamPlaceMenu
+              displayValue={teamName}
+              onClickMenu={handleTeamNameChange}
+            />
+          </S.TeamNameWrapper>
+        </div>
+      </S.InnerContainer>
+      <Button variant="normal" css={S.logoutButton} onClick={handleLogoutClick}>
+        로그아웃
+        <LogoutIcon />
+      </Button>
     </S.Container>
   );
 };
