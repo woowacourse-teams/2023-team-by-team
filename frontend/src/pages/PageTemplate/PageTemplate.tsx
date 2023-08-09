ac65ec1885eca7ffe4c6af65b687f4fc91031efc
Suyoung Jung <devsuyoung@gmail.com>
2023-08-09 17:24:19 +0900

    refactor: ModalProvider를 전역에서 제거하고 각기 필요한 컴포넌트를 감싸도록 수정
    (cherry picked from commit dbc0c5e0e1dc0a757413884ad1f32895ae68c252)
    
    # Conflicts:
    #	frontend/src/pages/PageTemplate/PageTemplate.tsx

--- frontend/src/pages/PageTemplate/PageTemplate.tsx
+++ frontend/src/pages/PageTemplate/PageTemplate.tsx
@@ -3,14 +3,20 @@ import NavigationBar from '~/components/common/NavigationBar/NavigationBar';
 import SideBar from '~/components/common/SideBar/SideBar';
 import { Outlet } from 'react-router-dom';
 import Header from '~/components/common/Header/Header';
+import { TeamPlaceProvider } from '~/contexts/TeamPlaceContext';
+import { ModalProvider } from '~/components/common/Modal/ModalContext';
 
 const PageTemplate = () => {
   return (
     <>
       <Header />
       <S.Container>
-        <NavigationBar />
-        <Outlet />
+        <ModalProvider>
+          <NavigationBar />
+        </ModalProvider>
+        <ModalProvider>
+          <Outlet />
+        </ModalProvider>
         <SideBar />
       </S.Container>
     </>
