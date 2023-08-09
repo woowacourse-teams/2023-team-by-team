d69fafa352721043e1751c1c4ce059cf457f8f37
sh111-coder <ohk9134@naver.com>
2023-08-09 14:55:35 +0900

    refactor: JwtTokenExtractor, JwtTokenProvider 메소드 네이밍 변경

--- backend/src/main/java/team/teamby/teambyteam/auth/jwt/JwtTokenExtractor.java
+++ backend/src/main/java/team/teamby/teambyteam/auth/jwt/JwtTokenExtractor.java
@@ -11,10 +11,18 @@ public class JwtTokenExtractor {
 
     private static final String PREFIX_BEARER = "Bearer ";
 
-    public String extractToken(final HttpServletRequest request) {
-        final String bearerToken = request.getHeader(HttpHeaders.AUTHORIZATION);
-        if (StringUtils.hasText(bearerToken) && bearerToken.startsWith(PREFIX_BEARER)) {
-            return bearerToken.substring(PREFIX_BEARER.length());
+    public String extractAccessToken(final HttpServletRequest request) {
+        final String accessToken = request.getHeader(ACCESS_TOKEN_HEADER);
+        if (StringUtils.hasText(accessToken) && accessToken.startsWith(PREFIX_BEARER)) {
+            return accessToken.substring(PREFIX_BEARER.length());
+        }
+        throw new AuthenticationException.FailAuthenticationException();
+    }
+
+    public String extractRefreshToken(final HttpServletRequest request) {
+        final String refreshToken = request.getHeader(REFRESH_TOKEN_HEADER);
+        if (StringUtils.hasText(refreshToken) && refreshToken.startsWith(PREFIX_BEARER)) {
+            return refreshToken.substring(PREFIX_BEARER.length());
         }
         throw new AuthenticationException.FailAuthenticationException();
     }
