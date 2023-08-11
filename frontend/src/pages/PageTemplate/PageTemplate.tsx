ae04cce7b10a085172880ed4a47a99e439479259
Suyoung Jung <devsuyoung@gmail.com>
2023-08-11 10:04:11 +0900

    refactor: 불필요한 import문 제거

--- frontend/src/pages/PageTemplate/PageTemplate.tsx
+++ frontend/src/pages/PageTemplate/PageTemplate.tsx
@@ -3,7 +3,6 @@ import NavigationBar from '~/components/common/NavigationBar/NavigationBar';
 import SideBar from '~/components/common/SideBar/SideBar';
 import { Outlet } from 'react-router-dom';
 import Header from '~/components/common/Header/Header';
-import { TeamPlaceProvider } from '~/contexts/TeamPlaceContext';
 import { ModalProvider } from '~/components/common/Modal/ModalContext';
 
 const PageTemplate = () => {
