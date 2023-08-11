a349a71a7591296e2f75bf182cdc7950d665ac86
요술토끼 <curious.wzrabbit@gmail.com>
2023-08-11 10:11:31 +0900

    feat: 랜딩 페이지 구현

--- frontend/src/pages/LandingPage/LandingPage.styled.ts
+++ frontend/src/pages/LandingPage/LandingPage.styled.ts
@@ -1,12 +1,103 @@
-import { styled } from 'styled-components';
+import { styled, css } from 'styled-components';
+import { Link } from 'react-router-dom';
 
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
-  justify-content: center;
 
+  width: 100vw;
   height: 100vh;
+`;
 
-  row-gap: 20px;
+export const MainContainer = styled.div`
+  display: flex;
+  justify-content: right;
+  align-items: center;
+  flex-grow: 1;
+`;
+
+export const Main = styled.main`
+  display: flex;
+  justify-content: right;
+  flex-direction: column;
+
+  width: 750px;
+  padding-right: 50px;
+
+  & h2,
+  & p {
+    text-align: right;
+  }
+`;
+
+export const Header = styled.header`
+  height: 64px;
+  padding: 8px;
+`;
+
+export const LandingPageLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  column-gap: 20px;
+`;
+
+export const GoogleLoginButtonWrapper = styled.div`
+  &:hover button {
+    box-shadow: 0 18px 20px ${({ theme }) => theme.color.GRAY300};
+
+    transform: translateY(-6px);
+  }
+`;
+
+export const GoogleLogo = styled.img`
+  width: 26px;
+`;
+
+export const headerTitle = css`
+  font-size: 32px;
+  font-weight: 800;
+  color: ${({ theme }) => theme.color.PRIMARY};
+  text-align: right;
+`;
+
+export const mainPrefix = css`
+  font-size: 60px;
+  font-weight: 900;
+  color: ${({ theme }) => theme.color.BLUE700};
+`;
+
+export const mainTitle = css`
+  margin-bottom: 30px;
+
+  font-size: 76px;
+  font-weight: 900;
+  color: ${({ theme }) => theme.color.PURPLE};
+`;
+
+export const mainLore = css`
+  font-size: 28px;
+  font-weight: 600;
+  color: ${({ theme }) => theme.color.BLACK};
+`;
+
+export const googleLoginButton = css`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  width: 300px;
+  height: 60px;
+  margin: 80px 0 0 auto;
+
+  border: 1px solid ${({ theme }) => theme.color.GRAY300};
+  border-radius: 12px;
+
+  box-shadow: 0 12px 20px ${({ theme }) => theme.color.GRAY300};
+  transition: 0.2s;
+`;
+
+export const googleLoginText = css`
+  font-size: 18px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.color.GRAY700};
 `;
--- frontend/src/pages/LandingPage/LandingPage.tsx
+++ frontend/src/pages/LandingPage/LandingPage.tsx
@@ -1,9 +1,13 @@
+import { useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import Button from '~/components/common/Button/Button';
+import { fetchGoogleLogin } from '~/apis/auth';
 import { PATH_NAME } from '~/constants/routes';
 import * as S from './LandingPage.styled';
-import { fetchGoogleLogin } from '~/apis/auth';
-import { useEffect } from 'react';
+import Button from '~/components/common/Button/Button';
+import IntroCardPile from '~/components/landing/IntroCardPile/IntroCardPile';
+import Text from '~/components/common/Text/Text';
+import { LogoIcon } from '~/assets/svg';
+import { googleLogo } from '~/assets/png';
 
 const LandingPage = () => {
   const navigate = useNavigate();
@@ -15,21 +19,46 @@ const LandingPage = () => {
     window.location.href = googleLoginUrl;
   };
 
-  const handleNavigateClick = () => {
-    navigate(PATH_NAME.TEAM_SELECT);
-  };
-
   useEffect(() => {
     if (accessToken) {
       navigate(PATH_NAME.TEAM_SELECT);
       return;
     }
-  }, []);
+  }, [accessToken, navigate]);
 
   return (
     <S.Container>
-      <Button onClick={handleGoogleLogin}>구글 로그인</Button>
-      <Button onClick={handleNavigateClick}>팀바팀 이동하기</Button>
+      <S.Header>
+        <S.LandingPageLink to={PATH_NAME.LANDING}>
+          <LogoIcon />
+          <Text as="h1" css={S.headerTitle}>
+            팀바팀
+          </Text>
+        </S.LandingPageLink>
+      </S.Header>
+      <S.MainContainer>
+        <S.Main>
+          <Text as="h2" css={S.mainPrefix}>
+            쉽고 간단한 팀플 플랫폼,
+          </Text>
+          <Text as="h2" css={S.mainTitle}>
+            팀바팀
+          </Text>
+          <Text css={S.mainLore}>팀 프로젝트의 모든 것을</Text>
+          <Text css={S.mainLore}>팀바팀으로 관리해보세요.</Text>
+          <S.GoogleLoginButtonWrapper>
+            <Button
+              variant="plain"
+              css={S.googleLoginButton}
+              onClick={handleGoogleLogin}
+            >
+              <S.GoogleLogo src={googleLogo} alt="구글 아이콘" />
+              <Text css={S.googleLoginText}>Google 계정으로 로그인하기</Text>
+            </Button>
+          </S.GoogleLoginButtonWrapper>
+        </S.Main>
+      </S.MainContainer>
+      <IntroCardPile />
     </S.Container>
   );
 };
