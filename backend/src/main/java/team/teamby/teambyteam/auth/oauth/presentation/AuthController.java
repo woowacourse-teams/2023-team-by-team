1734729dae5c10ca63204860ea281f811e0663f3
pilyang <pilyang.dev@gmail.com>
2023-08-12 21:47:32 +0900

    fix: 구글 토큰 중복 요청 문제 해결

--- backend/src/main/java/team/teamby/teambyteam/auth/oauth/presentation/AuthController.java
+++ backend/src/main/java/team/teamby/teambyteam/auth/oauth/presentation/AuthController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import team.teamby.teambyteam.auth.oauth.application.GoogleOAuthService;
+import team.teamby.teambyteam.auth.oauth.application.dto.TokenResponse;
 import team.teamby.teambyteam.auth.oauth.presentation.dto.GoogleLoginUrlResponse;
 import team.teamby.teambyteam.auth.oauth.util.OAuthUriGenerator;
 
@@ -34,8 +35,9 @@ public class AuthController {
     @GetMapping("/code/google")
     public void googleRedirect(final HttpServletResponse response, @RequestParam final String code) throws IOException {
         final String baseUrl = "/login";
-        final String accessToken = googleOAuthService.createToken(code).accessToken();
-        final String refreshToken = googleOAuthService.createToken(code).refreshToken();
+        final TokenResponse tokens = googleOAuthService.createToken(code);
+        final String accessToken = tokens.accessToken();
+        final String refreshToken = tokens.refreshToken();
 
         String url = generateUrl(baseUrl, accessToken, refreshToken);
         response.sendRedirect(url);
