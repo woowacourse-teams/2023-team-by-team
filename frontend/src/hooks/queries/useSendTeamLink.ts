cd756e2fabdcf7194431862bd416ac7b3ac930ac
요술토끼 <curious.wzrabbit@gmail.com>
2023-08-12 22:57:22 +0900

    feat: 새로운 링크가 추가되면 자동으로 다시 갱신하도록 개선

--- frontend/src/hooks/queries/useSendTeamLink.ts
+++ frontend/src/hooks/queries/useSendTeamLink.ts
@@ -1,10 +1,14 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { sendTeamLink } from '~/apis/link';
 import type { TeamLinkWithoutInfo } from '~/types/link';
 
 export const useSendTeamLink = (teamPlaceId: number) => {
-  const { mutate } = useMutation((body: TeamLinkWithoutInfo) =>
-    sendTeamLink(teamPlaceId, body),
+  const queryClient = useQueryClient();
+  const { mutate } = useMutation(
+    (body: TeamLinkWithoutInfo) => sendTeamLink(teamPlaceId, body),
+    {
+      onSuccess: () => queryClient.invalidateQueries(['teamLinks']),
+    },
   );
 
   return { mutateSendTeamLink: mutate };
