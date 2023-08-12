849cb4bbb32c1e3ac29340f86091084851ece146
요술토끼 <curious.wzrabbit@gmail.com>
2023-08-13 02:34:26 +0900

    feat: 링크가 하나도 없을 경우 보여줄 대체 placeholder 구현

---
+++ frontend/src/components/link/EmptyLinkPlaceholder/EmptyLinkPlaceholder.styled.ts
@@ -0,0 +1,55 @@
+import { styled, css, keyframes } from 'styled-components';
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  height: 100%;
+`;
+
+export const PlaceholderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 18px;
+
+  width: 100%;
+  height: 100%;
+
+  opacity: 0;
+
+  animation: ${fadeIn} 0.4s forwards;
+`;
+
+export const LinkEmptyImage = styled.img`
+  width: 120px;
+  height: 120px;
+  margin-bottom: 10px;
+`;
+
+export const placeholderButton = css`
+  width: 380px;
+  height: 220px;
+  padding: 0;
+`;
+
+export const titleText = css`
+  font-size: 28px;
+  color: ${({ theme }) => theme.color.GRAY400};
+`;
+
+export const clickToAddText = css`
+  font-size: 20px;
+  color: ${({ theme }) => theme.color.GRAY500};
+`;
---
+++ frontend/src/components/link/EmptyLinkPlaceholder/EmptyLinkPlaceholder.tsx
@@ -0,0 +1,30 @@
+import * as S from './EmptyLinkPlaceholder.styled';
+import Button from '~/components/common/Button/Button';
+import Text from '~/components/common/Text/Text';
+import { linkEmptyImage } from '~/assets/png';
+
+interface EmptyLinkPlaceholderProps {
+  onClick: () => void;
+}
+
+const EmptyLinkPlaceholder = (props: EmptyLinkPlaceholderProps) => {
+  const { onClick } = props;
+
+  return (
+    <S.Container>
+      <Button variant="plain" onClick={onClick} css={S.placeholderButton}>
+        <S.PlaceholderContainer>
+          <S.LinkEmptyImage src={linkEmptyImage} />
+          <Text weight="bold" css={S.titleText}>
+            등록된 링크가 없어요
+          </Text>
+          <Text weight="semiBold" css={S.clickToAddText}>
+            여기를 클릭해 새로운 링크를 등록해보세요
+          </Text>
+        </S.PlaceholderContainer>
+      </Button>
+    </S.Container>
+  );
+};
+
+export default EmptyLinkPlaceholder;
