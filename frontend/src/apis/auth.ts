c4a085d81836b2f5d4c660cf99f18128ed031b41
Rulu <79538610+hafnium1923@users.noreply.github.com>
2023-08-12 23:02:18 +0900

    [FE] 토큰 만료시 refershToken으로 재발급 요청 (#466)
    * feat: 토큰 만료시 refershToken으로 재발급 요청
    
    * refactor: 토큰 재요청 fetch 함수 분리 및 queryclient 기본값으로

--- frontend/src/apis/auth.ts
+++ frontend/src/apis/auth.ts
@@ -1,5 +1,24 @@
 import { http } from '~/apis/http';
+import { LOCAL_STORAGE_KEY } from '~/constants/localStorage';
 
 export const fetchGoogleLogin = () => {
   return http.get<{ googleLoginUrl: string }>(`/api/auth/oauth/google/login`);
+};
+
+export const sendTokenReissue = async () => {
+  const response = await fetch('/api/token/reissue', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization-Refresh': `Bearer ${localStorage.getItem(
+        LOCAL_STORAGE_KEY.REFRESH_TOKEN,
+      )}`,
+    },
+  });
+
+  if (!response.ok) {
+    throw new Error('네트워크 통신 중 에러가 발생했습니다.');
+  }
+
+  return response;
 };
