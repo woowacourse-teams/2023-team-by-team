4ea1804fef920313c88be564379b9dd76ed502c0
요술토끼 <curious.wzrabbit@gmail.com>
2023-08-14 18:06:33 +0900

    feat: 모아보기 페이지 구현 및 라우팅 설정

---
+++ frontend/src/pages/TeamOverviewPage/TeamOverviewPage.tsx
@@ -0,0 +1,29 @@
+import * as S from './TeamOverviewPage.styled';
+import TeamCalendarPage from '../TeamCalendarPage/TeamCalendarPage';
+import TeamLinkPage from '../TeamLinkPage/TeamLinkPage';
+import TeamFeedPage from '../TeamFeedPage/TeamFeedPage';
+import { ModalProvider } from '~/components/common/Modal/ModalContext';
+
+const TeamOverviewPage = () => {
+  return (
+    <S.Container>
+      <S.TeamCalendarSection>
+        <ModalProvider>
+          <TeamCalendarPage />
+        </ModalProvider>
+      </S.TeamCalendarSection>
+      <S.TeamLinkSection>
+        <ModalProvider>
+          <TeamLinkPage />
+        </ModalProvider>
+      </S.TeamLinkSection>
+      <S.TeamFeedSection>
+        <ModalProvider>
+          <TeamFeedPage size="sm" />
+        </ModalProvider>
+      </S.TeamFeedSection>
+    </S.Container>
+  );
+};
+
+export default TeamOverviewPage;
