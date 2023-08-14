a5adff2def55d1ec6bd8d86da3fdcccbaa0914e5
SproutMj <mjk54565@gmail.com>
2023-08-14 16:43:21 +0900

    refactor: 등록 알림에서 링크를 제거

--- backend/src/main/java/team/teamby/teambyteam/feed/domain/notification/sharedlinknotification/SharedLinkNotificationContentGenerator.java
+++ backend/src/main/java/team/teamby/teambyteam/feed/domain/notification/sharedlinknotification/SharedLinkNotificationContentGenerator.java
@@ -7,9 +7,8 @@ public class SharedLinkNotificationContentGenerator {
 
     protected static Content generateSharedLinkCreateContent(final SharedLinkEvent sharedLinkEvent) {
         final String title = sharedLinkEvent.getTitle().getValue();
-        final String url = sharedLinkEvent.getSharedURL().getValue();
 
-        final String content = String.format("'%s'[%s] 공유링크가 등록되었습니다.", title, url);
+        final String content = String.format("'%s' 공유 링크가 등록되었습니다.", title);
 
         return new Content(content);
     }
