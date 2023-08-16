de454776f49356668325f58d677ce579da2e0868
Suyoung <jsy19970512@naver.com>
2023-08-16 14:44:32 +0900

    [FE] 알림 목록 모달 컴포넌트 구현 (#492)
    * refactor: 팀 정보 보기 아이콘 수정
    
    * feat: 종 모양 아이콘 추가
    
    * feat: Notification을 무한 스크롤 방식으로 fetch하는 커스텀 훅 작성
    
    * refactor: 헤더 스타일 수정
    
    * feat: NotificationListModal 컴포넌트, 스토리 작성
    
    * refactor: useFetchNotifications 훅에 enabled 옵션 추가
    
    * refactor: css 속성 순서 수정
    
    * refactor: useFetchNotifications 쿼리 옵션 위치 수정
    
    * refactor: fetchNextPage에 유효한 teamPlaceId 판별 조건문 추가

---
+++ frontend/src/components/feed/NotificationListModal/NotificationListModal.styled.ts
@@ -0,0 +1,64 @@
+import { css, styled } from 'styled-components';
+
+export const Backdrop = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  width: 100%;
+  height: 100%;
+`;
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+  top: 50px;
+  right: 20px;
+
+  width: 340px;
+  max-height: 400px;
+  padding: 20px;
+
+  border-radius: 8px;
+  box-shadow:
+    0 0 1px #1b1d1f33,
+    0 15px 25px #1b1d1f33,
+    0 5px 10px #1b1d1f1f;
+  background-color: ${({ theme }) => theme.color.WHITE};
+`;
+
+export const NotificationList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  overflow-x: hidden;
+  overflow-y: auto;
+
+  width: 100%;
+  margin-top: 16px;
+  padding-bottom: 10px;
+  gap: 18px;
+`;
+
+export const NotificationItem = styled.li`
+  display: flex;
+  flex-direction: column;
+
+  padding: 16px 18px;
+  gap: 10px;
+
+  border: 1px solid ${({ theme }) => theme.color.GRAY200};
+  border-radius: 8px;
+`;
+
+export const content = css`
+  line-height: 1.5;
+`;
+
+export const createdAtText = css`
+  color: ${({ theme }) => theme.color.GRAY600};
+`;
+
+export const lastNotificationText = css`
+  margin: 0 auto;
+`;
