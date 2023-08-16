52e1aafa3a5fce29e9835669982925970b7ba152
Suyoung Jung <devsuyoung@gmail.com>
2023-08-16 10:51:58 +0900

    refactor: 팀 플레이스 메뉴 버튼 접근성 개선

--- frontend/src/components/team/TeamPlaceMenu/TeamPlaceMenu.tsx
+++ frontend/src/components/team/TeamPlaceMenu/TeamPlaceMenu.tsx
@@ -24,7 +24,11 @@ const TeamPlaceMenu = (props: TeamPlaceMenuProps) => {
 
   return (
     <Menu>
-      <Menu.Button css={S.teamPlaceButton} aira-label="목록에서 팀 선택하기">
+      <Menu.Button
+        type="button"
+        css={S.teamPlaceButton}
+        aria-label="목록에서 팀 선택하기"
+      >
         <Text as="span" css={S.teamPlaceName}>
           {displayValue}
         </Text>
