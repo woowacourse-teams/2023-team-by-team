6873696b467eb6d26bf8f5778a4201db0e312677
요술토끼 <curious.wzrabbit@gmail.com>
2023-08-18 11:21:21 +0900

    feat: 팀 피드 placeholder 화면 구현 및 적용 / 스크롤 이슈 해결

---
+++ frontend/src/components/feed/EmptyFeedPlaceholder/EmptyFeedPlaceholder.styled.ts
@@ -0,0 +1,41 @@
+import { styled, css } from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  width: 100%;
+  height: 100%;
+`;
+
+export const PlaceholderContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  row-gap: 18px;
+
+  width: 100%;
+  height: 100%;
+`;
+
+export const FeedEmptyImage = styled.img`
+  width: 18%;
+  margin-bottom: 10px;
+`;
+
+export const placeholderButton = css`
+  width: 380px;
+  height: 220px;
+  padding: 0;
+`;
+
+export const titleText = css`
+  font-size: 28px;
+  color: ${({ theme }) => theme.color.GRAY400};
+`;
+
+export const clickToAddText = css`
+  font-size: 20px;
+  color: ${({ theme }) => theme.color.GRAY500};
+`;
