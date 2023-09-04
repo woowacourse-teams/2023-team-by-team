8b198d443bc950add0459cb36d614d3278277c9f
요술토끼 <curious.wzrabbit@gmail.com>
2023-09-04 14:12:27 +0900

    feat: 탈퇴 문구를 작성해야만 버튼이 활성화되도록 개선

--- frontend/src/components/user/AccountDeleteModal/AccountDeleteModal.tsx
+++ frontend/src/components/user/AccountDeleteModal/AccountDeleteModal.tsx
@@ -3,18 +3,30 @@ import Button from '~/components/common/Button/Button';
 import Text from '~/components/common/Text/Text';
 import Input from '~/components/common/Input/Input';
 import * as S from './AccountDeleteModal.styled';
+import { ACCOUNT_DELETE_CONFIRM_TEXT } from '~/constants/user';
+import { useModal } from '~/hooks/useModal';
+import { useAccountDeleteModal } from '~/hooks/user/useAccountDeleteModal';
 import { CloseIcon } from '~/assets/svg';
 
 const AccountDeleteModal = () => {
+  const { closeModal } = useModal();
+  const {
+    username,
+    inputValue,
+    handleInputValueChange,
+    isDeleteButtonDisabled,
+  } = useAccountDeleteModal();
+
   return (
     <Modal>
-      <S.Backdrop />
+      <S.Backdrop onClick={closeModal} />
       <S.Container>
         <S.ModalHeader>
           <Button
             variant="plain"
             type="button"
             css={S.closeButton}
+            onClick={closeModal}
             aria-label="회원 탈퇴 모달 닫기"
           >
             <CloseIcon />
@@ -22,7 +34,7 @@ const AccountDeleteModal = () => {
         </S.ModalHeader>
         <S.ModalBody>
           <Text size="xxl" weight="bold">
-            <S.Username>팀바팀</S.Username>님을 떠나보내야 한다니 아쉬워요.
+            <S.Username>{username}</S.Username>님을 떠나보내야 한다니 아쉬워요.
           </Text>
           <Text size="lg">회원 탈퇴 전, 아래의 유의사항을 읽어 주세요.</Text>
           <S.WarningBox>
@@ -33,19 +45,25 @@ const AccountDeleteModal = () => {
           </S.WarningBox>
           <Text size="lg">
             유의사항을 모두 확인하셨고, 회원 탈퇴를 원하신다면 하단의 입력창에{' '}
-            <b>탈퇴합니다</b>를 입력해 주세요.
+            <b>{ACCOUNT_DELETE_CONFIRM_TEXT}</b>를 입력해 주세요.
           </Text>
         </S.ModalBody>
         <S.AccountDeleteForm>
           <Input
             width="100%"
             height="40px"
-            placeholder="'탈퇴합니다' 를 입력해 주세요."
+            placeholder={`'${ACCOUNT_DELETE_CONFIRM_TEXT}' 를 입력해 주세요.`}
             css={S.deleteConfirmInput}
+            value={inputValue}
+            onChange={handleInputValueChange}
             autoFocus
             required
           />
-          <Button type="button" css={S.accountDeleteButton}>
+          <Button
+            type="button"
+            disabled={isDeleteButtonDisabled}
+            css={S.accountDeleteButton}
+          >
             회원 탈퇴하기
           </Button>
         </S.AccountDeleteForm>
---
+++ frontend/src/constants/user.ts
@@ -0,0 +1,1 @@
+export const ACCOUNT_DELETE_CONFIRM_TEXT = '탈퇴합니다';
