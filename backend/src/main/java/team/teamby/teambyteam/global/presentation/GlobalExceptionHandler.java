29865c47b773d5eec59fe016230e51931983c025
the9kim <the9rla@gmail.com>
2023-09-06 13:22:41 +0900

    feat: 핸들러에 멤버 이름 관련 예외 클래스 추가

--- backend/src/main/java/team/teamby/teambyteam/global/presentation/GlobalExceptionHandler.java
+++ backend/src/main/java/team/teamby/teambyteam/global/presentation/GlobalExceptionHandler.java
@@ -111,7 +111,9 @@ public class GlobalExceptionHandler {
             ScheduleException.SpanWrongOrderException.class,
             TeamPlaceInviteCodeException.LengthException.class,
             TeamPlaceException.NameLengthException.class,
-            TeamPlaceException.NameBlankException.class
+            TeamPlaceException.NameBlankException.class,
+            MemberException.NameLengthException.class,
+            MemberException.NameBlankException.class
     })
     public ResponseEntity<ErrorResponse> handleCustomBadRequestException(final RuntimeException exception) {
         final String message = exception.getMessage();
