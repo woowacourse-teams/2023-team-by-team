f79affa61e6a54926f35069985ffdb6026731e7d
Suyoung Jung <jsy19970512@naver.com>
2023-09-08 15:22:42 +0900

    fix: 1월 / 12월인 경우에 대한 예외처리 로직 작성

--- frontend/src/components/team_calendar/TeamCalendar/TeamCalendar.tsx
+++ frontend/src/components/team_calendar/TeamCalendar/TeamCalendar.tsx
@@ -48,8 +48,17 @@ const TeamCalendar = (props: TeamCalendarProps) => {
   } = useScheduleModal();
 
   const schedules = useFetchSchedules(teamPlaceId, year, month);
-  usePrefetchSchedules(teamPlaceId, year, month - 1);
-  usePrefetchSchedules(teamPlaceId, year, month + 1);
+  // NOTE: month의 값은 0부터 시작하므로 1월, 11월에 해당하는 month의 값은 각각 0, 11이다.
+  usePrefetchSchedules(
+    teamPlaceId,
+    month === 11 ? year + 1 : year,
+    month === 11 ? 0 : month + 1,
+  );
+  usePrefetchSchedules(
+    teamPlaceId,
+    month === 0 ? year - 1 : year,
+    month === 0 ? 11 : month - 1,
+  );
 
   const [clickedDate, setClickedDate] = useState(currentDate);
   const [modalType, setModalType] = useState<ModalOpenType>(
