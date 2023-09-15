0374af22d6ad01a9fc8072c46eadb1fe329824c4
SproutMj <mjk54565@gmail.com>
2023-09-15 10:17:16 +0900

    refactor: Response에 @Valid 삭제

--- backend/src/main/java/team/teamby/teambyteam/feed/application/dto/PresignedUrlsResponse.java
+++ backend/src/main/java/team/teamby/teambyteam/feed/application/dto/PresignedUrlsResponse.java
@@ -1,10 +1,8 @@
 package team.teamby.teambyteam.feed.application.dto;
 
-import jakarta.validation.constraints.Size;
-
 import java.util.List;
 
 public record PresignedUrlsResponse(
-        @Size(min = 1, max = 4) List<PreSignedUrlResponse> imageUrls
+        List<PreSignedUrlResponse> imageUrls
 ) {
 }
