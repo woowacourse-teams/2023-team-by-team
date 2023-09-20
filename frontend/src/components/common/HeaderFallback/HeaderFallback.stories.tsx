c7ed1f9e2914a9daabd3e7b9528cc610fc6511a8
Suyoung Jung <jsy19970512@naver.com>
2023-09-20 19:39:53 +0900

    feat: HeaderFallback 컴포넌트 작성

---
+++ frontend/src/components/common/HeaderFallback/HeaderFallback.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import HeaderFallback from '~/components/common/HeaderFallback/HeaderFallback';
+
+const meta = {
+  title: 'common/HeaderFallback',
+  component: HeaderFallback,
+  tags: ['autodocs'],
+} satisfies Meta<typeof HeaderFallback>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {},
+};
---
+++ frontend/src/components/common/HeaderFallback/HeaderFallback.tsx
@@ -0,0 +1,42 @@
+import Button from '~/components/common/Button/Button';
+import Skeleton from '~/components/common/Skeleton/Skeleton';
+import { BellIcon, LogoIcon, TeamIcon } from '~/assets/svg';
+import * as S from './HeaderFallback.styled';
+
+const HeaderFallback = () => {
+  return (
+    <S.Container>
+      <S.InnerContainer>
+        <LogoIcon />
+        <div>
+          <S.TeamBadgePlaceholder>
+            <Skeleton variant="circle" />
+          </S.TeamBadgePlaceholder>
+          <S.TeamPlaceMenuPlaceholder>
+            <Skeleton />
+          </S.TeamPlaceMenuPlaceholder>
+        </div>
+      </S.InnerContainer>
+
+      <S.ButtonContainer>
+        <Button type="button" variant="plain" disabled>
+          <BellIcon />
+        </Button>
+
+        <Button type="button" variant="plain" disabled>
+          <TeamIcon />
+        </Button>
+
+        <S.Divider />
+
+        <Button type="button" variant="plain" disabled>
+          <S.ProfileImagePlaceholder>
+            <Skeleton variant="circle" />
+          </S.ProfileImagePlaceholder>
+        </Button>
+      </S.ButtonContainer>
+    </S.Container>
+  );
+};
+
+export default HeaderFallback;
