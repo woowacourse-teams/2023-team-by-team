929c4ab4067008c574808a0e649dc11e93b854ed
Suyoung Jung <jsy19970512@naver.com>
2023-09-20 20:32:05 +0900

    refactor: 등록된 스레드 없을 시 디자인 수정

--- frontend/src/components/feed/EmptyFeedPlaceholder/EmptyFeedPlaceholder.tsx
+++ frontend/src/components/feed/EmptyFeedPlaceholder/EmptyFeedPlaceholder.tsx
@@ -1,33 +1,16 @@
-import * as S from './EmptyFeedPlaceholder.styled';
-import Button from '~/components/common/Button/Button';
 import Text from '~/components/common/Text/Text';
 import { feedEmptyImage } from '~/assets/png';
+import * as S from './EmptyFeedPlaceholder.styled';
 
-interface EmptyFeedPlaceholderProps {
-  onClick: () => void;
-}
-
-const EmptyFeedPlaceholder = (props: EmptyFeedPlaceholderProps) => {
-  const { onClick } = props;
-
+const EmptyFeedPlaceholder = () => {
   return (
     <S.Container>
-      <Button
-        variant="plain"
-        onClick={onClick}
-        css={S.placeholderButton}
-        aria-label="새로운 링크 등록하기"
-      >
-        <S.PlaceholderContainer>
-          <S.FeedEmptyImage src={feedEmptyImage} alt="비어있는 피드 이미지" />
-          <Text weight="bold" css={S.titleText}>
-            등록된 피드가 없어요
-          </Text>
-          <Text weight="semiBold" css={S.clickToAddText}>
-            여기를 클릭해 새로운 피드를 작성해보세요
-          </Text>
-        </S.PlaceholderContainer>
-      </Button>
+      <S.PlaceholderContainer>
+        <S.FeedEmptyImage src={feedEmptyImage} alt="비어있는 피드 이미지" />
+        <Text weight="bold" css={S.titleText}>
+          대화를 시작해 보세요!
+        </Text>
+      </S.PlaceholderContainer>
     </S.Container>
   );
 };
