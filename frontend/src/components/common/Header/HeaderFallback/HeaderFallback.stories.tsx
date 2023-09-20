78dc56a3de53add1c35a18964ea94469d8221a9b
hafnium1923 <hafnium1923@gmail.com>
2023-09-20 20:49:38 +0900

    refactor: HeaderFallback 스토리북 title 변경

--- frontend/src/components/common/Header/HeaderFallback/HeaderFallback.stories.tsx
+++ frontend/src/components/common/Header/HeaderFallback/HeaderFallback.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/react';
 import HeaderFallback from '~/components/common/Header/HeaderFallback/HeaderFallback';
 
 const meta = {
-  title: 'common/HeaderFallback',
+  title: 'Fallback/HeaderFallback',
   component: HeaderFallback,
   tags: ['autodocs'],
 } satisfies Meta<typeof HeaderFallback>;
