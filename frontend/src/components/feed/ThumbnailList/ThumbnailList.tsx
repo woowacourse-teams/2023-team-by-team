82e61efebc54b7e130e0da10e93d376064c185c5
WizardRabbit <curious.wzrabbit@gmail.com>
2023-09-22 02:03:01 +0900

    refactor: 타입 관련 변동사항에 대한 ThumbnailList 컴포넌트 반영

--- frontend/src/components/feed/ThumbnailList/ThumbnailList.tsx
+++ frontend/src/components/feed/ThumbnailList/ThumbnailList.tsx
@@ -1,17 +1,18 @@
 import * as S from './ThumbnailList.styled';
 import DeletableThumbnail from '~/components/feed/DeletableThumbnail/DeletableThumbnail';
 import ViewableThumbnail from '~/components/feed/ViewableThumbnail/ViewableThumbnail';
-import type { ThreadImage } from '~/types/feed';
-import { MAX_UPLOAD_IMAGE_COUNT } from '~/constants/feed';
+import type { ThreadImage, PreviewImage } from '~/types/feed';
+import type { ChangeEventHandler } from 'react';
 import ImageAddButton from '../ImageAddButton/ImageAddButton';
+import { MAX_UPLOAD_IMAGE_COUNT } from '~/constants/feed';
 
 type ThumbnailListProps = DeletableThumbnails | ViewableThumbnails;
 
 interface DeletableThumbnails {
   mode: 'delete';
-  images: ThreadImage[];
-  onDelete: (imageId: number) => void;
-  onClickAddButton: () => void;
+  images: PreviewImage[];
+  onDelete: (imageUuid: string) => void;
+  onChange: ChangeEventHandler<HTMLInputElement>;
 }
 
 interface ViewableThumbnails {
@@ -25,23 +26,23 @@ const ThumbnailList = (props: ThumbnailListProps) => {
 
   return (
     <S.Container role="list">
-      {images.map((image, index) =>
-        mode === 'delete' ? (
-          <DeletableThumbnail
-            key={image.id}
-            image={image}
-            onDelete={props.onDelete}
-          />
-        ) : (
-          <ViewableThumbnail
-            key={image.id}
-            image={image}
-            onClick={() => props.onClick(images, index + 1)}
-          />
-        ),
-      )}
+      {mode === 'delete'
+        ? images.map((image) => (
+            <DeletableThumbnail
+              key={image.uuid}
+              image={image}
+              onDelete={props.onDelete}
+            />
+          ))
+        : images.map((image, index) => (
+            <ViewableThumbnail
+              key={image.id}
+              image={image}
+              onClick={() => props.onClick(images, index + 1)}
+            />
+          ))}
       {mode === 'delete' && images.length < MAX_UPLOAD_IMAGE_COUNT && (
-        <ImageAddButton onClick={props.onClickAddButton} />
+        <ImageAddButton onChangeImage={props.onChange} />
       )}
     </S.Container>
   );
