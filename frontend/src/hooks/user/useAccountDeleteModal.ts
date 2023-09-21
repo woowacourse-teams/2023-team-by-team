facb50288b930fd75ebfcc797ded978562925c1f
hafnium1923 <hafnium1923@gmail.com>
2023-09-21 16:11:00 +0900

    feat: 회원 탈퇴 훅을 탈퇴 모달과 연결

--- frontend/src/hooks/user/useAccountDeleteModal.ts
+++ frontend/src/hooks/user/useAccountDeleteModal.ts
@@ -1,20 +1,57 @@
-import type { ChangeEvent } from 'react';
+import type { ChangeEvent, FormEventHandler } from 'react';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { LOCAL_STORAGE_KEY } from '~/constants/localStorage';
+import { PATH_NAME } from '~/constants/routes';
 import { ACCOUNT_DELETE_CONFIRM_TEXT } from '~/constants/user';
+import { useDeleteUserAccount } from '~/hooks/queries/useDeleteUserAccount';
 import { useFetchUserInfo } from '~/hooks/queries/useFetchUserInfo';
+import { useToast } from '~/hooks/useToast';
 
 export const useAccountDeleteModal = () => {
   const { userInfo } = useFetchUserInfo();
+  const { mutateDeleteUserAccount } = useDeleteUserAccount();
   const [inputValue, setInputValue] = useState('');
+  const navigate = useNavigate();
+  const { showToast } = useToast();
 
   const handleInputValueChange = (e: ChangeEvent<HTMLInputElement>) => {
     setInputValue(() => e.target.value);
   };
 
+  const handleDeleteAccountSubmit: FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+
+    console.log('in');
+    if (inputValue !== ACCOUNT_DELETE_CONFIRM_TEXT) {
+      alert('탈퇴를 위해 정확한 문구를 입력하세요');
+      setInputValue(() => '');
+    }
+
+    mutateDeleteUserAccount(undefined, {
+      onSuccess: () => {
+        alert('정상적으로 회원탈퇴 되었습니다.');
+
+        localStorage.removeItem(LOCAL_STORAGE_KEY.ACCESS_TOKEN);
+        localStorage.removeItem(LOCAL_STORAGE_KEY.REFRESH_TOKEN);
+        localStorage.removeItem(LOCAL_STORAGE_KEY.TEAM_PLACE_ID);
+
+        navigate(PATH_NAME.LANDING);
+      },
+      onError: () => {
+        showToast('error', '탈퇴가 실패했습니다. 다시 시도해주세요');
+        return;
+      },
+    });
+  };
+
   return {
     username: userInfo?.name,
     inputValue,
-    handleInputValueChange,
     isDeleteButtonDisabled: inputValue !== ACCOUNT_DELETE_CONFIRM_TEXT,
+    handlers: {
+      handleInputValueChange,
+      handleDeleteAccountSubmit,
+    },
   };
 };
