ccb7442e55c1ca507bf541419c755089c7dffd83
WizardRabbit <curious.wzrabbit@gmail.com>
2023-09-22 02:02:07 +0900

    feat: useImageUploader 커스텀 훅을 구현
    - 업로드된 이미지를 파일 배열에 추가하고 기존 file input을 비움
    - preview 이미지를 열람 가능하도록 해당 값 반환 가능
    - uuid를 이용해 하나의 이미지 삭제 가능
    - 모든 이미지 삭제 가능
    - 지금까지 올라온 이미지를 file 객체 배열로 받을 수 있음

---
+++ frontend/src/hooks/thread/useImageUploader.tsx
@@ -0,0 +1,116 @@
+import { useState, useEffect } from 'react';
+import type { ChangeEvent } from 'react';
+import type { FileWithUuid, PreviewImage } from '~/types/feed';
+import { MAX_UPLOAD_IMAGE_COUNT } from '~/constants/feed';
+import { useToast } from '../useToast';
+
+/**
+ * `useImageUploader` 는 이미지를 로컬에 업로드하여 관리하기 위한 커스텀 훅입니다.
+ *
+ * @returns {PreviewImage[]} previewImages - 미리보기 이미지의 `src`와 식별을 위한 `uuid`를 제공합니다.
+ * @returns {File[]} imageFiles - 실제 업로드에 사용될 이미지 정보들을 `File` 객체로 된 배열로 제공합니다.
+ * @returns {(e: ChangeEvent<HTMLInputElement>) => void} - updateImages 이미지 업로드 시 호출하는 함수입니다.
+ * @returns {deleteImageByUuid} deleteImageByUuid - `uuid`를 이용해 특정 이미지를 삭제할 시 호출하는 함수입니다.
+ * @returns {deleteAllImages} deleteAllImages - 모든 이미지를 삭제할 때 호출하는 함수입니다.
+ */
+const useImageUploader = () => {
+  const [filesWithUuid, setFilesWithUuid] = useState<FileWithUuid[]>([]);
+  const [previewImages, setPreviewImages] = useState<PreviewImage[]>([]);
+  const { showToast } = useToast();
+
+  const updateImages = (e: ChangeEvent<HTMLInputElement>) => {
+    if (!(e.target instanceof HTMLInputElement)) {
+      return;
+    }
+
+    const newFiles = e.target.files;
+
+    if (!newFiles) {
+      e.target.value = '';
+
+      return;
+    }
+
+    if (filesWithUuid.length + newFiles.length > MAX_UPLOAD_IMAGE_COUNT) {
+      e.target.value = '';
+      showToast(
+        'error',
+        `이미지는 최대 ${MAX_UPLOAD_IMAGE_COUNT}장까지 업로드 가능합니다.`,
+      );
+
+      return;
+    }
+
+    const newFilesWithId = [...newFiles].map((file) => ({
+      uuid: crypto.randomUUID(),
+      file,
+    }));
+
+    setFilesWithUuid((prevFilesWithUuid) => [
+      ...prevFilesWithUuid,
+      ...newFilesWithId,
+    ]);
+
+    e.target.value = '';
+  };
+
+  const readImage = (file: File): Promise<string> => {
+    return new Promise((resolve, reject) => {
+      const fileReader = new FileReader();
+
+      fileReader.onload = () => {
+        const resultUrl = fileReader.result;
+
+        if (typeof resultUrl === 'string') {
+          resolve(resultUrl);
+        }
+
+        reject();
+      };
+
+      fileReader.readAsDataURL(file);
+    });
+  };
+
+  const updatePreviewImages = async () => {
+    const uuids = filesWithUuid.map(({ uuid }) => uuid);
+    const readResults = await Promise.allSettled(
+      filesWithUuid.map(({ file }) => readImage(file)),
+    );
+
+    const imageUrls = readResults.map((result, index) => {
+      if (result.status === 'fulfilled') {
+        return { uuid: uuids[index], url: result.value };
+      } else {
+        return { uuid: uuids[index], url: '' };
+      }
+    });
+
+    setPreviewImages(() => imageUrls);
+  };
+
+  const deleteImageByUuid = (targetUuid: string) => {
+    setFilesWithUuid((prevFilesWithUuid) =>
+      prevFilesWithUuid.filter(({ uuid }) => uuid !== targetUuid),
+    );
+  };
+
+  const deleteAllImages = () => {
+    setFilesWithUuid(() => []);
+  };
+
+  useEffect(() => {
+    updatePreviewImages();
+    /* eslint-disable-next-line */
+  }, [filesWithUuid]);
+
+  return {
+    previewImages,
+    imageFiles: filesWithUuid.map(({ file }) => file),
+    updateImages,
+    deleteImageByUuid,
+    deleteAllImages,
+  };
+};
+
+export default useImageUploader;
