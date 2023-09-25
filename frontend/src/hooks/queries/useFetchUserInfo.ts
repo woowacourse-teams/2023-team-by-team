17cae452a20aba99d2840e4197777485237d1c35
Suyoung Jung <jsy19970512@naver.com>
2023-09-25 18:08:07 +0900

    fix: useFetchUserInfo suspense 옵션 제거

--- frontend/src/hooks/queries/useFetchUserInfo.ts
+++ frontend/src/hooks/queries/useFetchUserInfo.ts
@@ -2,9 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import { fetchUserInfo } from '~/apis/user';
 
 export const useFetchUserInfo = () => {
-  const { data } = useQuery(['userInfo'], fetchUserInfo, {
-    suspense: true,
-  });
+  const { data } = useQuery(['userInfo'], fetchUserInfo);
 
   return { userInfo: data };
 };
