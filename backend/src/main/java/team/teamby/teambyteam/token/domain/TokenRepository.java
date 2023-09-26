9e18a84537862f7a09b4eeb6420d7288b0a542dc
sh111-coder <ohk9134@naver.com>
2023-09-26 13:47:24 +0900

    feat: 토큰 삭제 쿼리 JPQL로 작성

--- backend/src/main/java/team/teamby/teambyteam/token/domain/TokenRepository.java
+++ backend/src/main/java/team/teamby/teambyteam/token/domain/TokenRepository.java
@@ -1,6 +1,8 @@
 package team.teamby.teambyteam.token.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import team.teamby.teambyteam.member.domain.Member;
 
 import java.util.Optional;
@@ -11,5 +13,7 @@ public interface TokenRepository extends JpaRepository<Token, Long> {
 
     Optional<Token> findByMember(final Member member);
 
+    @Query("DELETE from Token t where t.member = :member")
+    @Modifying
     void deleteByMember(final Member member);
 }
--- backend/src/test/java/team/teamby/teambyteam/token/domain/TokenRepositoryTest.java
+++ backend/src/test/java/team/teamby/teambyteam/token/domain/TokenRepositoryTest.java
@@ -33,4 +33,18 @@ class TokenRepositoryTest extends RepositoryTest {
         assertThat(findToken).usingRecursiveComparison()
                 .isEqualTo(savedToken);
     }
+
+    @Test
+    @DisplayName("토큰을 멤버로 삭제한다.")
+    void deleteByMember() {
+        // given
+        final Member philip = memberRepository.save(PHILIP());
+        final Token savedToken = tokenRepository.save(new Token(philip, CORRECT_REFRESH_TOKEN));
+
+        // when
+        tokenRepository.deleteByMember(philip);
+
+        // then
+        assertThat(tokenRepository.findByMember(philip)).isEmpty();
+    }
 }
