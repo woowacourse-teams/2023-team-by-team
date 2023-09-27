ee39a0b84b0bc868d2c3aa085e07dd686e302784
KSH_coding <ohk9134@naver.com>
2023-09-27 10:35:06 +0900

    [BE] 회원탈퇴 오류 수정 (회원 탈퇴 이벤트 구현) (#704)
    * refactor: 회원 탈퇴 시 회원 탈퇴 이벤트 발행
    
    * feat: 토큰 삭제 이벤트 리스너 로직 구현
    
    * test: 회원 탈퇴 테스트 시 토큰 있는 상태인 테스트로 리팩토링
    
    * refactor: 멤버 Delete, 토큰 Delete 로직 이벤트 리스너에 모두 위임
    
    * refactor: 회원 탈퇴 로깅 시 Email Value 로깅하도록 리팩토링
    
    * feat: 토큰 삭제 쿼리 JPQL로 작성
    
    * refactor: 이벤트 리스너 TransactionalEventListener로 변경
    
    * refactor: 토큰 삭제 파라미터 Member -> memberId로 리팩토링
    
    * refactor: MemberLeaveEvent에서도 memberId 받도록 리팩토링

--- backend/src/main/java/team/teamby/teambyteam/token/domain/TokenRepository.java
+++ backend/src/main/java/team/teamby/teambyteam/token/domain/TokenRepository.java
@@ -1,6 +1,8 @@
 package team.teamby.teambyteam.token.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import team.teamby.teambyteam.member.domain.Member;
 
 import java.util.Optional;
@@ -10,4 +12,8 @@ public interface TokenRepository extends JpaRepository<Token, Long> {
     Optional<Token> findByRefreshToken(final String refreshToken);
 
     Optional<Token> findByMember(final Member member);
+
+    @Query("DELETE from Token t where t.member.id = :memberId")
+    @Modifying
+    void deleteByMemberId(final Long memberId);
 }
