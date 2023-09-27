1f6bd5a8dfae89ed3ee2808e93886f2e02810011
sh111-coder <ohk9134@naver.com>
2023-09-27 18:40:48 +0900

    ################ # <type>: <subject> # subject : 1줄 요약 알잘딱하게 # no dot(.) test: 테스트를 위한 이미지 추가 & 픽스쳐 리팩토링 # <BLANK LINE> (제목과 본문의 분리를 위함) refactor: ImageUploadService Deprecated 처리 ################ # Body Message (optional) # 본문(구체적인 내용)을 아랫줄에 작성 # 여러 줄의 메시지를 작성할 땐 "-"로 구분
    # <BLANK LINE>
    
    ################
    # co-author
    # Co-authored-by: {GITHUB_ID} <{E_MAIL}>
    # Co-authored-by: pilyang <pilyang.dev@gmail.com>
    # Co-authored-by: sh111-coder <ohk9134@naver.com>
    # Co-authored-by: the9kim <the9rla@gmail.com>
    Co-authored-by: SproutMJ <mjk54565@gmail.com>
    
    # --- COMMIT END ---
    # Allowed <type>
    #   feat    : 새로운 기능
    #   fix     : 버그 수정
    #   refactor: 코드 리팩토링, 파일 이동, 컴포넌트 스타일 변경
    #   style   : 코드 의미에 영향을 주지 않는 변경사항 (포매팅 수정, 들여쓰기 추가, ...)
    #   docs    : 문서의 추가, 수정, 삭제
    #   test    : 테스트 추가, 수정, 삭제 (비즈니스 로직에 변경 없음)
    #   chore   : 기타 변경사항 (빌드 부분 혹은 패키지 의존성 설정 등)
    # ------------------
    # git convention url : https://gist.github.com/stephenparish/9941e89d80e2bc58a153
    ################

--- backend/src/test/java/team/teamby/teambyteam/common/fixtures/FeedThreadFixtures.java
+++ backend/src/test/java/team/teamby/teambyteam/common/fixtures/FeedThreadFixtures.java
@@ -1,10 +1,60 @@
 package team.teamby.teambyteam.common.fixtures;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+import org.springframework.mock.web.MockMultipartFile;
 import team.teamby.teambyteam.feed.application.dto.FeedThreadWritingRequest;
 import team.teamby.teambyteam.feed.domain.FeedThread;
 import team.teamby.teambyteam.feed.domain.vo.Content;
 
 public class FeedThreadFixtures {
+
+    /**
+     * FILE
+     */
+    public static final File UNDER_SIZE_PNG_FILE1 = new File("src/test/resources/images/infra-under-size.png");
+    public static final File UNDER_SIZE_PNG_FILE2 = new File("src/test/resources/images/logo-under-size.png");
+    public static final File UNDER_SIZE_PNG_FILE3 = new File("src/test/resources/images/ci-cd-under-size.png");
+    public static final File UNDER_SIZE_PNG_FILE4 = new File("src/test/resources/images/diagram-under-size.png");
+    public static final File OVER_SIZE_PNG_FILE = new File("src/test/resources/images/over-size-image.png");
+    public static final File UNDER_SIZE_WRONG_EXTENSION_FILE = new File("src/test/resources/images/wrong-extension-file.pptx");
+
+    /**
+     * MOCK_MULTIPART_FILE
+     */
+    public static final MockMultipartFile UNDER_SIZE_PNG_MOCK_MULTIPART_FILE1;
+    public static final MockMultipartFile UNDER_SIZE_PNG_MOCK_MULTIPART_FILE2;
+    public static final MockMultipartFile UNDER_SIZE_PNG_MOCK_MULTIPART_FILE3;
+    public static final MockMultipartFile UNDER_SIZE_PNG_MOCK_MULTIPART_FILE4;
+    public static final MockMultipartFile OVER_SIZE_PNG_MOCK_MULTIPART_FILE;
+    public static final MockMultipartFile UNDER_SIZE_WRONG_EXTENSION_MOCK_MULTIPART_FILE;
+
+    static {
+        try {
+            UNDER_SIZE_PNG_MOCK_MULTIPART_FILE1 = new MockMultipartFile("under-size1",
+                    UNDER_SIZE_PNG_FILE1.getName(), "image/png", new FileInputStream(UNDER_SIZE_PNG_FILE1));
+
+            UNDER_SIZE_PNG_MOCK_MULTIPART_FILE2 = new MockMultipartFile("under-size2",
+                    UNDER_SIZE_PNG_FILE1.getName(), "image/png", new FileInputStream(UNDER_SIZE_PNG_FILE2));
+
+            UNDER_SIZE_PNG_MOCK_MULTIPART_FILE3 = new MockMultipartFile("under-size3",
+                    UNDER_SIZE_PNG_FILE1.getName(), "image/png", new FileInputStream(UNDER_SIZE_PNG_FILE3));
+
+            UNDER_SIZE_PNG_MOCK_MULTIPART_FILE4 = new MockMultipartFile("under-size4",
+                    UNDER_SIZE_PNG_FILE1.getName(), "image/png", new FileInputStream(UNDER_SIZE_PNG_FILE4));
+
+            OVER_SIZE_PNG_MOCK_MULTIPART_FILE = new MockMultipartFile("over-size",
+                    UNDER_SIZE_PNG_FILE1.getName(), "image/png", new FileInputStream(UNDER_SIZE_PNG_FILE4));
+
+            UNDER_SIZE_WRONG_EXTENSION_MOCK_MULTIPART_FILE = new MockMultipartFile("under-size-wrong-extension",
+                    UNDER_SIZE_PNG_FILE1.getName(), "application/vnd.openxmlformats-officedocument.presentationml.presentation", new FileInputStream(UNDER_SIZE_PNG_FILE4));
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
 
     /**
@@ -16,8 +66,16 @@ public class FeedThreadFixtures {
     /**
      * REQUEST
      */
-    public static final FeedThreadWritingRequest HELLO_WRITING_REQUEST = new FeedThreadWritingRequest(HELLO_CONTENT);
-    public static final FeedThreadWritingRequest HI_WRITING_REQUEST = new FeedThreadWritingRequest(HI_CONTENT);
+    public static final FeedThreadWritingRequest CONTENT_ONLY_REQUEST = new FeedThreadWritingRequest(HELLO_CONTENT,
+            Collections.emptyList());
+    public static final FeedThreadWritingRequest IMAGE_ONLY_REQUEST = new FeedThreadWritingRequest("",
+            List.of(UNDER_SIZE_PNG_MOCK_MULTIPART_FILE1, UNDER_SIZE_PNG_MOCK_MULTIPART_FILE2));
+    public static final FeedThreadWritingRequest CONTENT_AND_IMAGE_REQUEST = new FeedThreadWritingRequest(HELLO_CONTENT,
+            List.of(UNDER_SIZE_PNG_MOCK_MULTIPART_FILE1, UNDER_SIZE_PNG_MOCK_MULTIPART_FILE2));
+    public static final FeedThreadWritingRequest EMPTY_REQUEST = new FeedThreadWritingRequest("",
+            Collections.emptyList());
+    public static final FeedThreadWritingRequest HI_WRITING_REQUEST = new FeedThreadWritingRequest(HI_CONTENT,
+            List.of(UNDER_SIZE_PNG_MOCK_MULTIPART_FILE1, UNDER_SIZE_PNG_MOCK_MULTIPART_FILE2));
 
     /**
      * ENTITY
