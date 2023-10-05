fd7a3a743d20f113cdf4691c4901cad7c54f1cc0
Suyoung Jung <jsy19970512@naver.com>
2023-10-05 12:56:48 +0900

    feat: ICalendar url fetch 실패 시 커스텀 에러 메시지 추가

--- frontend/src/hooks/queries/useFetchICalendarUrl.ts
+++ frontend/src/hooks/queries/useFetchICalendarUrl.ts
@@ -1,26 +1,18 @@
 import { useQuery } from '@tanstack/react-query';
-import { useEffect } from 'react';
 import { fetchICalendarUrl } from '~/apis/schedule';
-import { useToast } from '~/hooks/useToast';
 
 export const useFetchICalendarUrl = (teamPlaceId: number) => {
-  const { data, error } = useQuery(
+  const { data } = useQuery(
     ['iCalendarUrl', teamPlaceId],
     () => fetchICalendarUrl(teamPlaceId),
     {
       enabled: teamPlaceId > 0,
+      meta: {
+        errorMessage:
+          '캘린더 내보내기에 실패했습니다. \n지속되는 경우 관리자에게 문의해주세요.',
+      },
     },
   );
-  const { showToast } = useToast();
-
-  useEffect(() => {
-    if (error) {
-      showToast(
-        'error',
-        '잠시 후 다시 시도해 주세요. 지속적으로 확인할 수 없는 경우 관리자에게 문의해 주세요.',
-      );
-    }
-  }, [error]);
 
   return data ?? { url: '' };
 };
