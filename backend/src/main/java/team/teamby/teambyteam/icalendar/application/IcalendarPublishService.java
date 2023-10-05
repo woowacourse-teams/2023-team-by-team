2039de570eb53a7cba3ccfd86cc2285d076f75a6
pilyang <pilyang.dev@gmail.com>
2023-10-05 17:47:30 +0900

    feat: 예외사항 예외 추가

--- backend/src/main/java/team/teamby/teambyteam/icalendar/application/IcalendarPublishService.java
+++ backend/src/main/java/team/teamby/teambyteam/icalendar/application/IcalendarPublishService.java
@@ -1,15 +1,16 @@
 package team.teamby.teambyteam.icalendar.application;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import team.teamby.teambyteam.filesystem.FileCloudUploader;
-import team.teamby.teambyteam.icalendar.domain.vo.IcalendarFileName;
 import team.teamby.teambyteam.icalendar.domain.IcalendarParser;
-import team.teamby.teambyteam.icalendar.domain.vo.PublishUrl;
 import team.teamby.teambyteam.icalendar.domain.PublishedIcalendar;
 import team.teamby.teambyteam.icalendar.domain.PublishedIcalendarRepository;
+import team.teamby.teambyteam.icalendar.domain.vo.IcalendarFileName;
+import team.teamby.teambyteam.icalendar.domain.vo.PublishUrl;
 import team.teamby.teambyteam.schedule.domain.Schedule;
 import team.teamby.teambyteam.schedule.domain.ScheduleRepository;
 import team.teamby.teambyteam.teamplace.domain.TeamPlace;
@@ -17,12 +18,13 @@ import team.teamby.teambyteam.teamplace.domain.TeamPlaceRepository;
 
 import java.util.List;
 
+@Slf4j
 @Service
 @Transactional
 @RequiredArgsConstructor
 public class IcalendarPublishService {
 
-    private static final String TEAM_PLACE_NOT_CREATED_EXCEPTION_MESSAGE = "아직 생성되지 않은 팀플레이스 아이디입니다 - ";
+    private static final String TEAM_PLACE_NOT_CREATED_EXCEPTION_MESSAGE = "존재하지 않은 팀플레이스 아이디입니다 - ";
 
     private final PublishedIcalendarRepository publishedIcalendarRepository;
     private final TeamPlaceRepository teamPlaceRepository;
@@ -39,7 +41,10 @@ public class IcalendarPublishService {
         }
         final IcalendarFileName icalendarFileName = IcalendarFileName.generateRandomFileName(teamPlaceId);
         final TeamPlace teamPlace = teamPlaceRepository.findById(teamPlaceId)
-                .orElseThrow(() -> new IllegalArgumentException(TEAM_PLACE_NOT_CREATED_EXCEPTION_MESSAGE + teamPlaceId));
+                .orElseThrow(() -> {
+                    log.error("캘린더 생성요청이 들어온 팀플레이스가 존제하지 않음 - teamPlaceId : {}", teamPlaceId);
+                    return new IllegalArgumentException(TEAM_PLACE_NOT_CREATED_EXCEPTION_MESSAGE + teamPlaceId);
+                });
 
         final String uploadedUrl = uploadIcalendarFile(teamPlace, icalendarFileName);
 
