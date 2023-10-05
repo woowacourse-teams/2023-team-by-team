d2429eea52df95d33a2d8a093882d8065c232130
Suyoung Jung <jsy19970512@naver.com>
2023-10-05 16:30:53 +0900

    feat: ICalendarModal 컴포넌트 작성

---
+++ frontend/src/components/team_calendar/ICalendarModal/ICalendarModal.styled.ts
@@ -0,0 +1,119 @@
+import styled, { css } from 'styled-components';
+
+export const Backdrop = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+`;
+
+export const Container = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  display: flex;
+  flex-direction: column;
+
+  width: 400px;
+  min-height: 200px;
+  padding: 20px;
+
+  border-radius: 8px;
+  box-shadow:
+    0 0 1px #1b1d1f33,
+    0 15px 25px #1b1d1f33,
+    0 5px 10px #1b1d1f1f;
+  background-color: ${({ theme }) => theme.color.WHITE};
+`;
+
+export const Header = styled.div`
+  position: relative;
+  display: flex;
+  align-items: center;
+
+  & > button {
+    margin-left: auto;
+  }
+`;
+
+export const TooltipWrapper = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+export const Tooltip = styled.div`
+  position: absolute;
+  top: 30px;
+  left: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 320px;
+  height: 100px;
+  padding: 10px 20px;
+
+  border: 1px solid ${({ theme }) => theme.color.GRAY300};
+  border-radius: 8px;
+  background-color: ${({ theme }) => theme.color.WHITE};
+
+  & > p {
+    line-height: 1.3;
+  }
+`;
+
+export const UrlContainer = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  border: 1px solid ${({ theme }) => theme.color.GRAY300};
+  border-radius: 8px;
+
+  margin: 10px 0 20px 0;
+`;
+
+export const UrlWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 90%;
+  height: 40px;
+  padding: 0 8px;
+
+  & > div {
+    width: 100%;
+
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+  }
+`;
+
+export const UserGuideLink = styled.a`
+  width: 200px;
+`;
+
+export const closeButton = css`
+  padding: 0;
+`;
+
+export const copyButton = css`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: 32px;
+  height: 40px;
+  padding: 0;
+
+  background-color: ${({ theme }) => theme.color.GRAY100};
+  border-radius: 0 8px 8px 0;
+`;
+
+export const shortCutText = css`
+  color: ${({ theme }) => theme.color.GRAY800};
+`;
---
+++ frontend/src/components/team_calendar/ICalendarModal/ICalendarModal.tsx
@@ -0,0 +1,113 @@
+import { useState } from 'react';
+import Text from '~/components/common/Text/Text';
+import Button from '~/components/common/Button/Button';
+import Modal from '~/components/common/Modal/Modal';
+import Spacing from '~/components/common/Spacing/Spacing';
+import { useModal } from '~/hooks/useModal';
+import { useFetchICalendarUrl } from '~/hooks/queries/useFetchICalendarUrl';
+import { useTeamPlace } from '~/hooks/useTeamPlace';
+import { useToast } from '~/hooks/useToast';
+import { ClipboardIcon, CloseIcon, QuestionIcon } from '~/assets/svg';
+import { ICALENDAR_USER_GUIDE_URL } from '~/constants/calendar';
+import * as S from './ICalendarModal.styled';
+
+const ICalendarModal = () => {
+  const { closeModal } = useModal();
+  const { showToast } = useToast();
+  const { teamPlaceId } = useTeamPlace();
+
+  const { url } = useFetchICalendarUrl(teamPlaceId);
+
+  const [isTooltipOpen, setIsTooltipOpen] = useState(false);
+
+  const handleCopyButtonClick = () => {
+    if (url === '') {
+      return;
+    }
+
+    try {
+      navigator.clipboard.writeText(url);
+      showToast('success', '내보내기 링크가 복사되었습니다.');
+    } catch (error) {
+      showToast('error', '내보내기 링크 복사에 실패했습니다.');
+    }
+  };
+
+  return (
+    <Modal>
+      <S.Backdrop onClick={closeModal} />
+      <S.Container>
+        <S.Header>
+          <Text as="span" size="xl" weight="semiBold">
+            일정 내보내기
+          </Text>
+          <Spacing size={8} direction="horizontal" />
+          <S.TooltipWrapper
+            onMouseEnter={() => setIsTooltipOpen(true)}
+            onMouseLeave={() => setIsTooltipOpen(false)}
+          >
+            <QuestionIcon />
+            {isTooltipOpen && (
+              <>
+                <S.Tooltip>
+                  <Text>
+                    팀바팀 캘린더에 등록된 일정을 <br />
+                    구글 캘린더, 애플의 iCal 등에서 사용 가능한{' '}
+                    <Text as="span" weight="semiBold">
+                      .ics
+                    </Text>{' '}
+                    파일로 내보내는 기능입니다.
+                  </Text>
+                </S.Tooltip>
+              </>
+            )}
+          </S.TooltipWrapper>
+          <Button
+            variant="plain"
+            type="button"
+            onClick={closeModal}
+            aria-label="일정 내보내기 모달 닫기"
+            css={S.closeButton}
+          >
+            <CloseIcon />
+          </Button>
+        </S.Header>
+
+        <Spacing size={16} />
+
+        <Text as="span" weight="semiBold">
+          내보낸 파일(.ics) 경로
+        </Text>
+        <S.UrlContainer>
+          <S.UrlWrapper>
+            <div title={url}>
+              <Text as="span">{url}</Text>
+            </div>
+          </S.UrlWrapper>
+          <Button
+            type="button"
+            variant="plain"
+            css={S.copyButton}
+            onClick={handleCopyButtonClick}
+            aria-label="일정 내보내기 링크 복사하기"
+            disabled={url === ''}
+          >
+            <ClipboardIcon />
+          </Button>
+        </S.UrlContainer>
+
+        <S.UserGuideLink
+          href={ICALENDAR_USER_GUIDE_URL}
+          target="_blank"
+          rel="noreferrer"
+        >
+          <Text as="span" size="lg" weight="semiBold" css={S.shortCutText}>
+            📘 내보낸 파일(.ics) 사용법
+          </Text>
+        </S.UserGuideLink>
+      </S.Container>
+    </Modal>
+  );
+};
+
+export default ICalendarModal;
