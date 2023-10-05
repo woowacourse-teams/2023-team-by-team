be04c209eaa13848efd6df018c917a08cef8b796
Suyoung Jung <jsy19970512@naver.com>
2023-10-05 12:56:24 +0900

    feat: 에러 발생 시 toast를 띄워주는 로직을 전역 QueryCache에 추가

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,35 +1,63 @@
 import { StrictMode } from 'react';
+import type { ReactNode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import {
+  QueryCache,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
 import App from '~/App';
 import GlobalStyle from '~/styles/GlobalStyle';
 import { theme } from './styles/theme';
 import { worker } from '~/mocks/browser';
 import { ToastProvider } from '~/components/common/Toast/ToastContext';
 import ToastList from '~/components/common/Toast/ToastList';
+import { useToast } from '~/hooks/useToast';
 
 if (process.env.WORKER === 'on') {
   worker.start();
 }
 
-const queryClient = new QueryClient();
+const _QueryClientProvider = ({ children }: { children: ReactNode }) => {
+  const { showToast } = useToast();
+  const queryClient = new QueryClient({
+    queryCache: new QueryCache({
+      onError: (error, query) => {
+        if (error instanceof Error) {
+          const customErrorMessage = query.meta?.errorMessage;
+
+          if (typeof customErrorMessage === 'string') {
+            showToast('error', customErrorMessage);
+            return;
+          }
+
+          showToast('error', error.message);
+        }
+      },
+    }),
+  });
+
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
 
 root.render(
   <StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme={theme}>
-        <ToastProvider>
+    <ThemeProvider theme={theme}>
+      <ToastProvider>
+        <_QueryClientProvider>
           <GlobalStyle />
           <BrowserRouter>
             <App />
           </BrowserRouter>
           <ToastList />
-        </ToastProvider>
-      </ThemeProvider>
-    </QueryClientProvider>
+        </_QueryClientProvider>
+      </ToastProvider>
+    </ThemeProvider>
   </StrictMode>,
 );
