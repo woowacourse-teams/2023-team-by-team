75ef51c6fe18a24eb478515dd8b975fc83838a43
Jae_Philip_Yang <pilyang.dev@gmail.com>
2023-10-06 17:52:17 +0900

    [BE] S3파일 업로드 후  cloud-front 캐시 제거 기능 구현 (#739)
    * chore: cloudfront 의존성 추가
    
    Co-authored-by: the9kim <the9rla@gmail.com>
    
    * feat: CloudFront create invalidation 구현
    
    Co-authored-by: the9kim <the9rla@gmail.com>
    
    * feat: 파일 업로드시 케시제거 기능 추가
    
    Co-authored-by: the9kim <the9rla@gmail.com>
    
    * fix: cloudfront client 초기화 오류 수정 - Region 추가
    
    Co-authored-by: the9kim <the9rla@gmail.com>
    
    * style: 누락된 final추가
    
    Co-authored-by: the9kim <the9rla@gmail.com>
    
    * refactor: npe방지
    
    Co-authored-by: the9kim <the9rla@gmail.com>
    
    ---------
    
    Co-authored-by: the9kim <the9rla@gmail.com>

--- backend/src/main/java/team/teamby/teambyteam/filesystem/awss3/configuration/S3Configuration.java
+++ backend/src/main/java/team/teamby/teambyteam/filesystem/awss3/configuration/S3Configuration.java
@@ -4,25 +4,39 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.cloudfront.CloudFrontClient;
 import software.amazon.awssdk.services.s3.S3Client;
 import team.teamby.teambyteam.filesystem.FileCloudUploader;
+import team.teamby.teambyteam.filesystem.awss3.CloudfrontCacheInvalidator;
 import team.teamby.teambyteam.filesystem.awss3.S3Uploader;
 
 @Configuration
 public class S3Configuration {
 
     @Value("${aws.s3.region}")
-    private String region;
+    private String s3Region;
+
+    @Value("${aws.cloud-front.region}")
+    private String cloudFrontRegion;
 
     @Bean
     public S3Client s3Client() {
         return S3Client.builder()
-                .region(Region.of(region))
+                .region(Region.of(s3Region))
                 .build();
     }
 
     @Bean
     public FileCloudUploader fileCloudUploader() {
-        return new S3Uploader(s3Client());
+        return new S3Uploader(s3Client(), cloudfrontCacheInvalidator());
+    }
+
+    @Bean
+    public CloudfrontCacheInvalidator cloudfrontCacheInvalidator() {
+        return new CloudfrontCacheInvalidator(
+                CloudFrontClient.builder()
+                        .region(Region.of(cloudFrontRegion))
+                        .build()
+        );
     }
 }
