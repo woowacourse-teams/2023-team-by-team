f5582aa1f247db68f5127465f059bd756d61b858
WizardRabbit <curious.wzrabbit@gmail.com>
2023-10-06 11:00:43 +0900

    test: 펼쳐보기/접기 버튼 컴포넌트에 해당하는 스토리북 업데이트
    - 더 이상 absolute를 사용하지 않음을 명시
    - 테마에 따른 다양한 스토리 추가

--- frontend/src/components/common/ExpandButton/ExpandButton.stories.tsx
+++ frontend/src/components/common/ExpandButton/ExpandButton.stories.tsx
@@ -3,7 +3,7 @@ import ExpandButton from './ExpandButton';
 
 /**
  * `ExpandButton` 은 스레드 및 공지 스레드에서의 펼치기/접기 기능을 사용하기 위한 버튼입니다.
- *  기본적으로 `position: absolute` 가 적용되어 있으며 부모 요소의 최하단에 배치됩니다.
+ *  **더 이상 이 컴포넌트는 `position: absolute`를 지니지 않도록 변경되었음에 유의하세요.**
  */
 const meta = {
   title: 'Feed/ExpandButton',
@@ -15,9 +15,8 @@ const meta = {
         <div
           style={{
             position: 'relative',
-            padding: '120px',
+            padding: '20px',
             backgroundColor: '#404040',
-            border: '1px solid #000',
           }}
         >
           <Story />
@@ -34,14 +33,30 @@ type Story = StoryObj<typeof meta>;
 /**
  * 검은 배경은 단지 컴포넌트의 랜더링 결과 잘 보이게 하기 위함이며, 실제로는 컴포넌트에 포함되지 않습니다.
  */
-export const NotExpanded: Story = {
+export const WhiteNotExpanded: Story = {
   args: {
     isExpanded: false,
+    theme: 'white',
   },
 };
 
-export const Expanded: Story = {
+export const WhiteExpanded: Story = {
   args: {
     isExpanded: true,
+    theme: 'white',
+  },
+};
+
+export const BlurpleNotExpanded: Story = {
+  args: {
+    isExpanded: false,
+    theme: 'blurple',
+  },
+};
+
+export const BlurpleExpanded: Story = {
+  args: {
+    isExpanded: true,
+    theme: 'blurple',
   },
 };
