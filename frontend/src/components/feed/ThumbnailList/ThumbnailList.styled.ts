b727b0e51054db0bde64bbdfc29d286d77d36da8
WizardRabbit <curious.wzrabbit@gmail.com>
2023-10-06 11:07:28 +0900

    refactor: 재사용을 위해 썸네일 리스트 컴포넌트의 padding을 bottom만 유지하고 모두 제거
    - bottom만 유지한 것은 overflow-x의 스크롤에 대응하기 위함

--- frontend/src/components/feed/ThumbnailList/ThumbnailList.styled.ts
+++ frontend/src/components/feed/ThumbnailList/ThumbnailList.styled.ts
@@ -2,10 +2,13 @@ import { styled } from 'styled-components';
 
 export const Container = styled.ul`
   display: flex;
+  overflow-x: auto;
+  overflow-y: hidden;
   flex-direction: row;
+  flex-shrink: 0;
   column-gap: 12px;
 
   width: 100%;
-  height: 136px;
-  padding: 20px;
+  height: 116px;
+  padding-bottom: 20px;
 `;
