744b6bc6f7fe98374242d3092faa17295f37413c
sh111-coder <ohk9134@naver.com>
2023-10-10 09:14:19 +0900

    refactor: null 처리 Objects 사용 리팩토링

--- backend/src/main/java/team/teamby/teambyteam/filesystem/util/FileUtil.java
+++ backend/src/main/java/team/teamby/teambyteam/filesystem/util/FileUtil.java
@@ -1,5 +1,6 @@
 package team.teamby.teambyteam.filesystem.util;
 
+import java.util.Objects;
 import org.springframework.web.multipart.MultipartFile;
 import team.teamby.teambyteam.feed.exception.FeedException;
 
@@ -9,7 +10,7 @@ public class FileUtil {
 
     public static String getFileExtension(final MultipartFile file) {
         final String originalFilename = file.getOriginalFilename();
-        if (originalFilename != null) {
+        if (Objects.nonNull(originalFilename)) {
             int dotIndex = originalFilename.lastIndexOf(".");
             if (dotIndex >= 0 && dotIndex < originalFilename.length() - 1) {
                 return originalFilename.substring(dotIndex + 1);
