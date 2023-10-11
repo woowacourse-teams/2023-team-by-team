c04e7af716ca117205377a25b1d3f92011cd7302
pilyang <pilyang.dev@gmail.com>
2023-10-11 16:54:55 +0900

    feat: event cache 제거 스케줄링 구현
    Co-authored-by: SproutMJ <mjk54565@gmail.com>

--- backend/src/main/java/team/teamby/teambyteam/sse/domain/TeamPlaceEmitterRepository.java
+++ backend/src/main/java/team/teamby/teambyteam/sse/domain/TeamPlaceEmitterRepository.java
@@ -1,8 +1,10 @@
 package team.teamby.teambyteam.sse.domain;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
 
+import java.time.LocalDateTime;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.stream.Collectors;
@@ -31,5 +33,16 @@ public class TeamPlaceEmitterRepository {
                 .stream()
                 .filter(entry -> entry.getKey().isPublishedTo(teamPlaceId))
                 .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+    }
+
+    @Scheduled(fixedDelay = 1000 * 60)
+    private void cacheRefreshSchedule() {
+        final LocalDateTime now = LocalDateTime.now();
+        eventCache.keySet().forEach(key -> {
+                    if (key.getTimeStamp().isBefore(now.minusMinutes(1))) {
+                        eventCache.remove(key);
+                    }
+                }
+        );
     }
 }
