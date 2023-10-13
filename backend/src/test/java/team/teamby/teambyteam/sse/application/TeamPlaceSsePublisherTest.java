9e2a63d4109362fd8898cf9e78edb8f431abc9de
pilyang <pilyang.dev@gmail.com>
2023-10-13 11:39:10 +0900

    test: SsePUblisher test 작성

---
+++ backend/src/test/java/team/teamby/teambyteam/sse/application/TeamPlaceSsePublisherTest.java
@@ -0,0 +1,104 @@
+package team.teamby.teambyteam.sse.application;
+
+import org.assertj.core.api.SoftAssertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyEmitter;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter;
+import org.springframework.web.servlet.mvc.method.annotation.SseEmitter.SseEventBuilder;
+import team.teamby.teambyteam.sse.domain.TeamPlaceEmitterId;
+import team.teamby.teambyteam.sse.domain.TeamPlaceEmitterRepository;
+import team.teamby.teambyteam.sse.domain.TeamPlaceSseEvent;
+
+import java.io.IOException;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
+class TeamPlaceSsePublisherTest {
+
+    @Autowired
+    private TeamPlaceSsePublisher teamPlaceSsePublisher;
+
+    @Autowired
+    private TeamPlaceEmitterRepository teamPlaceEmitterRepository;
+
+    @Test
+    @DisplayName("발행된 이벤트로 SSE를 전송한다")
+    void sendSseWithEmitter() throws IOException {
+        // given
+        final Long teamPlaceId = 1L;
+        final String eventName = "test_event";
+        final String data = "{message: event}";
+        final TestEvent testEvent = new TestEvent(teamPlaceId, eventName, data);
+
+        final TeamPlaceEmitterId emitterId = TeamPlaceEmitterId.of(teamPlaceId, 1L);
+
+        final SseEmitter sseEmitter = Mockito.mock(SseEmitter.class);
+        final SseEmitter savedEmitter = teamPlaceEmitterRepository.save(emitterId, sseEmitter);
+
+        // when
+        teamPlaceSsePublisher.publishEvent(testEvent);
+
+        // then
+
+        // verify if the SSE is sent
+        ArgumentCaptor<SseEventBuilder> argumentCaptor = ArgumentCaptor.forClass(SseEventBuilder.class);
+        verify(savedEmitter).send(argumentCaptor.capture());
+
+        // verify the content of the SSE
+        final Set<ResponseBodyEmitter.DataWithMediaType> properties = argumentCaptor.getValue().build();
+        final String publishedSse = properties.stream()
+                .map(ResponseBodyEmitter.DataWithMediaType::getData)
+                .map(Object::toString)
+                .collect(Collectors.joining());
+
+        final String[] eventOutputs = publishedSse.split("\n");
+
+        SoftAssertions.assertSoftly(softly -> {
+            softly.assertThat(eventOutputs[0]).startsWith("id:" + teamPlaceId + "_" + eventName + "_");
+            softly.assertThat(eventOutputs[1]).isEqualTo("event:" + eventName);
+            softly.assertThat(eventOutputs[2]).isEqualTo("data:{\"id\":1,\"data\":\"{message: event}\"}");
+        });
+
+        // remove cache
+        teamPlaceEmitterRepository.deleteById(emitterId);
+    }
+
+    private static class TestEvent implements TeamPlaceSseEvent {
+
+        private final Long id;
+        private final String name;
+        private final TestData data;
+
+        public TestEvent(final Long id, final String name, final String data) {
+            this.id = id;
+            this.name = name;
+            this.data = new TestData(id, data);
+        }
+
+        @Override
+        public Long getTeamPlaceId() {
+            return id;
+        }
+
+        @Override
+        public String getEventName() {
+            return name;
+        }
+
+        @Override
+        public Object getEvent() {
+            return data;
+        }
+
+        private record TestData(long id, String data) {
+        }
+    }
+}
