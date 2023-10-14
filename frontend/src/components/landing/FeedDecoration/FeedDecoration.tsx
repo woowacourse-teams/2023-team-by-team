7e3166e9e3c481a0c5b37b2f546bf29d8fd5ecc3
WizardRabbit <curious.wzrabbit@gmail.com>
2023-10-15 03:40:00 +0900

    refactor: 랜딩 컴포넌트들의 스타일 컴포넌트의 속성이 DOM에 노출되지 않도록 개선

--- frontend/src/components/landing/FeedDecoration/FeedDecoration.tsx
+++ frontend/src/components/landing/FeedDecoration/FeedDecoration.tsx
@@ -5,16 +5,16 @@ const FeedDecoration = () => {
   return (
     <S.Container>
       <S.SampleBadge />
-      <S.SampleThread css={S.sampleThread1}>
-        <S.WritingLine width="85%" delay="1s" color="light" />
-        <S.WritingLine width="55%" delay="1.3s" color="light" />
-        <S.WritingLine width="60%" delay="1.6s" color="light" />
+      <S.SampleThread $css={S.sampleThread1}>
+        <S.WritingLine $width="85%" $delay="1s" $color="light" />
+        <S.WritingLine $width="55%" $delay="1.3s" $color="light" />
+        <S.WritingLine $width="60%" $delay="1.6s" $color="light" />
       </S.SampleThread>
-      <S.SampleThread css={S.sampleThread2}>
-        <S.WritingLine width="70%" delay="2.8s" color="dark" />
-        <S.WritingLine width="30%" delay="3.1s" color="dark" />
+      <S.SampleThread $css={S.sampleThread2}>
+        <S.WritingLine $width="70%" $delay="2.8s" $color="dark" />
+        <S.WritingLine $width="30%" $delay="3.1s" $color="dark" />
       </S.SampleThread>
-      <S.SampleThread css={S.sampleThread3} />
+      <S.SampleThread $css={S.sampleThread3} />
       <S.CircleButtonsContainer>
         <S.CircleButton>
           <ArrowUpIcon />
