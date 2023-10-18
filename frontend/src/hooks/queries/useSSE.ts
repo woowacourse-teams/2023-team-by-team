42cbfc38bd0f9e421f992b220827a5884db11287
Rulu <79538610+hafnium1923@users.noreply.github.com>
2023-10-18 20:24:08 +0900

    [FE] sse 문제 해결 (#797)
    * refactor: 원래 로직으로 바꾸고 콘솔
    
    * refactor: 잘못 import된 것 삭제

--- frontend/src/hooks/queries/useSSE.ts
+++ frontend/src/hooks/queries/useSSE.ts
@@ -1,16 +1,17 @@
-import { useCallback, useEffect, useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useQueryClient } from '@tanstack/react-query';
 import { baseUrl } from '~/apis/http';
 import { EventSourcePolyfill } from 'event-source-polyfill';
 import { LOCAL_STORAGE_KEY } from '~/constants/localStorage';
-
 export const useSSE = (teamPlaceId: number) => {
-  const queryClient = useQueryClient();
   const [accessToken, setAccessToken] = useState(
     localStorage.getItem(LOCAL_STORAGE_KEY.ACCESS_TOKEN),
   );
+  const queryClient = useQueryClient();
 
   useEffect(() => {
+    console.log(accessToken);
+
     const handleChangeAccessToken = () => {
       setAccessToken(localStorage.getItem(LOCAL_STORAGE_KEY.ACCESS_TOKEN));
     };
@@ -20,7 +21,7 @@ export const useSSE = (teamPlaceId: number) => {
     return () => window.removeEventListener('storage', handleChangeAccessToken);
   }, []);
 
-  const connect = useCallback(() => {
+  useEffect(() => {
     if (!teamPlaceId) {
       return;
     }
@@ -37,12 +38,6 @@ export const useSSE = (teamPlaceId: number) => {
     eventSource.addEventListener('new_thread', () => {
       queryClient.invalidateQueries(['threadData', teamPlaceId]);
     });
-
-    eventSource.onerror = () => {
-      if (eventSource.readyState === EventSource.CLOSED) {
-        window.setTimeout(() => connect(), 0);
-      }
-    };
 
     return () => {
       eventSource.close();
