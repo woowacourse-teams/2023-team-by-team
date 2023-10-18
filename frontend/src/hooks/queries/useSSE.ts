4f335ee23aef76c03b2f0c5970b9f0c758c7beb0
Rulu <79538610+hafnium1923@users.noreply.github.com>
2023-10-18 19:50:47 +0900

    [FE] SSE 연결 로직 useCallback으로 감싸기 (#795)

--- frontend/src/hooks/queries/useSSE.ts
+++ frontend/src/hooks/queries/useSSE.ts
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { useQueryClient } from '@tanstack/react-query';
 import { baseUrl } from '~/apis/http';
 import { EventSourcePolyfill } from 'event-source-polyfill';
@@ -6,41 +6,46 @@ import { LOCAL_STORAGE_KEY } from '~/constants/localStorage';
 
 export const useSSE = (teamPlaceId: number) => {
   const queryClient = useQueryClient();
-
-  const connect = () => {
-    return new EventSourcePolyfill(
-      baseUrl + `/api/team-place/${teamPlaceId}/subscribe`,
-      {
-        headers: {
-          Authorization: `Bearer ${localStorage.getItem(
-            LOCAL_STORAGE_KEY.ACCESS_TOKEN,
-          )}`,
-        },
-      },
-    );
-  };
-
-  const eventSource = connect();
+  const [accessToken, setAccessToken] = useState(
+    localStorage.getItem(LOCAL_STORAGE_KEY.ACCESS_TOKEN),
+  );
 
   useEffect(() => {
+    const handleChangeAccessToken = () => {
+      setAccessToken(localStorage.getItem(LOCAL_STORAGE_KEY.ACCESS_TOKEN));
+    };
+
+    window.addEventListener('storage', handleChangeAccessToken);
+
+    return () => window.removeEventListener('storage', handleChangeAccessToken);
+  }, []);
+
+  const connect = useCallback(() => {
     if (!teamPlaceId) {
       return;
     }
+
+    const eventSource = new EventSourcePolyfill(
+      baseUrl + `/api/team-place/${teamPlaceId}/subscribe`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      },
+    );
 
     eventSource.addEventListener('new_thread', () => {
       queryClient.invalidateQueries(['threadData', teamPlaceId]);
     });
 
-    eventSource.onerror = (event) => {
-      console.log(event);
-      eventSource.close();
-      // if (eventSource.readyState === ) {
-      //   setEventSource(() => connect());
-      // }
+    eventSource.onerror = () => {
+      if (eventSource.readyState === EventSource.CLOSED) {
+        window.setTimeout(() => connect(), 0);
+      }
     };
 
     return () => {
       eventSource.close();
     };
-  }, [queryClient, teamPlaceId, eventSource]);
+  }, [queryClient, teamPlaceId, accessToken]);
 };
