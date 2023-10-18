dae75fb2dd8a5e3e464919f871aaf7f93fe2b125
Rulu <hafnium1923@gmail.com>
2023-10-18 14:58:38 +0900

    fix: 토큰 바뀌면 SSE도 대응하도록 변경

--- frontend/src/hooks/queries/useSSE.ts
+++ frontend/src/hooks/queries/useSSE.ts
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import type { RefObject } from 'react';
 import { useQueryClient } from '@tanstack/react-query';
 import { baseUrl } from '~/apis/http';
@@ -6,7 +6,20 @@ import { EventSourcePolyfill } from 'event-source-polyfill';
 import { LOCAL_STORAGE_KEY } from '~/constants/localStorage';
 
 export const useSSE = (teamPlaceId: number, ref: RefObject<HTMLDivElement>) => {
+  const [accessToken, setAccessToken] = useState(
+    localStorage.getItem(LOCAL_STORAGE_KEY.ACCESS_TOKEN),
+  );
   const queryClient = useQueryClient();
+
+  useEffect(() => {
+    const handleChangeAccessToken = () => {
+      setAccessToken(localStorage.getItem(LOCAL_STORAGE_KEY.ACCESS_TOKEN));
+    };
+
+    window.addEventListener('storage', handleChangeAccessToken);
+
+    return () => window.removeEventListener('storage', handleChangeAccessToken);
+  }, []);
 
   useEffect(() => {
     if (!teamPlaceId) {
@@ -17,9 +30,7 @@ export const useSSE = (teamPlaceId: number, ref: RefObject<HTMLDivElement>) => {
       baseUrl + `/api/team-place/${teamPlaceId}/subscribe`,
       {
         headers: {
-          Authorization: `Bearer ${localStorage.getItem(
-            LOCAL_STORAGE_KEY.ACCESS_TOKEN,
-          )}`,
+          Authorization: `Bearer ${accessToken}`,
         },
       },
     );
@@ -31,5 +42,5 @@ export const useSSE = (teamPlaceId: number, ref: RefObject<HTMLDivElement>) => {
     return () => {
       eventSource.close();
     };
-  }, [queryClient, teamPlaceId, ref]);
+  }, [queryClient, teamPlaceId, ref, accessToken]);
 };
