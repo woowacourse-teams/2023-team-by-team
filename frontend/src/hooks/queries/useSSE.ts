cad7116acc2845095d69c2c20591b9fd429f339d
Rulu <79538610+hafnium1923@users.noreply.github.com>
2023-10-18 17:42:18 +0900

    [FE] SSE 재연결하는 로직 수정 (#785)
    * fix: 재연결하는 로직 수정
    
    * refactor: onerror 로직 리펙터링

--- frontend/src/hooks/queries/useSSE.ts
+++ frontend/src/hooks/queries/useSSE.ts
@@ -1,46 +1,47 @@
 import { useEffect, useState } from 'react';
-import type { RefObject } from 'react';
 import { useQueryClient } from '@tanstack/react-query';
 import { baseUrl } from '~/apis/http';
 import { EventSourcePolyfill } from 'event-source-polyfill';
 import { LOCAL_STORAGE_KEY } from '~/constants/localStorage';
 
 export const useSSE = (teamPlaceId: number) => {
-  const [accessToken, setAccessToken] = useState(
-    localStorage.getItem(LOCAL_STORAGE_KEY.ACCESS_TOKEN),
-  );
   const queryClient = useQueryClient();
-
-  useEffect(() => {
-    const handleChangeAccessToken = () => {
-      setAccessToken(localStorage.getItem(LOCAL_STORAGE_KEY.ACCESS_TOKEN));
-    };
-
-    window.addEventListener('storage', handleChangeAccessToken);
-
-    return () => window.removeEventListener('storage', handleChangeAccessToken);
-  }, []);
+  const [eventSource, setEventSource] = useState<EventSourcePolyfill | null>();
 
   useEffect(() => {
     if (!teamPlaceId) {
       return;
     }
 
-    const eventSource = new EventSourcePolyfill(
-      baseUrl + `/api/team-place/${teamPlaceId}/subscribe`,
-      {
-        headers: {
-          Authorization: `Bearer ${accessToken}`,
+    const connect = () => {
+      return new EventSourcePolyfill(
+        baseUrl + `/api/team-place/${teamPlaceId}/subscribe`,
+        {
+          headers: {
+            Authorization: `Bearer ${localStorage.getItem(
+              LOCAL_STORAGE_KEY.ACCESS_TOKEN,
+            )}`,
+          },
         },
-      },
-    );
+      );
+    };
+
+    setEventSource(() => connect());
+
+    if (!eventSource) return;
 
     eventSource.addEventListener('new_thread', () => {
       queryClient.invalidateQueries(['threadData', teamPlaceId]);
     });
 
+    eventSource.onerror = () => {
+      if (eventSource.readyState === EventSource.CLOSED) {
+        window.setTimeout(() => setEventSource(() => connect()), 0);
+      }
+    };
+
     return () => {
       eventSource.close();
     };
-  }, [queryClient, teamPlaceId, accessToken]);
+  }, [queryClient, teamPlaceId, eventSource]);
 };
