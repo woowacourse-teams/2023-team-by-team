7e8153667d8a2a3beb25a0eaeed97119144d4453
Rulu <hafnium1923@gmail.com>
2023-10-19 14:23:32 +0900

    refactor: 등록 성공 시 조회 쿼리 무효화 구문 삭제

--- frontend/src/hooks/queries/useSendThread.ts
+++ frontend/src/hooks/queries/useSendThread.ts
@@ -1,25 +1,17 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { useMutation } from '@tanstack/react-query';
 import { sendThread } from '~/apis/feed';
 import type { ThreadContent } from '~/types/feed';
 
 export const useSendThread = (teamPlaceId: number) => {
-  const queryClient = useQueryClient();
-  const { mutate, isLoading } = useMutation(
-    (threadContent: ThreadContent) => {
-      const { content, images } = threadContent;
-      const formData = new FormData();
+  const { mutate, isLoading } = useMutation((threadContent: ThreadContent) => {
+    const { content, images } = threadContent;
+    const formData = new FormData();
 
-      formData.append('content', content);
-      images.forEach((image) => formData.append('images', image));
+    formData.append('content', content);
+    images.forEach((image) => formData.append('images', image));
 
-      return sendThread(teamPlaceId, formData);
-    },
-    {
-      onSuccess: () => {
-        queryClient.invalidateQueries(['threadData', teamPlaceId]);
-      },
-    },
-  );
+    return sendThread(teamPlaceId, formData);
+  });
 
   return { mutateSendThread: mutate, isSendThreadLoading: isLoading };
 };
