14e37ed22a5b1e145660b5688dd4bb3271aa3790
Suyoung <jsy19970512@naver.com>
2023-10-19 16:59:58 +0900

    [FE] iCalendar url에 placeholder 적용하여 빈 input을 보지 않도록 수정 (#812)
    * refactor: iCalendar url에 placeholder 적용하여 빈 input을 보지 않도록 수정
    
    * refactor: url path를 prod로 수정

--- frontend/src/hooks/queries/useFetchICalendarUrl.ts
+++ frontend/src/hooks/queries/useFetchICalendarUrl.ts
@@ -11,6 +11,7 @@ export const useFetchICalendarUrl = (teamPlaceId: number) => {
         errorMessage:
           '일정 내보내기에 실패했습니다. \n지속되는 경우 관리자에게 문의해주세요.',
       },
+      placeholderData: { url: 'https://assets.teamby.team/prod/ical/1' },
     },
   );
 
