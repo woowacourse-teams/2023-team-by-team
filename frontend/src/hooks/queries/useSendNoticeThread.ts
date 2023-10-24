53a7ebedb59f9e8d4086eb187dab7a1dfecfe26b
Rulu <79538610+hafnium1923@users.noreply.github.com>
2023-10-24 16:28:24 +0900

    [FE] 공지 등록 후 공지 쿼리 무효화 추가 (#823)

--- frontend/src/hooks/queries/useSendNoticeThread.ts
+++ frontend/src/hooks/queries/useSendNoticeThread.ts
@@ -1,17 +1,25 @@
-import { useMutation } from '@tanstack/react-query';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { sendNoticeThread } from '~/apis/feed';
 import type { ThreadContent } from '~/types/feed';
 
 export const useSendNoticeThread = (teamPlaceId: number) => {
-  const { mutate, isLoading } = useMutation((threadContent: ThreadContent) => {
-    const { content, images } = threadContent;
-    const formData = new FormData();
+  const queryClient = useQueryClient();
+  const { mutate, isLoading } = useMutation(
+    (threadContent: ThreadContent) => {
+      const { content, images } = threadContent;
+      const formData = new FormData();
 
-    formData.append('content', content);
-    images.forEach((image) => formData.append('images', image));
+      formData.append('content', content);
+      images.forEach((image) => formData.append('images', image));
 
-    return sendNoticeThread(teamPlaceId, formData);
-  });
+      return sendNoticeThread(teamPlaceId, formData);
+    },
+    {
+      onSuccess: () => {
+        queryClient.invalidateQueries(['noticeThread', teamPlaceId]);
+      },
+    },
+  );
 
   return {
     mutateSendNoticeThread: mutate,
