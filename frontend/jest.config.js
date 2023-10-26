34993f5063c2d8daba1fb69c98887b8f469d73ff
Suyoung Jung <jsy19970512@naver.com>
2023-10-26 13:47:35 +0900

    refactor: ts-jest를 @swc/jest로 마이그레이션

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -1,6 +1,7 @@
-/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'ts-jest',
+  transform: {
+    '^.+\\.(t|j)sx?$': '@swc/jest',
+  },
   testEnvironment: 'node',
   moduleNameMapper: {
     '^~/(.*)$': '<rootDir>/src/$1',
