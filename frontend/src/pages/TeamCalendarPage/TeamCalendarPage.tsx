57c6c1f885a1668966d3f1bb439e7faee142c9cd
Rulu <hafnium1923@gmail.com>
2023-11-01 00:35:58 +0900

    feat: 팀 캘린더 임시로 모바일 대응

--- frontend/src/pages/TeamCalendarPage/TeamCalendarPage.tsx
+++ frontend/src/pages/TeamCalendarPage/TeamCalendarPage.tsx
@@ -1,10 +1,13 @@
 import TeamCalendar from '~/components/team_calendar/TeamCalendar/TeamCalendar';
 import * as S from './TeamCalendarPage.styled';
+import { useCheckMobileWeb } from '~/hooks/useCheckMobileWeb';
 
 const TeamCalendarPage = () => {
+  const isMobile = useCheckMobileWeb();
+
   return (
     <S.Container>
-      <TeamCalendar />
+      <TeamCalendar calendarSize={isMobile ? 'sm' : 'md'} />
     </S.Container>
   );
 };
