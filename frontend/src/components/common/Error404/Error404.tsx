4857dda4aaa28ce9b1bd9920bb12ab4fcaf06a8a
Rulu <hafnium1923@gmail.com>
2023-10-31 23:37:57 +0900

    feat: 에러페이지 모바일 대응

--- frontend/src/components/common/Error404/Error404.tsx
+++ frontend/src/components/common/Error404/Error404.tsx
@@ -3,6 +3,7 @@ import { error404Image, error404TextImage } from '~/assets/webp';
 import Text from '~/components/common/Text/Text';
 import BackButton from '~/components/common/BackButton/BackButton';
 import { PATH_NAME } from '~/constants/routes';
+import { useCheckMobileWeb } from '~/hooks/useCheckMobileWeb';
 
 interface Error404Props {
   isLoggedIn: boolean;
@@ -10,18 +11,38 @@ interface Error404Props {
 
 const Error404 = (props: Error404Props) => {
   const { isLoggedIn } = props;
+  const isMobile = useCheckMobileWeb();
+  const location = window.location.href;
 
   return (
     <S.Container>
       <S.ErrorImage src={error404Image} alt="해당 페이지를 찾을 수 없어요" />
       <S.ErrorDetails>
         <S.ErrorTextImage src={error404TextImage} alt="404" />
-        <Text size="xxl" weight="bold" css={S.errorText}>
-          해당 페이지를 찾을 수 없어요!
-        </Text>
+        {isMobile && location.includes('overview') ? (
+          <Text size="xxl" weight="bold" css={S.errorText}>
+            모바일에서는 이용할 수 없는 페이지에요!
+          </Text>
+        ) : (
+          <Text size="xxl" weight="bold" css={S.errorText}>
+            해당 페이지를 찾을 수 없어요!
+          </Text>
+        )}
         <BackButton
-          href={isLoggedIn ? PATH_NAME.TEAM_OVERVIEW : PATH_NAME.LANDING}
-          label={isLoggedIn ? '모아보기 페이지로' : '랜딩 페이지로'}
+          href={
+            isLoggedIn
+              ? isMobile
+                ? PATH_NAME.TEAM_CALENDAR
+                : PATH_NAME.TEAM_OVERVIEW
+              : PATH_NAME.LANDING
+          }
+          label={
+            isLoggedIn
+              ? isMobile
+                ? '팀 캘린더 페이지로'
+                : '모아보기 페이지로'
+              : '랜딩 페이지로'
+          }
         />
       </S.ErrorDetails>
     </S.Container>
--- frontend/src/pages/Error404Page/Error404Page.tsx
+++ frontend/src/pages/Error404Page/Error404Page.tsx
@@ -4,10 +4,12 @@ import { PATH_NAME } from '~/constants/routes';
 import LandingHeader from '~/components/common/LandingHeader/LandingHeader';
 import Error404 from '~/components/common/Error404/Error404';
 import { useToken } from '~/hooks/useToken';
+import { useCheckMobileWeb } from '~/hooks/useCheckMobileWeb';
 
 const Error404Page = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(true);
   const { accessToken } = useToken();
+  const isMobile = useCheckMobileWeb();
 
   useEffect(() => {
     const isLoggedIn = Boolean(accessToken);
@@ -18,7 +20,13 @@ const Error404Page = () => {
   return (
     <S.Container>
       <LandingHeader
-        href={isLoggedIn ? PATH_NAME.TEAM_OVERVIEW : PATH_NAME.LANDING}
+        href={
+          isLoggedIn
+            ? isMobile
+              ? PATH_NAME.TEAM_CALENDAR
+              : PATH_NAME.TEAM_OVERVIEW
+            : PATH_NAME.LANDING
+        }
       />
       <S.Error404Wrapper>
         <Error404 isLoggedIn={isLoggedIn} />
