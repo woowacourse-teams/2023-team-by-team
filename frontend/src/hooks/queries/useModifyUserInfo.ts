54a733c08221b999e6355846867c3b41e1bf3657
Suyoung Jung <jsy19970512@naver.com>
2023-11-01 16:06:40 +0900

    feat: 프로필 닉네임 수정 기능에 낙관적 업데이트 적용

--- frontend/src/hooks/queries/useModifyUserInfo.ts
+++ frontend/src/hooks/queries/useModifyUserInfo.ts
@@ -2,13 +2,38 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { modifyUserInfo } from '~/apis/user';
 import type { UserInfo } from '~/types/team';
 
+const QUERY_KEY = ['userInfo'];
+
 export const useModifyUserInfo = () => {
   const queryClient = useQueryClient();
   const { mutate } = useMutation(
     (body: Pick<UserInfo, 'name'>) => modifyUserInfo(body),
     {
+      onMutate: async (body) => {
+        await queryClient.cancelQueries(QUERY_KEY);
+
+        const previousUserInfo = queryClient.getQueryData<UserInfo>(QUERY_KEY);
+
+        if (previousUserInfo) {
+          queryClient.setQueryData<UserInfo>(QUERY_KEY, (old) => {
+            if (old) {
+              return { ...old, ...body };
+            }
+          });
+        }
+
+        return { previousUserInfo };
+      },
+      onError: (err, newName, context) => {
+        if (context?.previousUserInfo) {
+          queryClient.setQueryData<UserInfo>(
+            QUERY_KEY,
+            context.previousUserInfo,
+          );
+        }
+      },
       onSuccess: () => {
-        queryClient.invalidateQueries(['userInfo']);
+        queryClient.invalidateQueries(QUERY_KEY);
       },
     },
   );
