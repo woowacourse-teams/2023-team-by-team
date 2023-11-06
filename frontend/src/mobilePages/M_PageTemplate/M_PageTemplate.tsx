b8bc6d3a5bada14cc954cd372393ed3cb4d08595
Rulu <hafnium1923@gmail.com>
2023-11-06 15:43:46 +0900

    refactor: 모바일에도 SSE 연결 넣기

--- frontend/src/mobilePages/M_PageTemplate/M_PageTemplate.tsx
+++ frontend/src/mobilePages/M_PageTemplate/M_PageTemplate.tsx
@@ -3,8 +3,14 @@ import NavigationBar from '~/components/common/NavigationBar/NavigationBar';
 import { Outlet } from 'react-router-dom';
 import Header from '~/components/common/Header/Header';
 import { ModalProvider } from '~/components/common/Modal/ModalContext';
+import { useTeamPlace } from '~/hooks/useTeamPlace';
+import { useSSE } from '~/hooks/queries/useSSE';
 
 const M_PageTemplate = () => {
+  const { teamPlaceId } = useTeamPlace();
+
+  useSSE(teamPlaceId);
+
   return (
     <S.PageContainer>
       <ModalProvider>
