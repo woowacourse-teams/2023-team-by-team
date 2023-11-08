d5a39c29b2d95edff3558ea4f55e2901c87fa247
WizardRabbit <curious.wzrabbit@gmail.com>
2023-11-08 22:25:30 +0900

    feat: ScheduleIndicator 컴포넌트 구현
    - 스케줄 바를 인디케이터 형태로 보여주는 컴포넌트

---
+++ frontend/src/components/team_calendar/ScheduleBarsIndicator/ScheduleBarsIndicator.styled.ts
@@ -0,0 +1,15 @@
+import { styled } from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  position: absolute;
+
+  width: 100%;
+  height: 100%;
+`;
+
+export const CalendarRow = styled.div`
+  position: relative;
+  flex-grow: 1;
+`;
