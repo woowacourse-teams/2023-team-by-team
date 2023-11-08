dcb471ef26c996d7b545fa7a078fdb09323548ce
WizardRabbit <curious.wzrabbit@gmail.com>
2023-11-08 12:47:29 +0900

    feat: 날짜 포맷을 받아 n일 지난 날짜 포맷을 리턴해 주는 유틸 함수 작성

--- frontend/src/components/team_calendar/CalendarDragScreen/generateMovingScheduleBars.ts
+++ frontend/src/components/team_calendar/CalendarDragScreen/generateMovingScheduleBars.ts
@@ -1,3 +1,4 @@
+import type { Schedule } from '~/types/schedule';
 const getCalendarGridDifference = (
   relativeX: number,
   relativeY: number,
@@ -14,4 +15,21 @@ const getCalendarGridDifference = (
       : Math.ceil((relativeX * 7) / calendarWidth);
 
   return { rowDifference, columnDifference };
+};
+
+const changeDateTimeByDays = (
+  dateTime: Schedule['startDateTime'],
+  days: number,
+) => {
+  const changedDate = new Date(Number(new Date(dateTime)) + 86_400_000 * days);
+
+  const year = String(changedDate.getFullYear()).padStart(4, '0');
+  const month = String(changedDate.getMonth() + 1).padStart(2, '0');
+  const day = String(changedDate.getDate()).padStart(2, '0');
+  const time = dateTime.split(' ')[1];
+  const [minute, second] = time.split(':');
+
+  const changedDateTime: Schedule['startDateTime'] = `${year}-${month}-${day} ${minute}:${second}`;
+
+  return changedDateTime;
 };
