8fd537e924175aa6e67d6833d7d7b76846ff5f21
WizardRabbit <curious.wzrabbit@gmail.com>
2023-11-09 11:53:23 +0900

    feat: CalendarDragScreen에 통합된 가짜 스케줄 바 컴포넌트를 부착

--- frontend/src/components/team_calendar/CalendarDragScreen/CalendarDragScreen.tsx
+++ frontend/src/components/team_calendar/CalendarDragScreen/CalendarDragScreen.tsx
@@ -1,6 +1,6 @@
 import * as S from './CalendarDragScreen.styled';
 import { useRef } from 'react';
-import MovingScheduleBars from '~/components/team_calendar/MovingScheduleBars/MovingScheduleBars';
+import FakeScheduleBarsScreen from '~/components/team_calendar/FakeScheduleBarsScreen/FakeScheduleBarsScreen';
 import type { Schedule } from '~/types/schedule';
 import type { CalendarSize } from '~/types/size';
 import { useCalendarDragScreen } from './useCalendarDragScreen';
@@ -30,27 +30,32 @@ const CalendarDragScreen = (props: CalendarDragScreenProps) => {
     onMouseUp,
   } = props;
   const calendarRef = useRef<HTMLDivElement>(null);
-  const { movingScheduleBars, relativeX, relativeY } = useCalendarDragScreen({
-    visible,
-    initX,
-    initY,
-    calendarRef,
-    calendarSize,
-    onMouseUp,
-    year,
-    month,
-    level,
-    schedule,
-  });
+  const { movingScheduleBars, scheduleBarsIndicator, relativeX, relativeY } =
+    useCalendarDragScreen({
+      visible,
+      initX,
+      initY,
+      calendarRef,
+      calendarSize,
+      onMouseUp,
+      year,
+      month,
+      level,
+      schedule,
+    });
 
   return (
     <S.Container $visible={visible} ref={calendarRef}>
-      <MovingScheduleBars
+      <FakeScheduleBarsScreen
+        mode="indicator"
+        scheduleBars={scheduleBarsIndicator}
+      />
+      <FakeScheduleBarsScreen
+        mode="schedule"
         scheduleBars={movingScheduleBars}
         relativeX={relativeX}
         relativeY={relativeY}
       />
-      {/* <ScheduleBarIndicator schedule={schedule} /> */}
     </S.Container>
   );
 };
