520de20e0396913af9267964a4d72b4134146443
Suyoung <jsy19970512@naver.com>
2023-11-10 19:50:03 +0900

    [FE] 프로필 닉네임 수정 낙관적 업데이트 적용 (#849)
    * feat: 프로필 닉네임 수정 기능에 낙관적 업데이트 적용
    
    * refactor: 닉네임 수정 시 낙관적으로 UI를 업데이트하고, 실패 시 되돌아가도록 수정
    
    * refactor: onSettled 발생 시에 쿼리를 무효화하도록 수정

--- frontend/src/hooks/queries/useModifyUserInfo.ts
+++ frontend/src/hooks/queries/useModifyUserInfo.ts
@@ -2,13 +2,38 @@ import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { modifyUserInfo } from '~/apis/user';
 import type { UserInfo } from '~/types/team';
 
+const QUERY_KEY = ['userInfo'];
+
 export const useModifyUserInfo = () => {
   const queryClient = useQueryClient();
   const { mutate } = useMutation(
     (body: Pick<UserInfo, 'name'>) => modifyUserInfo(body),
     {
-      onSuccess: () => {
-        queryClient.invalidateQueries(['userInfo']);
+      onMutate: async (body) => {
+        await queryClient.cancelQueries(QUERY_KEY);
+
+        const previousUserInfo = queryClient.getQueryData<UserInfo>(QUERY_KEY);
+
+        if (previousUserInfo) {
+          queryClient.setQueryData<UserInfo>(QUERY_KEY, (old) => {
+            if (old) {
+              return { ...old, ...body };
+            }
+          });
+        }
+
+        return { previousUserInfo };
+      },
+      onError: (err, newName, context) => {
+        if (context?.previousUserInfo) {
+          queryClient.setQueryData<UserInfo>(
+            QUERY_KEY,
+            context.previousUserInfo,
+          );
+        }
+      },
+      onSettled: () => {
+        queryClient.invalidateQueries(QUERY_KEY);
       },
     },
   );
