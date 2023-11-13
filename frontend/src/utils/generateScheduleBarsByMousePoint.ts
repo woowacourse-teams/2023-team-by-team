6d31c42da69aca1ce19e3ef3e31ccd3ae1928f43
WizardRabbit <curious.wzrabbit@gmail.com>
2023-11-13 17:30:03 +0900

    refactor: 캘린더 날짜의 변화값을 구하는 유틸함수의 이름명 변경, 내부 변수 상수화

--- frontend/src/utils/generateScheduleBarsByMousePoint.ts
+++ frontend/src/utils/generateScheduleBarsByMousePoint.ts
@@ -1,4 +1,5 @@
 import { generateScheduleBars } from '~/utils/generateScheduleBars';
+import { CALENDAR, ONE_DAY } from '~/constants/calendar';
 import type { Schedule } from '~/types/schedule';
 import type { CalendarSize } from '~/types/size';
 
@@ -13,36 +14,6 @@ interface GenerateScheduleBarsByMousePointProps {
   level: number;
   calendarSize: CalendarSize;
 }
-
-const getCalendarGridDifference = (
-  relativeX: number,
-  relativeY: number,
-  calendarWidth: number,
-  calendarHeight: number,
-) => {
-  const rowDifference = Math.round((relativeY * 6) / calendarHeight);
-  const columnDifference = Math.round((relativeX * 7) / calendarWidth);
-  const calculatedDifference = rowDifference * 7 + columnDifference;
-
-  return calculatedDifference;
-};
-
-const changeDateTimeByDays = (
-  dateTime: Schedule['startDateTime'],
-  days: number,
-) => {
-  const changedDate = new Date(Number(new Date(dateTime)) + 86_400_000 * days);
-
-  const year = String(changedDate.getFullYear()).padStart(4, '0');
-  const month = String(changedDate.getMonth() + 1).padStart(2, '0');
-  const day = String(changedDate.getDate()).padStart(2, '0');
-  const time = dateTime.split(' ')[1];
-  const [minute, second] = time.split(':');
-
-  const changedDateTime: Schedule['startDateTime'] = `${year}-${month}-${day} ${minute}:${second}`;
-
-  return changedDateTime;
-};
 
 export const generateScheduleBarsByMousePoint = (
   params: GenerateScheduleBarsByMousePointProps,
@@ -59,7 +30,7 @@ export const generateScheduleBarsByMousePoint = (
     calendarSize,
   } = params;
 
-  const difference = getCalendarGridDifference(
+  const difference = getCalendarDateDifferenceByMousePoint(
     relativeX,
     relativeY,
     calendarWidth,
@@ -79,4 +50,39 @@ export const generateScheduleBarsByMousePoint = (
   }));
 
   return generatedScheduleBars;
+};
+
+const getCalendarDateDifferenceByMousePoint = (
+  relativeX: number,
+  relativeY: number,
+  calendarWidth: number,
+  calendarHeight: number,
+) => {
+  const rowDifference = Math.round(
+    (relativeY * CALENDAR.ROW_SIZE) / calendarHeight,
+  );
+  const columnDifference = Math.round(
+    (relativeX * CALENDAR.COLUMN_SIZE) / calendarWidth,
+  );
+  const calculatedDifference =
+    rowDifference * CALENDAR.COLUMN_SIZE + columnDifference;
+
+  return calculatedDifference;
+};
+
+const changeDateTimeByDays = (
+  dateTime: Schedule['startDateTime'],
+  days: number,
+) => {
+  const changedDate = new Date(Number(new Date(dateTime)) + ONE_DAY * days);
+
+  const year = String(changedDate.getFullYear()).padStart(4, '0');
+  const month = String(changedDate.getMonth() + 1).padStart(2, '0');
+  const day = String(changedDate.getDate()).padStart(2, '0');
+  const time = dateTime.split(' ')[1];
+  const [minute, second] = time.split(':');
+
+  const changedDateTime: Schedule['startDateTime'] = `${year}-${month}-${day} ${minute}:${second}`;
+
+  return changedDateTime;
 };
