71179a256df530d1f92efae344a4901f7528b56d
Rulu <79538610+hafnium1923@users.noreply.github.com>
2023-11-30 18:15:35 +0900

    [FE] useSSE 연결을 useCallback로 감싸 한번만 작동하도록 변경 (#874)

--- frontend/src/hooks/queries/useSSE.ts
+++ frontend/src/hooks/queries/useSSE.ts
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useCallback, useEffect } from 'react';
 import { useQueryClient } from '@tanstack/react-query';
 import { baseUrl } from '~/apis/http';
 import { EventSourcePolyfill } from 'event-source-polyfill';
@@ -9,37 +9,34 @@ export const useSSE = () => {
   const queryClient = useQueryClient();
   const { accessToken } = useToken();
   const { teamPlaceId } = useTeamPlace();
-  const currentId = teamPlaceId;
+
+  const connect = useCallback(() => {
+    console.log(teamPlaceId);
+    if (!teamPlaceId) {
+      return;
+    }
+
+    const eventSource = new EventSourcePolyfill(
+      baseUrl + `/api/team-place/${teamPlaceId}/subscribe`,
+      {
+        headers: {
+          Authorization: `Bearer ${accessToken}`,
+        },
+      },
+    );
+
+    eventSource.addEventListener('new_thread', (e) => {
+      console.log('1 ' + e.data);
+
+      queryClient.invalidateQueries(['threadData', teamPlaceId]);
+    });
+
+    return () => {
+      eventSource.close();
+    };
+  }, [queryClient, teamPlaceId, accessToken]);
 
   useEffect(() => {
-    const connect = () => {
-      console.log(teamPlaceId);
-      if (!teamPlaceId) {
-        return;
-      }
-
-      const eventSource = new EventSourcePolyfill(
-        baseUrl + `/api/team-place/${teamPlaceId}/subscribe`,
-        {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        },
-      );
-
-      eventSource.addEventListener('new_thread', (e) => {
-        console.log('1 ' + e.data);
-
-        queryClient.invalidateQueries(['threadData', teamPlaceId]);
-      });
-
-      if (currentId !== teamPlaceId) eventSource.close();
-
-      return () => {
-        eventSource.close();
-      };
-    };
-
     connect();
-  }, [queryClient, teamPlaceId, accessToken]);
+  }, [connect]);
 };
