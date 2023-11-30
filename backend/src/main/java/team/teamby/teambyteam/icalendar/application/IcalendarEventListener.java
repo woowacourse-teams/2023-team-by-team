acde40a886d7b22d13e84ebb409e8f64dbf468dd
Jae_Philip_Yang <pilyang.dev@gmail.com>
2023-11-30 15:57:08 +0900

    [BE] 일정변동이 잦은 경우 ics배포  (#861)
    * feat: IcalendarPublishCounter 구현
    
    * feat: 배포가 밀린 ics파일들 배포 스케쥴 구현
    
    * fix: 잘못선언된 레포지터리 메서드 제거
    
    * refactor: 주기적으로 ics업데이트하는 중간 클래스 생성
    
    - 기존 Scheduler를 주기적으로 ics배포를 제어하는 중간클래스로 변경
    
    * feat: 딜레이된 파일 배포시 딜레이 안된 카우트 초기화 기능 추가
    
    * fix: 10회까지 배포, 10회 초과시 딜레이

--- backend/src/main/java/team/teamby/teambyteam/icalendar/application/IcalendarEventListener.java
+++ backend/src/main/java/team/teamby/teambyteam/icalendar/application/IcalendarEventListener.java
@@ -1,7 +1,6 @@
 package team.teamby.teambyteam.icalendar.application;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Propagation;
@@ -12,40 +11,30 @@ import team.teamby.teambyteam.icalendar.application.event.CreateIcalendarEvent;
 import team.teamby.teambyteam.schedule.application.event.ScheduleEvent;
 import team.teamby.teambyteam.teamplace.application.event.TeamPlaceCreatedEvent;
 
-@Slf4j
 @Component
 @RequiredArgsConstructor
 public class IcalendarEventListener {
 
-    private final IcalendarPublishService icalendarPublishService;
+    private final PeriodicIcalendarPublishService periodicIcalendarPublishService;
 
     @Async
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void createIcalendar(final TeamPlaceCreatedEvent teamPlaceCreatedEvent) {
-        final Long teamPlaceId = teamPlaceCreatedEvent.teamPlaceId();
-
-        icalendarPublishService.createAndPublishIcalendar(teamPlaceId);
-        log.info("ics파일 생성 - teamPlaceId : {}", teamPlaceId);
+        periodicIcalendarPublishService.createAndPublishIcalendar(teamPlaceCreatedEvent.teamPlaceId());
     }
 
     @Async
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void createIcalendar(final CreateIcalendarEvent createIcalendarEvent) {
-        final Long teamPlaceId = createIcalendarEvent.teamPlaceId();
-
-        icalendarPublishService.createAndPublishIcalendar(teamPlaceId);
-        log.info("ics파일 생성 - teamPlaceId : {}", teamPlaceId);
+        periodicIcalendarPublishService.createAndPublishIcalendar(createIcalendarEvent.teamPlaceId());
     }
 
     @Async
     @Transactional(propagation = Propagation.REQUIRES_NEW)
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void updateIcalendar(final ScheduleEvent scheduleEvent) {
-        final Long teamPlaceId = scheduleEvent.getTeamPlaceId();
-
-        icalendarPublishService.updateIcalendar(teamPlaceId);
-        log.info("ics파일 업데이트 - teamPlaceId : {}", teamPlaceId);
+        periodicIcalendarPublishService.updateIcalendar(scheduleEvent.getTeamPlaceId());
     }
 }
---
+++ backend/src/main/java/team/teamby/teambyteam/icalendar/application/PeriodicIcalendarPublishService.java
@@ -0,0 +1,48 @@
+package team.teamby.teambyteam.icalendar.application;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import team.teamby.teambyteam.icalendar.domain.IcalendarPublishCounter;
+
+import java.util.List;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class PeriodicIcalendarPublishService {
+
+    private final IcalendarPublishService icalendarPublishService;
+    private final IcalendarPublishCounter publishCounter;
+
+    public void createAndPublishIcalendar(final Long teamPlaceId) {
+        icalendarPublishService.createAndPublishIcalendar(teamPlaceId);
+        log.info("ics파일 생성 - teamPlaceId : {}", teamPlaceId);
+    }
+
+    public void updateIcalendar(final Long teamPlaceId) {
+        if (publishCounter.isReachedToMaxCount(teamPlaceId)) {
+            log.warn("ics배포 과요청으로 배포 연기 : {}", teamPlaceId);
+            return;
+        }
+
+        icalendarPublishService.updateIcalendar(teamPlaceId);
+        log.info("ics파일 업데이트 - teamPlaceId : {}", teamPlaceId);
+        publishCounter.addCountFor(teamPlaceId);
+    }
+
+    /**
+     * 매시 45분에 밀린 icalendar 배포 수행
+     */
+    @Scheduled(cron = "0 45 * * * *")
+    private void publishDelayedIcalendarUpdates() {
+        publishCounter.clearNotDelayedCounts();
+
+        final List<Long> teamPlaceIds = publishCounter.getPublishDelayedTeamPlaceIds();
+        teamPlaceIds.forEach(teamPlaceId -> {
+            icalendarPublishService.updateIcalendar(teamPlaceId);
+            publishCounter.clearFor(teamPlaceId);
+        });
+    }
+}
