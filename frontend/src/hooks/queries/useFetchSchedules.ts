f83e85e6c294f59c50e128d8441cf6bdc49c8389
WizardRabbit <curious.wzrabbit@gmail.com>
2023-12-11 02:00:18 +0900

    fix: 일부 일정이 달력에 보이지 않는 문제 해결
    - 보이지 않는 일부 일정은 현재 월과 하루도 겹치지 않는 일정인데, 이 경우에도 달력에는 포함되어야 하는 경우가 있음
    - 이를 해결하기 위해 useQureies를 사용하였고, n월의 스케줄 정보를 가져올 때 n - 1, n, n + 1월의 일정을 모두 가져오도록 변경하였음

--- frontend/src/hooks/queries/useFetchSchedules.ts
+++ frontend/src/hooks/queries/useFetchSchedules.ts
@@ -1,24 +1,67 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQueries } from '@tanstack/react-query';
 import { fetchSchedules } from '~/apis/schedule';
 import { STALE_TIME } from '~/constants/query';
+import type { Schedule } from '~/types/schedule';
 
 export const useFetchSchedules = (
   teamPlaceId: number,
   year: number,
   month: number,
 ) => {
-  const { data } = useQuery(
-    ['schedules', teamPlaceId, year, month],
-    () => fetchSchedules(teamPlaceId, year, month + 1),
-    {
-      enabled: teamPlaceId > 0,
-      staleTime: STALE_TIME.SCHEDULES,
-    },
-  );
+  const previousQueryYear = month === 0 ? year - 1 : year;
+  const previousQueryMonth = month === 0 ? 12 : month;
+  const nextQueryYear = month === 11 ? year + 1 : year;
+  const nextQueryMonth = month === 11 ? 1 : month + 2;
 
-  if (data === undefined) return [];
+  const [previousQuery, currentQuery, nextQuery] = useQueries({
+    queries: [
+      {
+        queryKey: [
+          'schedules',
+          teamPlaceId,
+          previousQueryYear,
+          previousQueryMonth,
+        ],
+        queryFn: () =>
+          fetchSchedules(teamPlaceId, previousQueryYear, previousQueryMonth),
+        enabled: teamPlaceId > 0,
+        staleTime: STALE_TIME.SCHEDULES,
+      },
+      {
+        queryKey: ['schedules', teamPlaceId, year, month],
+        queryFn: () => fetchSchedules(teamPlaceId, year, month + 1),
+        enabled: teamPlaceId > 0,
+        staleTime: STALE_TIME.SCHEDULES,
+      },
+      {
+        queryKey: ['schedules', teamPlaceId, nextQueryYear, nextQueryMonth],
+        queryFn: () =>
+          fetchSchedules(teamPlaceId, nextQueryYear, nextQueryMonth),
+        enabled: teamPlaceId > 0,
+        staleTime: STALE_TIME.SCHEDULES,
+      },
+    ],
+  });
 
-  const { schedules } = data;
+  if (!previousQuery.data || !currentQuery.data || !nextQuery.data) {
+    return [];
+  }
 
-  return schedules;
+  const rawSchedules = [
+    ...previousQuery.data.schedules,
+    ...currentQuery.data.schedules,
+    ...nextQuery.data.schedules,
+  ];
+
+  const uniqueSchedules: Schedule[] = [];
+  const appearedScheduleIds = new Set();
+
+  rawSchedules.forEach((schedule) => {
+    if (!appearedScheduleIds.has(schedule.id)) {
+      uniqueSchedules.push(schedule);
+      appearedScheduleIds.add(schedule.id);
+    }
+  });
+
+  return uniqueSchedules;
 };
