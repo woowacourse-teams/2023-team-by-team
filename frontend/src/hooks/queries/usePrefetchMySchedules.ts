b78b8c67e655fda39147643dc984b9e1cb43ed7c
WizardRabbit <curious.wzrabbit@gmail.com>
2023-12-19 01:59:59 +0900

    refactor: 일정 조회 프리페칭 로직에 변경된 일정 명세 적용

--- frontend/src/hooks/queries/usePrefetchMySchedules.ts
+++ frontend/src/hooks/queries/usePrefetchMySchedules.ts
@@ -1,14 +1,21 @@
 import { useQueryClient } from '@tanstack/react-query';
 import { fetchMySchedules } from '~/apis/schedule';
 import { STALE_TIME } from '~/constants/query';
+import { generateYYYYMMDD } from '~/utils/generateYYYYMMDD';
 
-export const usePrefetchMySchedules = async (year: number, month: number) => {
-  const queryKey = ['mySchedules', year, month];
+export const usePrefetchMySchedules = async (
+  startDate: Date,
+  endDate: Date,
+) => {
+  const startDateFormat = generateYYYYMMDD(startDate);
+  const endDateFormat = generateYYYYMMDD(endDate);
+
+  const queryKey = ['mySchedules', startDateFormat, endDateFormat];
   const queryClient = useQueryClient();
 
   await queryClient.prefetchQuery(
     queryKey,
-    () => fetchMySchedules(year, month + 1),
+    () => fetchMySchedules(startDateFormat, endDateFormat),
     {
       staleTime: STALE_TIME.MY_SCHEDULES,
     },
