4e5cfff1a5b6e55eaf7c425993180f368fc8bb84
WizardRabbit <curious.wzrabbit@gmail.com>
2023-12-19 02:00:21 +0900

    refactor: 공통 캘린더 조회 비동기 로직에 변경된 일정 명세 적용

--- frontend/src/hooks/queries/useFetchMySchedules.ts
+++ frontend/src/hooks/queries/useFetchMySchedules.ts
@@ -1,13 +1,17 @@
 import { useQuery } from '@tanstack/react-query';
 import { fetchMySchedules } from '~/apis/schedule';
 import { STALE_TIME } from '~/constants/query';
+import { generateYYYYMMDD } from '~/utils/generateYYYYMMDD';
 
-export const useFetchMySchedules = (year: number, month: number) => {
+export const useFetchMySchedules = (startDate: Date, endDate: Date) => {
+  const startDateFormat = generateYYYYMMDD(startDate);
+  const endDateFormat = generateYYYYMMDD(endDate);
+
   const { data } = useQuery(
-    ['mySchedules', year, month],
-    () => fetchMySchedules(year, month + 1),
+    ['mySchedules', startDateFormat, endDateFormat],
+    () => fetchMySchedules(startDateFormat, endDateFormat),
     {
-      staleTime: STALE_TIME.MY_SCHEDULES,
+      staleTime: STALE_TIME.SCHEDULES,
     },
   );
 
