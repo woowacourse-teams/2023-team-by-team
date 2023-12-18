052f82b247729b668d622346b7f77ac255afda16
WizardRabbit <curious.wzrabbit@gmail.com>
2023-12-19 00:34:20 +0900

    feat: Date 객체를 YYYYMMDD 형태로 변환해주는 유틸 함수 구현

---
+++ frontend/src/utils/generateYYYYMMDD.ts
@@ -0,0 +1,7 @@
+export const generateYYYYMMDD = (date: Date) => {
+  const year = String(date.getFullYear()).padStart(4, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+
+  return `${year}${month}${day}`;
+};
