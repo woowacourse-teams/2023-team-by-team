29d6c342772b951bca8c97701c46e920cf164189
WizardRabbit <curious.wzrabbit@gmail.com>
2023-12-23 21:20:09 +0900

    feat: useDateTimeRange 커스텀 훅 생성 및 state 타입 추가
    - 일정 생성 시 모달, 일정 수정 시 모달이 하는 역할이 동일하므로, 공통으로 묶기 위한 커스텀 훅 생성
    - 또한, 일부 값을 변경할 때 UX 또는 오류 수정을 위해 자동으로 값을 변경하는 로직을 반영하기 위함

---
+++ frontend/src/hooks/schedule/useDateTimeRange.ts
@@ -0,0 +1,13 @@
+interface DateTimeRange {
+  title: string;
+  startDate: string;
+  startTime: string;
+  endDate: string;
+  endTime: string;
+  dateDifference: number;
+  isAllDay: boolean;
+}
+
+export const useDateTimeRange = () => {
+  return null;
+};
