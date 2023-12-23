8c899540eecb64e0b92211ed9c13632428347788
WizardRabbit <curious.wzrabbit@gmail.com>
2023-12-23 23:00:19 +0900

    feat: useScheduleEditModal에 구현한 커스텀 훅을 적용

--- frontend/src/hooks/schedule/useScheduleEditModal.ts
+++ frontend/src/hooks/schedule/useScheduleEditModal.ts
@@ -1,123 +1,61 @@
-import type { ChangeEventHandler, FormEventHandler } from 'react';
-import { useState } from 'react';
 import { useModifySchedule } from '~/hooks/queries/useModifySchedule';
 import { useModal } from '~/hooks/useModal';
 import { isYYYYMMDDHHMM } from '~/types/typeGuard';
+import type { FormEventHandler } from 'react';
 import type { Schedule } from '~/types/schedule';
 import { useToast } from '~/hooks/useToast';
 import { useTeamPlace } from '~/hooks/useTeamPlace';
+import { useDateTimeRange } from '~/hooks/schedule/useDateTimeRange';
 
 export const useScheduleEditModal = (
   scheduleId: Schedule['id'],
   initialSchedule?: Schedule,
 ) => {
-  const { teamPlaceId } = useTeamPlace();
-  const [startDate, startTime] = initialSchedule?.startDateTime.split(' ') ?? [
-    '',
-  ];
-  const [endDate, endTime] = initialSchedule?.endDateTime.split(' ') ?? [''];
-  const [schedule, setSchedule] = useState({
-    title: initialSchedule?.title,
+  const {
+    title,
     startDate,
     endDate,
-  });
-  const [times, setTimes] = useState({
     startTime,
-    endTime: endTime === '23:59' ? '23:30' : endTime,
-  });
-  const [isAllDay, setIsAllDay] = useState(endTime === '23:59');
+    endTime,
+    isValid,
+    isAllDay,
+    handleScheduleChange,
+    handleScheduleBlur,
+    handleStartTimeChange,
+    handleEndTimeChange,
+    handleIsAllDayChange,
+  } = useDateTimeRange(initialSchedule, initialSchedule?.title);
   const { closeModal } = useModal();
   const { showToast } = useToast();
+  const { teamPlaceId } = useTeamPlace();
   const { mutateModifySchedule } = useModifySchedule(teamPlaceId, scheduleId);
 
-  const handleIsAllDayChange = () => {
-    setIsAllDay((prev) => !prev);
-  };
-
-  const handleScheduleChange: ChangeEventHandler<HTMLInputElement> = (e) => {
-    const { name, value } = e.target;
-
-    setSchedule((prev) => ({ ...prev, [name]: value }));
-  };
-
-  const handleStartTimeChange = (value: string) => {
-    if (!isValidEndTime(value, times['endTime'])) {
-      setTimes((prev) => {
-        return {
-          ...prev,
-          ['startTime']: value,
-          ['endTime']: value,
-        };
-      });
-
-      return;
-    }
-
-    setTimes((prev) => {
-      return {
-        ...prev,
-        ['startTime']: value,
-      };
-    });
-  };
-
-  const handleEndTimeChange = (value: string) => {
-    if (!isValidEndTime(times['startTime'], value)) {
-      setTimes((prev) => {
-        return {
-          ...prev,
-          ['endTime']: prev['startTime'],
-        };
-      });
-
-      return;
-    }
-
-    setTimes((prev) => {
-      return {
-        ...prev,
-        ['endTime']: value,
-      };
-    });
-  };
-
-  const isValidEndTime = (startTime: string, endTime: string) => {
-    const { startDate, endDate } = schedule;
-    const start = new Date(`${startDate} ${startTime}`);
-    const end = new Date(`${endDate} ${endTime}`);
-
-    return start < end;
-  };
+  const schedule = { title, startDate, endDate };
+  const times = { startTime, endTime };
 
   const handleScheduleSubmit: FormEventHandler<HTMLFormElement> = (e) => {
     e.preventDefault();
 
-    const { title, startDate, endDate } = schedule;
-    const { startTime, endTime } = times;
-    if (
-      typeof title !== 'string' ||
-      typeof startDate !== 'string' ||
-      typeof endDate !== 'string'
-    )
+    const startDateTime = `${startDate} ${startTime}`;
+    const endDateTime = `${endDate} ${endTime}`;
+
+    if (!isYYYYMMDDHHMM(startDateTime) || !isYYYYMMDDHHMM(endDateTime)) {
       return;
+    }
 
-    const formattedStartDateTime = `${startDate} ${
-      isAllDay ? '00:00' : startTime
-    }`;
-    const formattedEndDateTime = `${endDate} ${isAllDay ? '23:59' : endTime}`;
-
-    if (
-      !isYYYYMMDDHHMM(formattedStartDateTime) ||
-      !isYYYYMMDDHHMM(formattedEndDateTime)
-    ) {
+    if (!isValid) {
+      showToast(
+        'error',
+        '날짜/시간 형식이 올바르지 않습니다. 올바르게 입력 후 다시 시도해 주세요.',
+      );
       return;
     }
 
     mutateModifySchedule(
       {
         title,
-        startDateTime: formattedStartDateTime,
-        endDateTime: formattedEndDateTime,
+        startDateTime,
+        endDateTime,
       },
       {
         onSuccess: () => {
@@ -140,6 +78,7 @@ export const useScheduleEditModal = (
 
     handlers: {
       handleScheduleChange,
+      handleScheduleBlur,
       handleScheduleSubmit,
       handleStartTimeChange,
       handleEndTimeChange,
