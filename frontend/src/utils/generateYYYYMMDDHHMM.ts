9122a1cb56828f22c34e1d401afda212a6604f98
WizardRabbit <curious.wzrabbit@gmail.com>
2023-12-23 21:16:03 +0900

    feat: YYYYMMDDHHMM 생성 유틸 함수 구현

---
+++ frontend/src/utils/generateYYYYMMDDHHMM.ts
@@ -0,0 +1,11 @@
+import type { YYYYMMDDHHMM } from '~/types/schedule';
+
+export const generateYYYYMMDDHHMM = (date: Date): YYYYMMDDHHMM => {
+  const year = String(date.getFullYear()).padStart(4, '0');
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const hour = String(date.getHours()).padStart(2, '0');
+  const minute = String(date.getMinutes()).padStart(2, '0');
+
+  return `${year}-${month}-${day} ${hour}:${minute}`;
+};
