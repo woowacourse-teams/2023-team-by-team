8985ed32e141caf7ecf6105eccb515578d358c51
Rulu <hafnium1923@gmail.com>
2024-01-02 18:22:52 +0900

    fix: api 요청 쿼리 변경

--- frontend/src/hooks/queries/useFetchMyDailySchedule.ts
+++ frontend/src/hooks/queries/useFetchMyDailySchedule.ts
@@ -2,14 +2,10 @@ import { useQuery } from '@tanstack/react-query';
 import { fetchMySchedules } from '~/apis/schedule';
 import { STALE_TIME } from '~/constants/query';
 
-export const useFetchMyDailySchedules = (
-  year: number,
-  month: number,
-  day: number,
-) => {
+export const useFetchMyDailySchedules = (startDate: string) => {
   const { data } = useQuery(
-    ['myDailySchedules', year, month, day],
-    () => fetchMySchedules(year, month + 1, day),
+    ['myDailySchedules', startDate],
+    () => fetchMySchedules(startDate, startDate),
     {
       staleTime: STALE_TIME.MY_DAILY_SCHEDULES,
     },
