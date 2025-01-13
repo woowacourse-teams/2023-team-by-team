f714b6be0de177230a53be46be168a428f1422e6
Rulu <hafnium1923@gmail.com>
2025-01-13 22:03:28 +0900

    fix: 메모 없을 때 등록안되는 버그 픽스

--- frontend/src/components/team_calendar/ScheduleAddModal/ScheduleAddModal.tsx
+++ frontend/src/components/team_calendar/ScheduleAddModal/ScheduleAddModal.tsx
@@ -15,6 +15,7 @@ import { getIsMobile } from '~/utils/getIsMobile';
 import Switch from '~/components/common/Switch/Switch';
 import theme from '~/styles/theme';
 import Svg from '~/components/common/Svg/Svg';
+import { SCHEDULE_DESCRIPTION_MAX_LENGTH } from '~/constants/calendar';
 
 interface ScheduleAddModalProps {
   calendarSize?: CalendarSize;
@@ -172,25 +173,31 @@ const ScheduleAddModal = (props: ScheduleAddModalProps) => {
               </Text>
             </Button>
           </S.ConvenientContainer>
-          <S.DescriptionDiv $isDescription={isDescription}>
-            <S.DescriptionTextarea
-              rows={1}
-              placeholder="메모를 작성해주세요.(최대 100자)"
-              value={schedule.description}
-              ref={descriptionInputRef}
-              onChange={handleDescriptionInput}
-              required
-            />
-            <S.WarnDiv>
-              {!isDescriptionMaxLength ? (
-                <Text size="xs">({schedule.description.length} / 100자)</Text>
-              ) : (
-                <Text size="xs" css={S.errorText}>
-                  최대 100자까지 입력가능합니다.
-                </Text>
-              )}
-            </S.WarnDiv>
-          </S.DescriptionDiv>
+          {isDescription && (
+            <>
+              <S.DescriptionTextarea
+                rows={1}
+                placeholder={`메모를 작성해주세요.(최대 ${SCHEDULE_DESCRIPTION_MAX_LENGTH}자)`}
+                value={schedule.description}
+                ref={descriptionInputRef}
+                onChange={handleDescriptionInput}
+              />
+              <S.WarnDiv>
+                {!isDescriptionMaxLength ? (
+                  <Text size="xs">
+                    ({schedule.description.length} / $
+                    {SCHEDULE_DESCRIPTION_MAX_LENGTH}자)
+                  </Text>
+                ) : (
+                  <Text size="xs" css={S.errorText}>
+                    최대 ${SCHEDULE_DESCRIPTION_MAX_LENGTH}자까지
+                    입력가능합니다.
+                  </Text>
+                )}
+              </S.WarnDiv>
+            </>
+          )}
+
           <S.InnerContainer>
             <S.TeamNameContainer title={displayName}>
               <TeamBadge teamPlaceColor={teamPlaceColor} size="md" />
